74d3cc936219ad5087488f0ea8c1284cae33aada
Pablo <ppajueloc@gmail.com>
2018-08-06 06:49:28 +0200

    - beta 4

--- app/src/main/java/com/dhis2/usescases/teiDashboard/dashboardfragments/RelationshipFragment.java
+++ app/src/main/java/com/dhis2/usescases/teiDashboard/dashboardfragments/RelationshipFragment.java
@@ -4,12 +4,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.dhis2.R;
+import com.dhis2.data.tuples.Pair;
 import com.dhis2.databinding.FragmentRelationshipsBinding;
 import com.dhis2.usescases.general.FragmentGlobalAbstract;
 import com.dhis2.usescases.teiDashboard.DashboardProgramModel;
@@ -19,6 +21,7 @@ import com.dhis2.usescases.teiDashboard.mobile.TeiDashboardMobileActivity;
 import com.dhis2.utils.Constants;
 
 import org.hisp.dhis.android.core.relationship.Relationship;
+import org.hisp.dhis.android.core.relationship.RelationshipType;
 
 import java.util.List;
 
@@ -59,7 +62,7 @@ public class RelationshipFragment extends FragmentGlobalAbstract {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_relationships, container, false);
         binding.setPresenter(presenter);
         relationshipAdapter = new RelationshipAdapter(presenter);
@@ -84,10 +87,8 @@ public class RelationshipFragment extends FragmentGlobalAbstract {
         binding.executePendingBindings();
     }
 
-    public Consumer<List<Relationship>> setRelationships() {
-        return relationships -> {
-            relationshipAdapter.addItems(relationships);
-        };
+    public Consumer<List<Pair<Relationship,RelationshipType>>> setRelationships() {
+        return relationships -> relationshipAdapter.addItems(relationships);
     }
 
     @Override
