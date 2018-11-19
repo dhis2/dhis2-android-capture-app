4a79031c67ce9d891c0ba4b09fa9c82c8816219b
Pablo <ppajuelo@quadram.mobi>
2018-11-19 10:22:01 +0100

    - master update

---
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/EventCaptureActivity.java
@@ -0,0 +1,10 @@
+package org.dhis2.usescases.eventsWithoutRegistration.eventCapture;
+
+import org.dhis2.usescases.general.ActivityGlobalAbstract;
+
+/**
+ * QUADRAM. Created by ppajuelo on 19/11/2018.
+ */
+public class EventCaptureActivity extends ActivityGlobalAbstract {
+
+}
