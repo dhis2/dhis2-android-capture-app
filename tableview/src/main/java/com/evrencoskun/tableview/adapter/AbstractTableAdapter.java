10cdcf422dafdd28795862c946a6be8efc2f2611
Marina <marina_cape@hotmail.com>
2019-03-19 10:45:30 +0100

    BUG on the last column of data sets

--- tableview/src/main/java/com/evrencoskun/tableview/adapter/AbstractTableAdapter.java
+++ tableview/src/main/java/com/evrencoskun/tableview/adapter/AbstractTableAdapter.java
@@ -271,6 +271,7 @@ public abstract class AbstractTableAdapter<CH, RH, C> implements ITableAdapter {
 
     public void changeCellItem(int columnPosition, int rowPosition, C cellModel) {
         List<C> cellItems = (List<C>) mCellRecyclerViewAdapter.getItem(rowPosition);
+        columnPosition = cellItems.size() -1 == columnPosition ? columnPosition +1 : columnPosition;
         if (cellItems != null && cellItems.size() > columnPosition) {
             // Update cell row items.
             cellItems.set(columnPosition, cellModel);
