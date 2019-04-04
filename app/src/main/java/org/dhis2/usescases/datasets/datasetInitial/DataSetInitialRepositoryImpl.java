5d04d96c16ab44fae6aeca055248cd320b3f5652
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2019-04-04 16:05:59 +0200

    Removes queries for sdk dataSet Module

--- app/src/main/java/org/dhis2/usescases/datasets/datasetInitial/DataSetInitialRepositoryImpl.java
+++ app/src/main/java/org/dhis2/usescases/datasets/datasetInitial/DataSetInitialRepositoryImpl.java
@@ -1,17 +1,15 @@
 package org.dhis2.usescases.datasets.datasetInitial;
 
-import android.database.Cursor;
-
 import com.squareup.sqlbrite2.BriteDatabase;
 
 import org.hisp.dhis.android.core.D2;
-import org.hisp.dhis.android.core.category.CategoryModel;
+import org.hisp.dhis.android.core.category.Category;
+import org.hisp.dhis.android.core.category.CategoryCombo;
 import org.hisp.dhis.android.core.category.CategoryOption;
-import org.hisp.dhis.android.core.category.CategoryOptionComboModel;
+import org.hisp.dhis.android.core.common.BaseIdentifiableObject;
 import org.hisp.dhis.android.core.dataset.DataInputPeriodModel;
 import org.hisp.dhis.android.core.organisationunit.OrganisationUnitModel;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.NonNull;
@@ -48,10 +46,8 @@ public class DataSetInitialRepositoryImpl implements DataSetInitialRepository {
     public Observable<DataSetInitialModel> dataSet() {
         return Observable.just(d2.dataSetModule().dataSets.byUid().eq(dataSetUid).one().get())
                 .map(dataSet -> {
-                    String categoryComboDisplayName =
-                            d2.categoryModule().categoryCombos.byUid().eq(dataSet.categoryCombo().uid()).one().get().displayName();
-
-                    List<CategoryModel> categoryModels = getCategoryModels(dataSet.categoryCombo().uid());
+                    String categoryComboDisplayName = d2.categoryModule().categoryCombos.byUid().eq(dataSet.categoryCombo().uid()).one().get().displayName();
+                    CategoryCombo categoryCombos = d2.categoryModule().categoryCombos.withCategories().byUid().eq(dataSet.categoryCombo().uid()).one().get();
 
                     return DataSetInitialModel.create(
                             dataSet.displayName(),
@@ -59,27 +55,9 @@ public class DataSetInitialRepositoryImpl implements DataSetInitialRepository {
                             dataSet.categoryCombo().uid(),
                             categoryComboDisplayName,
                             dataSet.periodType(),
-                            categoryModels
+                            categoryCombos.categories()
                     );
                 });
-    }
-
-    private List<CategoryModel> getCategoryModels(String categoryComboUid) {
-        String GET_CATEGORIES = "SELECT Category.* FROM Category " +
-                "JOIN CategoryCategoryComboLink ON CategoryCategoryComboLink.category = Category.uid " +
-                "WHERE CategoryCategoryComboLink.categoryCombo = ?";
-
-        List<CategoryModel> categoryModelList = new ArrayList<>();
-        try (Cursor cursor = briteDatabase.query(GET_CATEGORIES, categoryComboUid)) {
-            if (cursor != null && cursor.moveToFirst()) {
-                for (int i = 0; i < cursor.getCount(); i++) {
-                    categoryModelList.add(CategoryModel.create(cursor));
-                    cursor.moveToNext();
-                }
-            }
-        }
-
-        return categoryModelList;
     }
 
     @NonNull
@@ -97,24 +75,13 @@ public class DataSetInitialRepositoryImpl implements DataSetInitialRepository {
     @Override
     public Observable<List<CategoryOption>> catCombo(String categoryUid) {
         return Observable.just(d2.categoryModule().categories.withCategoryOptions().byUid().eq(categoryUid).one().get())
-                .map(category -> category.categoryOptions());
+                .map(Category::categoryOptions);
     }
 
     @NonNull
     @Override
-    public Flowable<String> getCategoryOptionCombo(String catOptions, String catCombo) {
-        String GET_CAT_OPTION_COMBO = " SELECT CategoryOptionCombo.* " +
-                " FROM CategoryOptionCombo " +
-                " JOIN CategoryCategoryComboLink ON CategoryCategoryComboLink.categoryCombo = CategoryOptionCombo.categoryCombo " +
-                " JOIN CategoryCategoryOptionLink ON CategoryCategoryOptionLink.categoryOption = CategoryOptionComboCategoryOptionLink.categoryOption " +
-                " JOIN CategoryOptionComboCategoryOptionLink ON CategoryOptionComboCategoryOptionLink.categoryOptionCombo = CategoryOptionCombo.uid " +
-                " WHERE CategoryOptionCombo.categoryCombo = ? ";
-
-        String query = GET_CAT_OPTION_COMBO;
-        query = query + " AND CategoryOptionComboCategoryOptionLink.categoryOption IN (" + catOptions + ")";
-        return briteDatabase.createQuery(CategoryOptionComboModel.TABLE, query, catCombo)
-                .mapToOne(cursor ->
-                        cursor.getString(cursor.getColumnIndex("uid"))
-                ).toFlowable(BackpressureStrategy.LATEST);
+    public Flowable<String> getCategoryOptionCombo(List<String> catOptions, String catCombo) {
+        return Flowable.just(d2.categoryModule().categoryOptionCombos.withCategoryOptions().byCategoryOptions(catOptions).byCategoryComboUid().eq(catCombo).one().get())
+                .map(BaseIdentifiableObject::uid);
     }
 }
