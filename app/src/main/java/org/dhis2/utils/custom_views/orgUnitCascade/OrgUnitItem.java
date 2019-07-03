6189b0ac1406065e68d13308bb23391af7a4bc8d
Marina <marina_cape@hotmail.com>
2019-07-03 15:04:23 +0200

    ANDROAPP-2283 Organisation units containing "/" in name get treated as different OUs on different levels

--- app/src/main/java/org/dhis2/utils/custom_views/orgUnitCascade/OrgUnitItem.java
+++ app/src/main/java/org/dhis2/utils/custom_views/orgUnitCascade/OrgUnitItem.java
@@ -65,6 +65,15 @@ public class OrgUnitItem {
         for (OrganisationUnit ou : orgUnitList) {
             String[] uidPath = ou.path().replaceFirst("/", "").split("/");
             String[] namePath = ou.displayNamePath().replaceFirst("/", "").split("/");
+            int count = 0;
+            for(int i = 0; i< ou.displayName().length(); i++){
+                if(ou.displayName().charAt(i) == '/')
+                    count++;
+            }
+
+            if(ou.displayName().contains("/"))
+                namePath[(namePath.length -1)-count] = ou.displayName();
+
             if (uidPath.length >= level && !menuOrgUnits.containsKey(uidPath[level - 1]) && (isEmpty(parentUid) || (level > 1 && uidPath[level - 2].equals(parentUid)))) {
                 boolean canCapture = ouRepo.byOrganisationUnitScope(ouScope).uid(uidPath[level - 1]).exists();
                 menuOrgUnits.put(uidPath[level - 1],
