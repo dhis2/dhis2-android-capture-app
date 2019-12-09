87a5de6e18d0014e49b97cfc8e59688c7c1a1a4b
Jorge Sánchez <xurxosf@gmail.com>
2019-12-09 11:14:33 +0100

    Fix bug with progress bar when enter to the app

--- app/src/main/java/org/dhis2/usescases/main/program/ProgramPresenter.kt
+++ app/src/main/java/org/dhis2/usescases/main/program/ProgramPresenter.kt
@@ -25,6 +25,16 @@ class ProgramPresenter internal constructor(
         val loadingProcessor = PublishProcessor.create<Boolean>()
 
         disposable.add(
+            loadingProcessor
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+                .subscribe(
+                    { view.showFilterProgress() },
+                    { Timber.e(it) }
+                )
+        )
+
+        disposable.add(
             filterManager.asFlowable()
                 .startWith(filterManager)
                 .doOnNext { Timber.tag("INIT DATA").d("NEW FILTER") }
@@ -55,16 +65,6 @@ class ProgramPresenter internal constructor(
                     { programs -> view.swapProgramModelData(programs) },
                     { throwable -> view.renderError(throwable.message ?: "") },
                     { Timber.tag("INIT DATA").d("LOADING ENDED") }
-                )
-        )
-
-        disposable.add(
-            loadingProcessor
-                .subscribeOn(schedulerProvider.io())
-                .observeOn(schedulerProvider.ui())
-                .subscribe(
-                    { view.showFilterProgress() },
-                    { Timber.e(it) }
                 )
         )
 
