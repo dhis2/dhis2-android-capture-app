a490faad869c460d20070d11b725ea02c0d8561c
jaimeToca <jaime.toca.munoz@gmail.com>
2020-01-15 12:17:24 +0100

    adding rx idler, robots and others

--- app/src/androidTest/java/org/dhis2/Dhis2Runner.java
+++ app/src/androidTest/java/org/dhis2/Dhis2Runner.java
@@ -2,21 +2,24 @@ package org.dhis2;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
+
 import androidx.test.runner.AndroidJUnitRunner;
+
+import com.squareup.rx2.idler.Rx2Idler;
+
+import org.dhis2.data.schedulers.SchedulerProvider;
+import org.dhis2.data.schedulers.SchedulersProviderImpl;
+
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.plugins.RxJavaPlugins;
 
 public class Dhis2Runner extends AndroidJUnitRunner {
 
-    @Override
-    public void onCreate(Bundle arguments) {
-        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
-        super.onCreate(arguments);
-    }
-
     @Override public void onStart() {
-    //    RxJavaPlugins.setInitComputationSchedulerHandler(
-    //            Rx2Idler.create("RxJava 2.x Computation Scheduler"));
-    //    RxJavaPlugins.setInitIoSchedulerHandler(
-    //            Rx2Idler.create("RxJava 2.x IO Scheduler"));
         super.onStart();
+        Log.i("DhisRunner", "onStart");
+        RxJavaPlugins.setInitIoSchedulerHandler(Rx2Idler.create("RxJava 2.x Io Scheduler"));
+        RxJavaPlugins.setInitIoSchedulerHandler(Rx2Idler.create("RxJava 2.x IO Scheduler"));
     }
 }
---
+++ app/src/androidTest/java/org/dhis2/usescases/login/LoginRobot.kt
@@ -0,0 +1,5 @@
+package org.dhis2.usescases.login
+
+class LoginRobot  {
+
+}
