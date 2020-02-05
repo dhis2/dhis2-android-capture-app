7c9235e5cd03fbae9657d6c840e2201ac915c4a4
Pablo <ppajueloc@gmail.com>
2020-02-05 13:49:21 +0100

    Update EventCaptureRepositoryImpl.java

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/EventCaptureRepositoryImpl.java
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/EventCaptureRepositoryImpl.java
@@ -647,7 +647,7 @@ public class EventCaptureRepositoryImpl implements EventCaptureContract.EventCap
     @Override
     public Flowable<Boolean> eventIntegrityCheck() {
         return d2.eventModule().events().uid(eventUid).get()
-                .map(event -> (event.status()!=EventStatus.ACTIVE || event.status() == EventStatus.ACTIVE) && event.eventDate() != null && !event.eventDate().after(new Date()))
+                .map(event -> (event.status()!=EventStatus.COMPLETED || event.status() == EventStatus.ACTIVE) && event.eventDate() != null && !event.eventDate().after(new Date()))
                 .toFlowable();
     }
 }
