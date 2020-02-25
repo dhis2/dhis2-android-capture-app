020c2c4ff292a99c91417e76ec33cd3a5589c603
mmmateos <mmm8228@gmail.com>
2020-02-25 14:03:02 +0100

    fix: increase row header width when clicking zoom icon

--- tableview/src/main/java/com/evrencoskun/tableview/TableView.java
+++ tableview/src/main/java/com/evrencoskun/tableview/TableView.java
@@ -850,11 +850,13 @@ public class TableView extends FrameLayout implements ITableView {
         }
 
         if (mColumnHeaderRecyclerView != null) {
-            // Update ColumnHeader left margin
-            LayoutParams layoutParams = (LayoutParams) mColumnHeaderRecyclerView.getLayoutParams();
-            layoutParams.leftMargin = rowHeaderWidth;
-            mColumnHeaderRecyclerView.setLayoutParams(layoutParams);
-            mColumnHeaderRecyclerView.requestLayout();
+            // Update ColumnHeaders left margin
+            for(CellRecyclerView mColumnHeaderRecyclerView : mColumnHeaderRecyclerViews) {
+                LayoutParams layoutParams = (LayoutParams) mColumnHeaderRecyclerView.getLayoutParams();
+                layoutParams.leftMargin = rowHeaderWidth;
+                mColumnHeaderRecyclerView.setLayoutParams(layoutParams);
+                mColumnHeaderRecyclerView.requestLayout();
+            }
         }
 
         if (mCellRecyclerView != null) {
@@ -863,6 +865,16 @@ public class TableView extends FrameLayout implements ITableView {
             layoutParams.leftMargin = rowHeaderWidth;
             mCellRecyclerView.setLayoutParams(layoutParams);
             mCellRecyclerView.requestLayout();
+        }
+
+        if(mBackupHeaders != null) {
+            // Update BackupHeaders left margin
+            for(CellRecyclerView mBackupHeader : mBackupHeaders) {
+                LayoutParams layoutParams = (LayoutParams) mBackupHeader.getLayoutParams();
+                layoutParams.leftMargin = rowHeaderWidth;
+                mBackupHeader.setLayoutParams(layoutParams);
+                mBackupHeader.requestLayout();
+            }
         }
 
         if (getAdapter() != null) {
