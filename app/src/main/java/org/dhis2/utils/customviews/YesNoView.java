bc5660b0c40d713a68b8711f4b2062c56c729229
Pablo <ppajueloc@gmail.com>
2020-03-04 10:24:44 +0100

    ANDROAPP-741 - BOOLEAN and TRUE_ONLY renderings

--- app/src/main/java/org/dhis2/utils/customviews/YesNoView.java
+++ app/src/main/java/org/dhis2/utils/customviews/YesNoView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -14,6 +15,7 @@ import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
 
 import com.google.android.material.checkbox.MaterialCheckBox;
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import org.dhis2.BR;
 import org.dhis2.R;
@@ -29,14 +31,22 @@ public class YesNoView extends FieldLayout implements RadioGroup.OnCheckedChange
 
     private ViewDataBinding binding;
 
+    private LinearLayout checksLayout;
     private RadioGroup radioGroup;
     private RadioButton yes;
     private RadioButton no;
     private MaterialCheckBox checkYes;
     private MaterialCheckBox checkNo;
+    private boolean changingChecks;
 
     private TextView labelView;
     private View clearButton;
+    private LinearLayout checkGroup;
+    private SwitchMaterial yesOnlyToggle;
+    private RadioGroup.OnCheckedChangeListener radioClickListener;
+    private CompoundButton.OnCheckedChangeListener checkBoxClickListener;
+    private CompoundButton.OnCheckedChangeListener toggleListener;
+    private OnValueChanged valueListener;
 
     public YesNoView(Context context) {
         super(context);
@@ -57,8 +67,12 @@ public class YesNoView extends FieldLayout implements RadioGroup.OnCheckedChange
         super.init(context);
     }
 
-    public void setValueType(ValueType valueType) {
+    public void setValueListener(OnValueChanged listener) {
+        this.valueListener = listener;
+    }
 
+    public void setValueType(ValueType valueType) {
+        this.valueType = valueType;
         if (valueType == ValueType.TRUE_ONLY) {
             no.setVisibility(View.GONE);
             checkNo.setVisibility(View.GONE);
@@ -66,41 +80,80 @@ public class YesNoView extends FieldLayout implements RadioGroup.OnCheckedChange
             no.setVisibility(View.VISIBLE);
             checkNo.setVisibility(View.VISIBLE);
         }
-
     }
 
     public void setRendering(ValueTypeRenderingType rendering) {
         switch (rendering) {
             case HORIZONTAL_CHECKBOXES:
-                radioGroup.setOrientation(LinearLayout.HORIZONTAL);
-                yes.setVisibility(View.GONE);
-                no.setVisibility(View.GONE);
-                checkYes.setVisibility(View.VISIBLE);
-                checkNo.setVisibility(View.VISIBLE);
+                checksLayout.setVisibility(View.VISIBLE);
+                radioGroup.setVisibility(View.GONE);
+                checkGroup.setVisibility(View.VISIBLE);
+                yesOnlyToggle.setVisibility(View.GONE);
+                clearButton.setVisibility(View.GONE);
+                checkGroup.setOrientation(LinearLayout.HORIZONTAL);
+                radioGroup.setOnCheckedChangeListener(null);
+                checkYes.setOnCheckedChangeListener(checkBoxClickListener);
+                checkNo.setOnCheckedChangeListener(checkBoxClickListener);
+                yesOnlyToggle.setOnCheckedChangeListener(null);
                 break;
             case VERTICAL_CHECKBOXES:
-                radioGroup.setOrientation(LinearLayout.VERTICAL);
-                yes.setVisibility(View.GONE);
-                no.setVisibility(View.GONE);
-                checkYes.setVisibility(View.VISIBLE);
-                checkNo.setVisibility(View.VISIBLE);
+                checksLayout.setVisibility(View.VISIBLE);
+                radioGroup.setVisibility(View.GONE);
+                checkGroup.setVisibility(View.VISIBLE);
+                yesOnlyToggle.setVisibility(View.GONE);
+                clearButton.setVisibility(View.GONE);
+                checkGroup.setOrientation(LinearLayout.VERTICAL);
+                radioGroup.setOnCheckedChangeListener(null);
+                checkYes.setOnCheckedChangeListener(checkBoxClickListener);
+                checkNo.setOnCheckedChangeListener(checkBoxClickListener);
+                yesOnlyToggle.setOnCheckedChangeListener(null);
                 break;
             case VERTICAL_RADIOBUTTONS:
+                checksLayout.setVisibility(View.VISIBLE);
+                radioGroup.setVisibility(View.VISIBLE);
+                checkGroup.setVisibility(View.GONE);
+                yesOnlyToggle.setVisibility(View.GONE);
+                clearButton.setVisibility(View.VISIBLE);
                 radioGroup.setOrientation(LinearLayout.VERTICAL);
-                yes.setVisibility(View.VISIBLE);
-                no.setVisibility(View.VISIBLE);
-                checkYes.setVisibility(View.GONE);
-                checkNo.setVisibility(View.GONE);
+                radioGroup.setOnCheckedChangeListener(radioClickListener);
+                checkYes.setOnCheckedChangeListener(null);
+                checkNo.setOnCheckedChangeListener(null);
+                yesOnlyToggle.setOnCheckedChangeListener(null);
                 break;
             case TOGGLE:
+                if (valueType == ValueType.TRUE_ONLY) {
+                    checksLayout.setVisibility(View.GONE);
+                    radioGroup.setVisibility(View.GONE);
+                    checkGroup.setVisibility(View.GONE);
+                    yesOnlyToggle.setVisibility(View.VISIBLE);
+                    clearButton.setVisibility(View.GONE);
+                    radioGroup.setOnCheckedChangeListener(null);
+                    yesOnlyToggle.setOnCheckedChangeListener(toggleListener);
+                } else {
+                    checksLayout.setVisibility(View.VISIBLE);
+                    radioGroup.setVisibility(View.VISIBLE);
+                    checkGroup.setVisibility(View.GONE);
+                    yesOnlyToggle.setVisibility(View.GONE);
+                    clearButton.setVisibility(View.VISIBLE);
+                    radioGroup.setOrientation(LinearLayout.HORIZONTAL);
+                    radioGroup.setOnCheckedChangeListener(radioClickListener);
+                    yesOnlyToggle.setOnCheckedChangeListener(null);
+                }
+                checkYes.setOnCheckedChangeListener(null);
+                checkNo.setOnCheckedChangeListener(null);
                 break;
             default:
             case HORIZONTAL_RADIOBUTTONS:
+                checksLayout.setVisibility(View.VISIBLE);
+                radioGroup.setVisibility(View.VISIBLE);
+                checkGroup.setVisibility(View.GONE);
+                yesOnlyToggle.setVisibility(View.GONE);
+                clearButton.setVisibility(View.VISIBLE);
                 radioGroup.setOrientation(LinearLayout.HORIZONTAL);
-                yes.setVisibility(View.VISIBLE);
-                no.setVisibility(View.VISIBLE);
-                checkYes.setVisibility(View.GONE);
-                checkNo.setVisibility(View.GONE);
+                radioGroup.setOnCheckedChangeListener(this);
+                checkYes.setOnCheckedChangeListener(null);
+                checkNo.setOnCheckedChangeListener(null);
+                yesOnlyToggle.setOnCheckedChangeListener(null);
                 break;
         }
     }
@@ -131,15 +184,75 @@ public class YesNoView extends FieldLayout implements RadioGroup.OnCheckedChange
         else
             binding = DataBindingUtil.inflate(inflater, R.layout.yes_no_view, this, true);
 
+        checksLayout = findViewById(R.id.checkLayouts);
         radioGroup = findViewById(R.id.radiogroup);
         clearButton = findViewById(R.id.clearSelection);
         yes = findViewById(R.id.yes);
         no = findViewById(R.id.no);
+        checkGroup = findViewById(R.id.checkGroup);
         checkYes = findViewById(R.id.checkYes);
         checkNo = findViewById(R.id.checkNo);
+        yesOnlyToggle = findViewById(R.id.yesOnlyToggle);
 
         labelView = findViewById(R.id.label);
-        radioGroup.setOnCheckedChangeListener(this);
+
+        clearButton.setOnClickListener(v -> {
+            if(valueListener!=null){
+                valueListener.onClearValue();
+            }
+        });
+
+        radioClickListener = (group, checkedId) -> {
+            if (valueListener != null) {
+                switch (checkedId) {
+                    case R.id.yes:
+                        valueListener.onValueChanged(true);
+                        break;
+                    case R.id.no:
+                        valueListener.onValueChanged(false);
+                        break;
+                    default:
+                        valueListener.onClearValue();
+                        break;
+                }
+            }
+        };
+
+        checkBoxClickListener = (buttonView, isChecked) -> {
+            changingChecks = false;
+            switch (buttonView.getId()) {
+                case R.id.checkYes:
+                    if (isChecked && checkNo.isChecked()) {
+                        changingChecks = true;
+                        checkNo.setChecked(false);
+                    }
+                    break;
+                case R.id.checkNo:
+                    if (isChecked && checkYes.isChecked()) {
+                        changingChecks = true;
+                        checkYes.setChecked(false);
+                    }
+                    break;
+            }
+            if (!changingChecks && valueListener != null) {
+                boolean anyCheck = checkYes.isChecked() || checkNo.isChecked();
+                if(anyCheck){
+                    valueListener.onValueChanged(checkYes.isChecked());
+                }else{
+                    valueListener.onClearValue();
+                }
+            }
+        };
+
+        toggleListener = (buttonView, isChecked) -> {
+            if (valueListener != null) {
+                if(isChecked) {
+                    valueListener.onValueChanged(true);
+                }else{
+                    valueListener.onClearValue();
+                }
+            }
+        };
 
     }
 
@@ -161,5 +274,39 @@ public class YesNoView extends FieldLayout implements RadioGroup.OnCheckedChange
     @Override
     protected void onFocusChanged(boolean gainFocus, int direction, @Nullable Rect previouslyFocusedRect) {
         super.onFocusChanged(gainFocus, direction, previouslyFocusedRect);
+    }
+
+    public void setInitialValue(String value) {
+        if (value != null && Boolean.valueOf(value)) {
+            radioGroup.check(R.id.yes);
+            checkYes.setChecked(true);
+            checkNo.setChecked(false);
+            yesOnlyToggle.setChecked(true);
+        } else if (value != null) {
+            radioGroup.check(R.id.no);
+            checkYes.setChecked(false);
+            checkNo.setChecked(true);
+            yesOnlyToggle.setChecked(false);
+        } else {
+            radioGroup.clearCheck();
+            checkYes.setChecked(false);
+            checkNo.setChecked(false);
+            yesOnlyToggle.setChecked(false);
+        }
+    }
+
+    public void setEditable(Boolean editable) {
+        for (int i = 0; i < radioGroup.getChildCount(); i++) {
+            radioGroup.getChildAt(i).setEnabled(editable);
+        }
+        checkYes.setEnabled(editable);
+        checkNo.setEnabled(editable);
+        yesOnlyToggle.setEnabled(editable);
+        clearButton.setEnabled(editable);
+    }
+
+    public interface OnValueChanged {
+        void onValueChanged(boolean isActive);
+        void onClearValue();
     }
 }
