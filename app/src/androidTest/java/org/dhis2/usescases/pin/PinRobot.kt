410b0f545674a8e1440325a5301dcdffa07b0fff
Milagros Rodriguez <milagros@krowdy.co>
2020-03-09 09:50:57 -0500

    test: pin tests

--- app/src/androidTest/java/org/dhis2/usescases/pin/PinRobot.kt
+++ app/src/androidTest/java/org/dhis2/usescases/pin/PinRobot.kt
@@ -2,10 +2,13 @@ package org.dhis2.usescases.pin
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import kotlinx.android.synthetic.main.layout_pin.view.*
 import org.dhis2.R
 import org.dhis2.common.BaseRobot
+
 
 fun pinRobot(pinBody: PinRobot.() -> Unit) {
     PinRobot().apply {
@@ -14,6 +17,10 @@ fun pinRobot(pinBody: PinRobot.() -> Unit) {
 }
 
 class PinRobot : BaseRobot() {
+
+    fun checkErrorAlertIsVisible() {
+        onView(withId(R.id.lockPin)).check(matches(withText("Wrong pin")))
+    }
 
     fun clickForgotCode() {
         onView(withId(R.id.forgotCode)).perform(click())
--- app/src/androidTest/java/org/dhis2/usescases/pin/PinTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/pin/PinTest.kt
@@ -1,17 +1,29 @@
 package org.dhis2.usescases.pin
 
 import android.Manifest
+import android.R
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.RootMatchers.withDecorView
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.google.android.material.internal.ContextUtils.getActivity
 import org.dhis2.data.prefs.Preference.Companion.PIN
 import org.dhis2.data.prefs.Preference.Companion.SESSION_LOCKED
 import org.dhis2.usescases.BaseTest
+import org.dhis2.usescases.login.LoginActivity
 import org.dhis2.usescases.main.MainActivity
 import org.dhis2.usescases.main.MainRobot
 import org.dhis2.usescases.main.homeRobot
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class PinTest : BaseTest() {
@@ -21,15 +33,18 @@ class PinTest : BaseTest() {
     @get:Rule
     val rule = ActivityTestRule(MainActivity::class.java, false, false)
 
+    @get:Rule
+    val ruleLoginActivity = ActivityTestRule(LoginActivity::class.java, false, false)
+
     override fun getPermissionsToBeAccepted(): Array<String> {
         return arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA)
     }
 
     @Test
     fun openPin() {
-        startActivity()
+        /*startActivity()
         mainRobot.clickOnNavigationDrawerMenu()
-                .clickOnPin()
+                .clickOnPin()*/
     }
 
     @Test
@@ -40,8 +55,13 @@ class PinTest : BaseTest() {
             clickOnNavigationDrawerMenu()
             clickOnPin()
         }
-        preferencesRobot.saveValue(PIN, "1234")
-        preferencesRobot.saveValue(SESSION_LOCKED, true)
+        onView(withText("1")).perform(click())
+        onView(withText("2")).perform(click())
+        onView(withText("3")).perform(click())
+        onView(withText("4")).perform(click())
+       // Thread.sleep(1000)
+        /*preferencesRobot.saveValue(PIN, "1234")
+        preferencesRobot.saveValue(SESSION_LOCKED, true)*/
     }
 
     @Test
@@ -50,20 +70,55 @@ class PinTest : BaseTest() {
         //prefrencesRobot.saveValue(Session_Locked, true)
         //Si escribo 1234 voy a la home
         //Si escribo 4123 no voy a ninguno sitio . Check Pin incorrecto
+
+        preferencesRobot.saveValue(SESSION_LOCKED, true)
+        preferencesRobot.saveValue(PIN, "1234")
+        startLoginActivity()
+        onView(withText("1")).perform(click())
+        onView(withText("2")).perform(click())
+        onView(withText("3")).perform(click())
+        onView(withText("4")).perform(click())
+        // inteded home activity
     }
 
     @Test
     fun shouldSendErrorIfPinIsWrong() {
 
+        //Type 4321
+        //should show warning
+
+        preferencesRobot.saveValue(SESSION_LOCKED, true)
+        preferencesRobot.saveValue(PIN, "1234")
+        startLoginActivity()
+        onView(withText("1")).perform(click())
+        onView(withText("2")).perform(click())
+        onView(withText("3")).perform(click())
+        onView(withText("3")).perform(click())
+
+        //check how to match toast
+        //onView(withText("Wrong pin")).inRoot(withDecorView(not(`is`(getActivity(context)!!.getWindow().getDecorView())))).check(matches(isDisplayed()))
+
+        Thread.sleep(1000)
     }
 
     @Test
     fun shouldSuccessfullyLoginIfClickForgotYourCode() {
-        //startActivity()
 
+        preferencesRobot.saveValue(SESSION_LOCKED, true)
+        preferencesRobot.saveValue(PIN, "1234")
+        startLoginActivity()
+
+        pinRobot {
+            clickForgotCode()
+        }
+        //intended al login
     }
 
     fun startActivity(){
+        rule.launchActivity(null)
+    }
+
+    fun startLoginActivity() {
         rule.launchActivity(null)
     }
 }
