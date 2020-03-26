7e8e18bceb3a4a6b888e955040bea9d45a3a8f23
ferdyrod <ferdyrodriguez@gmail.com>
2020-03-26 12:46:56 +0100

    Removes SharedPreferences from logout and uses instead the PreferenceProvider

--- app/src/main/java/org/dhis2/usescases/login/LoginPresenter.kt
+++ app/src/main/java/org/dhis2/usescases/login/LoginPresenter.kt
@@ -1,6 +1,8 @@
 package org.dhis2.usescases.login
 
 import android.os.Build
+import androidx.annotation.RestrictTo
+import androidx.annotation.RestrictTo.Scope
 import co.infinum.goldfinger.Goldfinger
 import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
@@ -194,8 +196,7 @@ class LoginPresenter(
                     .observeOn(schedulers.ui())
                     .subscribe(
                         {
-                            val prefs = view.abstracContext.sharedPreferences
-                            prefs.edit().putBoolean(SESSION_LOCKED, false).apply()
+                            preferenceProvider.setValue(SESSION_LOCKED, false)
                             view.handleLogout()
                         },
                         { view.handleLogout() }
@@ -335,6 +336,11 @@ class LoginPresenter(
         preferenceProvider.setValue(PREFS_USERS, HashSet(users))
 
         return Pair(urls, users)
+    }
+
+    @RestrictTo(Scope.TESTS)
+    fun setUserManager(userManager: UserManager) {
+        this.userManager = userManager
     }
 
     companion object {
