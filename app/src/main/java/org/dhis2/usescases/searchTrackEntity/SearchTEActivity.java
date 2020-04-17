d19074163c5f147d5fb2d758d76f471b2f235050
Pablo <ppajueloc@gmail.com>
2020-04-17 13:55:57 +0200

    Update SearchTEActivity.java

--- app/src/main/java/org/dhis2/usescases/searchTrackEntity/SearchTEActivity.java
+++ app/src/main/java/org/dhis2/usescases/searchTrackEntity/SearchTEActivity.java
@@ -17,7 +17,6 @@ import android.os.StrictMode;
 import android.transition.ChangeBounds;
 import android.transition.Transition;
 import android.transition.TransitionManager;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -443,8 +442,6 @@ public class SearchTEActivity extends ActivityGlobalAbstract implements SearchTE
 
     @Override
     public void setLiveData(LiveData<PagedList<SearchTeiModel>> liveData) {
-        Log.d("SEARCH", "Setting live data");
-        Long start = System.currentTimeMillis();
         if (!fromRelationship) {
             liveData.observe(this, searchTeiModels -> {
                 Trio<PagedList<SearchTeiModel>, String, Boolean> data = presenter.getMessage(searchTeiModels);
@@ -466,8 +463,6 @@ public class SearchTEActivity extends ActivityGlobalAbstract implements SearchTE
                     binding.message.setText(data.val1());
                     binding.scrollView.setVisibility(View.GONE);
                 }
-                Long end = System.currentTimeMillis();
-                Log.d("SEARCH", "Done in " + (end - start));
             });
         } else {
             liveData.observeForever(searchTeiModels -> {
