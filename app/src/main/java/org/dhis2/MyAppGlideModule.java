65bd3d8f9a9bf326f145ba7fff823a76862ae1af
jaimeToca <jaime.toca.munoz@gmail.com>
2020-05-19 16:20:39 +0200

    tst

--- app/src/main/java/org/dhis2/MyAppGlideModule.java
+++ app/src/main/java/org/dhis2/MyAppGlideModule.java
@@ -4,4 +4,4 @@ import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
 @GlideModule
-public final class MyAppGlideModule extends AppGlideModule {}
+public final class MyAppGlideModule extends AppGlideModule {} 
