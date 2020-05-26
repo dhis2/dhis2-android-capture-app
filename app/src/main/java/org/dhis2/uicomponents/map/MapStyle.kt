7bda66582633d27da2648e9b6c1424775900e061
mmmateos <mmm8228@gmail.com>
2020-05-26 16:36:23 +0200

    codestyle

--- app/src/main/java/org/dhis2/uicomponents/map/MapStyle.kt
+++ app/src/main/java/org/dhis2/uicomponents/map/MapStyle.kt
@@ -2,9 +2,11 @@ package org.dhis2.uicomponents.map
 
 import android.graphics.drawable.Drawable
 
-data class MapStyle(var programColor: String,
-                    var teiColor: Int,
-                    var teiSymbolIcon: Drawable,
-                    var enrollmentColor: Int,
-                    var enrollmentSymbolIcon: Drawable,
-                    var programDarkColor: Int)
+data class MapStyle(
+    var programColor: String,
+    var teiColor: Int,
+    var teiSymbolIcon: Drawable,
+    var enrollmentColor: Int,
+    var enrollmentSymbolIcon: Drawable,
+    var programDarkColor: Int
+)
