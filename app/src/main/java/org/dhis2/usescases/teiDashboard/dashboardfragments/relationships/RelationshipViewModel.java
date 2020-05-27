8de1a73226cd68a9086cfd63b21d9012f076b720
jaimeToca <jaime.toca.munoz@gmail.com>
2020-05-27 11:24:56 +0200

    added geometry to relationshipViewModel

--- app/src/main/java/org/dhis2/usescases/teiDashboard/dashboardfragments/relationships/RelationshipViewModel.java
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/dashboardfragments/relationships/RelationshipViewModel.java
@@ -2,6 +2,7 @@ package org.dhis2.usescases.teiDashboard.dashboardfragments.relationships;
 
 import com.google.auto.value.AutoValue;
 
+import org.hisp.dhis.android.core.common.Geometry;
 import org.hisp.dhis.android.core.relationship.Relationship;
 import org.hisp.dhis.android.core.relationship.RelationshipType;
 import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValue;
@@ -15,6 +16,10 @@ public abstract class RelationshipViewModel {
 
     public abstract Relationship relationship();
 
+    public abstract Geometry fromGeometry();
+
+    public abstract Geometry toGeometry();
+
     public abstract RelationshipType relationshipType();
 
     public abstract RelationshipDirection relationshipDirection();
@@ -27,8 +32,10 @@ public abstract class RelationshipViewModel {
                                                RelationshipType relationshipType,
                                                RelationshipDirection relationshipDirection,
                                                String teiUid,
-                                               List<TrackedEntityAttributeValue> attributeValues) {
-        return new AutoValue_RelationshipViewModel(relationship, relationshipType, relationshipDirection, teiUid, attributeValues);
+                                               List<TrackedEntityAttributeValue> attributeValues,
+                                               Geometry fromGeometry,
+                                               Geometry toGeometry) {
+        return new AutoValue_RelationshipViewModel(relationship, relationshipType, relationshipDirection, teiUid, attributeValues, fromGeometry, toGeometry);
     }
 
 }
