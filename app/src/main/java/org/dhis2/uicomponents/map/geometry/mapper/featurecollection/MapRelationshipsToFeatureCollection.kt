e42ae7cbbd941ce4d1147e102d8483816ad18048
jaimeToca <jaime.toca.munoz@gmail.com>
2020-05-28 09:16:24 +0200

    added a boundixBox use case and link mapper to it

--- app/src/main/java/org/dhis2/uicomponents/map/geometry/mapper/featurecollection/MapRelationshipsToFeatureCollection.kt
+++ app/src/main/java/org/dhis2/uicomponents/map/geometry/mapper/featurecollection/MapRelationshipsToFeatureCollection.kt
@@ -5,17 +5,18 @@ import com.mapbox.geojson.Feature
 import com.mapbox.geojson.FeatureCollection
 import com.mapbox.geojson.LineString
 import com.mapbox.geojson.Point
-import org.dhis2.uicomponents.map.geometry.bound.BoundsGeometry
+import com.mapbox.mapboxsdk.geometry.LatLng
+import org.dhis2.uicomponents.map.geometry.bound.GetBoundingBox
 import org.dhis2.uicomponents.map.geometry.line.MapLineToFeature
 import org.dhis2.uicomponents.map.model.RelationshipMapModel
 import org.hisp.dhis.android.core.arch.helpers.GeometryHelper
 
 class MapRelationshipsToFeatureCollection(
     private val mapLineToFeature: MapLineToFeature,
-    private val bounds: BoundsGeometry
+    private val bounds: GetBoundingBox
 ) {
     fun map(relationships: List<RelationshipMapModel>): Pair<FeatureCollection, BoundingBox> {
-        val features = relationships.map {
+        val featuresList = relationships.map {
             val lineStartPoint = GeometryHelper.getPoint(it.from.geometry!!)
             val lineEndPoint = GeometryHelper.getPoint(it.To.geometry!!)
 
@@ -26,14 +27,34 @@ class MapRelationshipsToFeatureCollection(
 
             val firstPoint = Point.fromLngLat(lonLineStart, latLineStart)
             val secondPoint = Point.fromLngLat(lonLineEnd, latLineEnd)
-            Feature.fromGeometry(LineString.fromLngLats(listOf(firstPoint, secondPoint)))
+
+            Feature.fromGeometry(LineString.fromLngLats(listOf(firstPoint, secondPoint))).apply {
+                addStringProperty(RELATIONSHIP, it.relationshipTypeUid)
+                addBooleanProperty(BIDIRECTIONAL, it.bidirectional)
+                addStringProperty(FROM_TEI, it.from.teiUid)
+                addStringProperty(TO_TEI, it.To.teiUid)
+            }
         }
 
-        val featureCollection = FeatureCollection.fromFeatures(features)
-        return Pair(featureCollection,BoundingBox.fromLngLats(0.0,0.0,0.0,0.0))
+        val latLongList = getLngLatAsCoordinateList(featuresList)
+        val enclosingBoundingBox = bounds.getEnclosingBoundingBox(latLongList)
+
+        val featureCollection = FeatureCollection.fromFeatures(featuresList)
+        
+        return Pair(featureCollection, enclosingBoundingBox)
+    }
+
+    private fun getLngLatAsCoordinateList(features : List<Feature>) : List<LatLng> {
+        return features.map {
+            val point = it.geometry() as Point
+            LatLng(point.longitude(), point.latitude())
+        }
     }
 
     companion object {
-        const val TEI = "teiUid"
+        const val FROM_TEI = "fromTeiUid"
+        const val TO_TEI = "toTeiUid"
+        const val RELATIONSHIP = "relationshipTypeUid"
+        const val BIDIRECTIONAL = "bidirectional"
     }
 }
