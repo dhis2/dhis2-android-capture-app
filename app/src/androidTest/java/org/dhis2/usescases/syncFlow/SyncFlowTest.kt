5cc02ab839dc41fd3844b81f59c0b6365c03833e
Milagros Rodriguez <milagros@krowdy.co>
2020-06-04 19:27:19 -0500

    test: prepare sync tests

---
+++ app/src/androidTest/java/org/dhis2/usescases/syncFlow/SyncFlowTest.kt
@@ -0,0 +1,44 @@
+package org.dhis2.usescases.syncFlow
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.dhis2.usescases.BaseTest
+import org.dhis2.usescases.eventsWithoutRegistration.eventSummary.EventSummaryActivity
+import org.dhis2.usescases.teiDashboard.TeiDashboardMobileActivity
+import org.dhis2.usescases.teidashboard.TeiDashboardTest
+import org.dhis2.usescases.teidashboard.robot.teiDashboardRobot
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SyncFlowTest : BaseTest() {
+
+    @get:Rule
+    val ruleTei = ActivityTestRule(TeiDashboardMobileActivity::class.java, false, false)
+    @get:Rule
+    val ruleDataSet = ActivityTestRule(EventSummaryActivity::class.java, false, false)
+
+    @Test
+    fun shouldSuccessfullySyncAChangedEvent() {
+        /*
+        * launch tei
+        * change event
+        * sync
+        * */
+
+        //startTeiActivity()
+
+        teiDashboardRobot {
+            clickOnFab()
+            clickOnReferral()
+            clickOnFirstReferralEvent()
+            clickOnReferralOption()
+            clickOnReferralNextButton()
+            checkEventCreatedToastIsShown()
+            checkEventWasCreated(TeiDashboardTest.LAB_MONITORING)
+        }
+
+    }
+
+}
