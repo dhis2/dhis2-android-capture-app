be64d071e21998ea06d32ac1e9e6c073bae825bd
Pablo <ppajueloc@gmail.com>
2020-06-19 12:32:08 +0200

    ANDROAPP-3118

--- app/src/main/java/org/dhis2/uicomponents/map/managers/RelationshipMapManager.kt
+++ app/src/main/java/org/dhis2/uicomponents/map/managers/RelationshipMapManager.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.content.res.AppCompatResources
 import com.mapbox.geojson.BoundingBox
 import com.mapbox.geojson.FeatureCollection
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
+import com.mapbox.mapboxsdk.utils.BitmapUtils
 import org.dhis2.R
 import org.dhis2.uicomponents.map.layer.LayerType
 import org.hisp.dhis.android.core.common.FeatureType
@@ -12,6 +13,8 @@ class RelationshipMapManager : MapManager() {
 
     companion object {
         const val RELATIONSHIP_ICON = "RELATIONSHIP_ICON"
+        const val RELATIONSHIP_ARROW = "RELATIONSHIP_ARROW"
+        const val RELATIONSHIP_ARROW_BIDIRECTIONAL = "RELATIONSHIP_ARROW_BIDIRECTIONAL"
     }
 
     private lateinit var boundingBox: BoundingBox
@@ -36,11 +39,34 @@ class RelationshipMapManager : MapManager() {
 
     override fun loadDataForStyle() {
         style?.addImage(
+            RELATIONSHIP_ARROW,
+            BitmapUtils.getBitmapFromDrawable(
+                AppCompatResources.getDrawable(
+                    mapView.context,
+                    R.drawable.ic_arrowhead
+                )
+            )!!,
+            true
+        )
+        style?.addImage(
             RELATIONSHIP_ICON,
-            AppCompatResources.getDrawable(
-                mapView.context,
-                R.drawable.map_marker
-            )!!
+            BitmapUtils.getBitmapFromDrawable(
+                AppCompatResources.getDrawable(
+                    mapView.context,
+                    R.drawable.map_marker
+                )
+            )!!,
+            true
+        )
+        style?.addImage(
+            RELATIONSHIP_ARROW_BIDIRECTIONAL,
+            BitmapUtils.getBitmapFromDrawable(
+                AppCompatResources.getDrawable(
+                    mapView.context,
+                    R.drawable.ic_arrowhead_bidirectional
+                )
+            )!!,
+            true
         )
         setSource()
         setLayer()
