7d766914f75a051a141643ec3be063a96ba67629
jaimeToca <jaime.toca.munoz@gmail.com>
2020-06-29 17:52:57 +0200

    fix crash when coordinates are invalid

--- app/src/main/java/org/dhis2/uicomponents/map/geometry/polygon/MapPolygonPointToFeature.kt
+++ app/src/main/java/org/dhis2/uicomponents/map/geometry/polygon/MapPolygonPointToFeature.kt
@@ -6,7 +6,7 @@ import org.hisp.dhis.android.core.arch.helpers.GeometryHelper
 import org.hisp.dhis.android.core.common.Geometry
 
 class MapPolygonPointToFeature {
-    fun map(geometry: Geometry): Feature {
+    fun map(geometry: Geometry): Feature? {
         val sdkPolygon = GeometryHelper.getPolygon(geometry)
         val lat = sdkPolygon[0][0][1]
         val lon = sdkPolygon[0][0][0]
@@ -14,7 +14,7 @@ class MapPolygonPointToFeature {
         val point = if (lat >= -90 && lat <= 90 && lon >= -180 && lon <= 180) {
             Point.fromLngLat(lon, lat)
         } else {
-            throw IllegalArgumentException("latitude or longitude have wrong values")
+            null
         }
         return Feature.fromGeometry(point)
     }
