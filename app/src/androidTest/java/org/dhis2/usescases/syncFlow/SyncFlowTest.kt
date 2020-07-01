58f389222495f90575c8e70ab4117f11f9a1eb9a
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2020-07-01 00:37:34 -0500

    test: sync tei successful and error

--- app/src/androidTest/java/org/dhis2/usescases/syncFlow/SyncFlowTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/syncFlow/SyncFlowTest.kt
@@ -9,6 +9,7 @@ import org.dhis2.usescases.searchTrackEntity.SearchTEActivity
 import org.dhis2.usescases.searchte.searchTeiRobot
 import org.dhis2.usescases.teiDashboard.TeiDashboardMobileActivity
 import org.dhis2.usescases.teidashboard.TeiDashboardTest
+import org.dhis2.usescases.teidashboard.robot.eventRobot
 import org.dhis2.usescases.teidashboard.robot.teiDashboardRobot
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.API_SYSTEM_INFO_PATH
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.GET
@@ -29,49 +30,13 @@ class SyncFlowTest : BaseTest() {
 
     @Test
     fun shouldSuccessfullySyncAChangedTEI() {
-        val teiName = "Lynn"
-        val teiLastName = "Dunn"
+        val teiName =  "Scott" //"Lynn"
+        val teiLastName =  "Kelley"//"Dunn"
+        val tbVisit = 0
+
+        //instead of creating a new event just edit (skip) one
 
         mockWebServerRobot.addResponse(GET, API_SYSTEM_INFO_PATH, LoginTest.API_SYSTEM_INFO_RESPONSE_OK)
-        mockWebServerRobot.addResponse(POST, SYNC_TEI_PATH, API_SYNC_TEI_OK) // check uid tei test === uid test payload
-
-        setupCredentials()
-        prepareTBProgrammeIntentAndLaunchActivity(ruleSearch)
-
-        searchTeiRobot {
-            closeSearchForm()
-            Thread.sleep(4000)
-            clickOnTEI(teiName, teiLastName)
-        }
-
-        teiDashboardRobot {
-            clickOnFab()
-            clickOnScheduleNew()
-            clickOnFirstReferralEvent()
-            clickOnReferralNextButton()
-            checkEventCreatedToastIsShown()
-            checkEventWasCreated(TeiDashboardTest.LAB_MONITORING)
-            pressBack()
-        }
-
-        syncFlowRobot {
-            Thread.sleep(8000)
-            clickOnSyncTei(teiName, teiLastName)
-            clickOnSyncButton()
-        }
-
-        syncFlowRobot {
-            Thread.sleep(4000)
-           // checkSyncWasSuccessfully() //sync failed
-        }
-
-    }
-
-    @Test
-    fun shouldShowErrorWhenTEISyncFails() {
-        val teiName = "Lars"
-        val teiLastName = "Overland"
-
         mockWebServerRobot.addResponse(POST, SYNC_TEI_PATH, API_SYNC_TEI_OK)
 
         setupCredentials()
@@ -84,12 +49,65 @@ class SyncFlowTest : BaseTest() {
         }
 
         teiDashboardRobot {
-            clickOnFab()
+            /*clickOnFab()
             clickOnScheduleNew()
             clickOnFirstReferralEvent()
             clickOnReferralNextButton()
             checkEventCreatedToastIsShown()
-           // checkEventWasCreated(TeiDashboardTest.LAB_MONITORING)
+            checkEventWasCreated(TeiDashboardTest.LAB_MONITORING)*/
+            clickOnEventWithPosition(tbVisit)
+        }
+
+        eventRobot {
+            clickOnUpdate()
+            pressBack()
+        }
+
+        syncFlowRobot {
+            Thread.sleep(8000)
+            clickOnSyncTei(teiName, teiLastName)
+            clickOnSyncButton()
+        }
+
+        syncFlowRobot {
+            Thread.sleep(4000)
+            checkSyncWasSuccessfully() //sync failed
+        }
+
+    }
+
+    @Test
+    fun shouldShowErrorWhenTEISyncFails() {
+        val teiName = "Lars"
+        val teiLastName = "Overland"
+        val labMonitoring = 1
+        //instead of creating a new event I will just edit one
+
+        mockWebServerRobot.addResponse(POST, SYNC_TEI_PATH, API_SYNC_TEI_ERROR)
+
+        setupCredentials()
+        prepareTBProgrammeIntentAndLaunchActivity(ruleSearch)
+
+        searchTeiRobot {
+            closeSearchForm()
+            Thread.sleep(4000)
+            clickOnTEI(teiName, teiLastName)
+        }
+
+        teiDashboardRobot {
+            clickOnEventWithPosition(labMonitoring)
+            waitToDebounce(600)
+        }
+
+        eventRobot {
+            fillRadioButtonForm(4)
+            clickOnFormFabButton()
+            clickOnFinishAndComplete()
+            waitToDebounce(600)
+        }
+
+        teiDashboardRobot {
+            checkEventWasCreatedAndClosed(TeiDashboardTest.LAB_MONITORING, 1)
             pressBack()
         }
 
@@ -109,10 +127,6 @@ class SyncFlowTest : BaseTest() {
     companion object {
         const val SYNC_TEI_PATH = "/api/trackedEntityInstances?strategy=SYNC"
         const val API_SYNC_TEI_OK = "mocks/syncFlow/teiSync.json"
-
-        const val SYNC_TEI_INFO_PATH = "/api/system/info?fields=serverDate,dateFormat,version,contextPath,systemName"
+        const val API_SYNC_TEI_ERROR = "mocks/syncFlow/teiSyncError.json"
     }
 }
-
-
-//{"contextPath":"https://play.dhis2.org/android-current","userAgent":"com.dhis2.debug/1.1.2/2.2.0/Android_27","calendar":"iso8601","dateFormat":"yyyy-mm-dd","serverDate":"2020-06-25T16:23:50.756","lastAnalyticsTableSuccess":"2020-03-19T07:42:21.595","intervalSinceLastAnalyticsTableSuccess":"2359 h, 41 m, 29 s","lastAnalyticsTableRuntime":"5 m, 4 s","lastSystemMonitoringSuccess":"2019-03-26T17:07:15.418","version":"2.34.0","revision":"b9e5b0f","buildTime":"2020-04-24T08:58:43.000","jasperReportsVersion":"6.3.1","environmentVariable":"DHIS2_HOME","databaseInfo":{"spatialSupport":true},"encryption":false,"emailConfigured":false,"redisEnabled":false,"systemId":"eed3d451-4ff5-4193-b951-ffcc68954299","systemName":"DHIS 2 Demo - Sierra Leone","clusterHostname":"","isMetadataVersionEnabled":true,"metadataSyncEnabled":false}
