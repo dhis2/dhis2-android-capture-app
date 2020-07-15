49255ace29d0fd8a0774cb0d1c80a1fbded50589
Pablo <ppajueloc@gmail.com>
2020-07-15 10:56:40 +0200

    ANDROAPP-3180 ANDROAPP-3128 ANDROAPP-3030

--- app/src/main/java/org/dhis2/utils/ValidationUtils.kt
+++ app/src/main/java/org/dhis2/utils/ValidationUtils.kt
@@ -10,15 +10,19 @@ class ValidationUtils {
         @JvmStatic
         fun validate(valueType: ValueType, value: String?): String? {
             return value?.let {
-                when (valueType) {
-                    ValueType.INTEGER,
-                    ValueType.INTEGER_NEGATIVE,
-                    ValueType.INTEGER_POSITIVE,
-                    ValueType.INTEGER_ZERO_OR_POSITIVE,
-                    ValueType.PERCENTAGE -> String.format(Locale.US, "%.0f", it.toFloat())
-                    ValueType.UNIT_INTERVAL -> it.toFloat().toString()
-                    ValueType.NUMBER -> it.toDouble().toString()
-                    else -> it
+                if(value == "-"){
+                    "0"
+                }else {
+                    when (valueType) {
+                        ValueType.INTEGER,
+                        ValueType.INTEGER_NEGATIVE,
+                        ValueType.INTEGER_POSITIVE,
+                        ValueType.INTEGER_ZERO_OR_POSITIVE,
+                        ValueType.PERCENTAGE -> String.format(Locale.US, "%.0f", it.toFloat())
+                        ValueType.UNIT_INTERVAL -> it.toFloat().toString()
+                        ValueType.NUMBER -> it.toDouble().toString()
+                        else -> it
+                    }
                 }
             }
         }
