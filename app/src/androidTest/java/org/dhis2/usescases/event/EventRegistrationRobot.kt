7e28ccb066f2070cec8cf029934d74addc460d8b
Milagros Rodriguez <sharmynrodriguez@gmail.com>
2020-08-06 23:54:18 -0500

    test: prepare test event details

--- app/src/androidTest/java/org/dhis2/usescases/event/EventRegistrationRobot.kt
+++ app/src/androidTest/java/org/dhis2/usescases/event/EventRegistrationRobot.kt
@@ -1,2 +1,41 @@
 package org.dhis2.usescases.event
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import junit.framework.Assert.assertEquals
+import org.dhis2.R
+import org.dhis2.common.BaseRobot
+import org.hamcrest.Matchers.allOf
+
+fun eventRegistrationRobot(eventRegistrationRobot: EventRegistrationRobot.() -> Unit) {
+    EventRegistrationRobot().apply {
+        eventRegistrationRobot()
+    }
+}
+
+class EventRegistrationRobot : BaseRobot() {
+
+    fun openMenuMoreOptions() {
+        onView(withId(R.id.moreOptions)).perform(click())
+    }
+
+    fun clickOnDelete() {
+        onView(withText(R.string.delete)).perform(click())
+    }
+
+    fun clickOnDetails() {
+        onView(withText(R.string.event_overview)).perform(click())
+    }
+
+   fun checkEventDetails() {
+     //  onView(withId(R.id.ns_event_initial)).check(matches(withText("Lab monitoring")))
+      // onView(withId(R.id.completion)).check(matches(withText("100%")))
+       onView(withId(R.id.date_layout)).check(matches(hasDescendant(allOf(withId(R.id.date), withText("2/8/2020")))))
+       onView(withId(R.id.org_unit_layout)).check(matches(hasDescendant(allOf(withId(R.id.org_unit), withText("Ngelehun CHC")))))
+   }
+
+}
--- app/src/androidTest/java/org/dhis2/usescases/event/EventTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/event/EventTest.kt
@@ -1,9 +1,13 @@
 package org.dhis2.usescases.event
 
+import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import org.dhis2.usescases.BaseTest
+import org.dhis2.usescases.eventsWithoutRegistration.eventCapture.EventCaptureActivity
+import org.dhis2.usescases.searchTrackEntity.SearchTEActivity
 import org.dhis2.usescases.teiDashboard.TeiDashboardMobileActivity
+import org.dhis2.usescases.teiFlow.TeiFlowTest
 import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
@@ -13,7 +17,10 @@ import org.junit.runner.RunWith
 class EventTest: BaseTest() {
 
     @get:Rule
-    val rule = ActivityTestRule(TeiDashboardMobileActivity::class.java, false, false)
+    val rule = ActivityTestRule(EventCaptureActivity::class.java, false, false)
+
+    @get:Rule
+    val ruleTeiDashboard = ActivityTestRule(TeiDashboardMobileActivity::class.java, false, false)
 
     @Ignore
     @Test
@@ -30,7 +37,6 @@ class EventTest: BaseTest() {
 
     }
 
-    @Ignore
     @Test
     fun shouldShowEventDetailsWhenClickOnDetailsInsideSpecificEvent() {
 
@@ -42,6 +48,39 @@ class EventTest: BaseTest() {
          * check %, OU and date
          * */
 
+        // Drew Vaughn
+        // tb visit (3) 20/6/2019 ehxNi0D67uQ
+
+        //Berhane Yusef Lab monitoring 1/6/2020  tBAguooTVqg  ur1Edk5Oe2n CHECK  Ngelehun CHC 50%
+        // Zimmerman Lab monitoring 6/8/2020  tNbDeLvprys ur1Edk5Oe2n CHECK 100
+        // Jason Smith Sputum smear microscopy test  20/4/2020  E9CNT1WyBaX  ur1Edk5Oe2n CHECK 0
+        // Sputum smear microscopy test  2/8/2020  E9CNT1WyBaX  ur1Edk5Oe2n 100
+
+        prepareEventDetailsIntentAndLaunchActivity(rule)
+
+        eventRegistrationRobot {
+            openMenuMoreOptions()
+            Thread.sleep(7000)
+            clickOnDetails()
+            checkEventDetails()
+        }
+
+    }
+
+
+    private fun prepareEventDetailsIntentAndLaunchActivity(rule: ActivityTestRule<EventCaptureActivity>) {
+        Intent().apply {
+            putExtra(PROGRAM_UID, PROGRAM_TB)
+            putExtra(EVENT_UID, EVENT_DETAILS_UID)
+        }.also { rule.launchActivity(it) }
+    }
+
+    companion object {
+        const val EVENT_UID = "EVENT_UID"
+        const val PROGRAM_UID = "PROGRAM_UID"
+
+        const val PROGRAM_TB = "ur1Edk5Oe2n"
+        const val EVENT_DETAILS_UID =  "DSKhD4VgQPQ"//"E9CNT1WyBaX" //"tNbDeLvprys" //"tBAguooTVqg"
     }
 
 }
