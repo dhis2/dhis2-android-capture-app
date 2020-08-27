2d27d894669f4aba1473d7d18e8731d4e1676efd
Jorge Sánchez <xurxosf@gmail.com>
2020-08-27 12:55:39 +0200

    Remove old useC¡cases files

--- app/src/psi/java/org/dhis2/usecases/teiDashboard/dashboardsfragments/feedback/ProgramType.kt
+++
@@ -1,7 +0,0 @@
-package org.dhis2.usecases.teiDashboard.dashboardsfragments.feedback
-
-enum class ProgramType { HNQIS, RDQA }
-
-enum class RdqaFeedbackFilter { BY_INDICATOR, BY_TECHNICAL_AREA}
-
-enum class HnqisFeedbackFilter { ALL, CRITICAL, NON_CRITICAL }
