d735c564b856d994a40ac7bac793d1104a477b41
Pablo <ppajueloc@gmail.com>
2020-09-01 15:40:36 +0200

    fix: [ANDROAPP-3296] Fixed org. unit list crash when scrolling

--- app/src/main/java/org/dhis2/usescases/orgunitselector/TreeNodeCallback.kt
+++ app/src/main/java/org/dhis2/usescases/orgunitselector/TreeNodeCallback.kt
@@ -2,26 +2,13 @@ package org.dhis2.usescases.orgunitselector
 
 import androidx.recyclerview.widget.DiffUtil
 
-class TreeNodeCallback(
-    private val oldList: List<TreeNode>,
-    private val newList: List<TreeNode>
-) : DiffUtil.Callback() {
-    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition].id == newList[newItemPosition].id
+class TreeNodeCallback() : DiffUtil.ItemCallback<TreeNode>() {
+    override fun areItemsTheSame(oldItem: TreeNode, newItem: TreeNode): Boolean {
+        return oldItem.content.uid() == newItem.content.uid()
     }
 
-    override fun getOldListSize(): Int {
-        return oldList.size
+    override fun areContentsTheSame(oldItem: TreeNode, newItem: TreeNode): Boolean {
+        return oldItem == newItem
     }
 
-    override fun getNewListSize(): Int {
-        return newList.size
-    }
-
-    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition].isChecked == newList[newItemPosition].isChecked &&
-            oldList[oldItemPosition].isOpen == newList[newItemPosition].isOpen &&
-            oldList[oldItemPosition].hasChild == newList[newItemPosition].hasChild &&
-            oldList[oldItemPosition].id == newList[newItemPosition].id
-    }
 }
