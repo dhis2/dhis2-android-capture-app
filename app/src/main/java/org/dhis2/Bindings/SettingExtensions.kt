8adc256d413e56b27d62a53f224eb78ce29ef28d
Pablo <ppajueloc@gmail.com>
2020-09-04 09:53:28 +0200

    code review fix

--- app/src/main/java/org/dhis2/Bindings/SettingExtensions.kt
+++ app/src/main/java/org/dhis2/Bindings/SettingExtensions.kt
@@ -5,9 +5,6 @@ import org.dhis2.R
 import org.hisp.dhis.android.core.settings.DataSyncPeriod
 import org.hisp.dhis.android.core.settings.LimitScope
 import org.hisp.dhis.android.core.settings.MetadataSyncPeriod
-import org.hisp.dhis.android.core.settings.MetadataSyncPeriod.EVERY_12_HOURS
-import org.hisp.dhis.android.core.settings.MetadataSyncPeriod.EVERY_24_HOURS
-import org.hisp.dhis.android.core.settings.MetadataSyncPeriod.MANUAL
 
 const val EVERY_15_MIN = 15 * 60
 const val EVERY_30_MIN = 30 * 60
@@ -16,14 +13,15 @@ const val EVERY_6_HOUR = 6 * 60 * 60
 const val EVERY_12_HOUR = 12 * 60 * 60
 const val EVERY_24_HOUR = 24 * 60 * 60
 const val EVERY_7_DAYS = 7 * 24 * 60 * 60
+const val MANUAL = 0
 
 fun MetadataSyncPeriod.toSeconds(): Int {
     return when (this) {
         MetadataSyncPeriod.EVERY_HOUR -> EVERY_HOUR
-        EVERY_12_HOURS -> EVERY_12_HOUR
-        EVERY_24_HOURS -> EVERY_24_HOUR
+        MetadataSyncPeriod.EVERY_12_HOURS -> EVERY_12_HOUR
+        MetadataSyncPeriod.EVERY_24_HOURS -> EVERY_24_HOUR
         MetadataSyncPeriod.EVERY_7_DAYS -> EVERY_7_DAYS
-        MANUAL -> 0
+        MetadataSyncPeriod.MANUAL -> MANUAL
     }
 }
 
@@ -34,7 +32,7 @@ fun DataSyncPeriod.toSeconds(): Int {
         DataSyncPeriod.EVERY_6_HOURS -> EVERY_6_HOUR
         DataSyncPeriod.EVERY_12_HOURS -> EVERY_12_HOUR
         DataSyncPeriod.EVERY_24_HOURS -> EVERY_24_HOUR
-        DataSyncPeriod.MANUAL -> 0
+        DataSyncPeriod.MANUAL -> MANUAL
     }
 }
 
