989ddc53b5d7c4bafacc4f79a0e3d3c7863064d0
Jorge Sánchez <xurxosf@gmail.com>
2020-09-10 11:43:40 +0200

    Fix bugs showing FeedbackItem as leaf

--- app/src/psi/java/org/dhis2/usescases/teiDashboard/dashboardsfragments/feedback/FeedbackItemBinder.kt
+++ app/src/psi/java/org/dhis2/usescases/teiDashboard/dashboardsfragments/feedback/FeedbackItemBinder.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.core.widget.TextViewCompat
 import androidx.recyclerview.widget.RecyclerView
 import org.dhis2.R
@@ -22,46 +23,84 @@ class FeedbackItemBinder : TreeAdapterBinder(FeedbackItem::class.java) {
         holder: RecyclerView.ViewHolder,
         node: TreeNode<*>
     ) {
-        val branch = node as TreeNode.Node
-
         with(holder as ViewHolder) {
             val feedbackItem: FeedbackItem = node.content as FeedbackItem
-            name.text = (feedbackItem.name)
 
-            if (node.children.isNotEmpty() && node.children[0] is TreeNode.Node) {
-                TextViewCompat.setTextAppearance(
-                    name,
-                    R.style.TextAppearance_MaterialComponents_Body1
-                )
-            } else {
-                TextViewCompat.setTextAppearance(
-                    name,
-                    R.style.TextAppearance_MaterialComponents_Body2
-                )
-            }
-
-            if (feedbackItem.value == null || feedbackItem.value.data.isNullOrBlank()) {
-                value.visibility = View.GONE
-            } else {
-                value.text = feedbackItem.value.data
-
-                if (feedbackItem.value.color!= null){
-                    value.setBackgroundColor(Color.parseColor(feedbackItem.value.color))
-                } else {
-                    value.setBackgroundColor(Color.TRANSPARENT)
-                }
-
-                value.visibility = View.VISIBLE
-            }
-
-            if (node.expanded) {
-                arrow.setImageResource(R.drawable.ic_arrow_up)
-            } else {
-                arrow.setImageResource(R.drawable.ic_arrow_down)
-            }
-
-            arrow.visibility = if (node.children.isEmpty()) View.INVISIBLE else View.VISIBLE
+            renderColor(itemView, node)
+            renderName(name, feedbackItem, node)
+            renderValue(value, feedbackItem, feedbackItem.value)
+            renderArrow(arrow, node)
         }
+    }
+
+    private fun renderColor(
+        itemView: View,
+        node: TreeNode<*>
+    ) {
+        if (node is TreeNode.Leaf) {
+            itemView.setBackgroundColor(
+                ContextCompat.getColor(
+                    itemView.context,
+                    R.color.bg_gray_faf
+                )
+            )
+        } else {
+            itemView.setBackgroundColor(Color.TRANSPARENT)
+        }
+    }
+
+    private fun renderName(
+        nameView: TextView,
+        feedbackItem: FeedbackItem,
+        node: TreeNode<*>
+    ) {
+        if (node is TreeNode.Node && node.children.isNotEmpty() && node.children[0] is TreeNode.Node) {
+            TextViewCompat.setTextAppearance(
+                nameView,
+                R.style.TextAppearance_MaterialComponents_Body1
+            )
+        } else {
+            TextViewCompat.setTextAppearance(
+                nameView,
+                R.style.TextAppearance_MaterialComponents_Body2
+            )
+        }
+
+        nameView.text = (feedbackItem.name)
+    }
+
+    private fun renderValue(
+        valueView: TextView,
+        feedbackItem: FeedbackItem,
+        value: FeedbackItemValue?
+    ) {
+        if (feedbackItem.value == null || value?.data.isNullOrBlank()) {
+            valueView.visibility = View.GONE
+        } else {
+            valueView.text = value?.data
+
+            if (value?.color != null) {
+                valueView.setBackgroundColor(Color.parseColor(value.color))
+            } else {
+                valueView.setBackgroundColor(Color.TRANSPARENT)
+            }
+
+            valueView.visibility = View.VISIBLE
+        }
+    }
+
+    private fun renderArrow(
+        arrow: ImageView,
+        node: TreeNode<*>
+    ) {
+        if (node is TreeNode.Node && node.expanded) {
+            arrow.setImageResource(R.drawable.ic_arrow_up)
+        } else {
+            arrow.setImageResource(R.drawable.ic_arrow_down)
+        }
+
+        arrow.visibility =
+            if (node is TreeNode.Node && node.children.isNotEmpty()) View.VISIBLE else View.INVISIBLE
     }
 
     internal class ViewHolder(rootView: View) : RecyclerView.ViewHolder(rootView) {
