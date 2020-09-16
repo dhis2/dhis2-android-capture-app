08533065fa8527e5e1ecea4ee22a5ab5a6d2c8c9
Pablo <ppajueloc@gmail.com>
2020-09-16 17:29:07 +0200

    set empty string as default value

--- app/src/main/java/org/dhis2/usescases/teiDashboard/DashboardRepositoryImpl.java
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/DashboardRepositoryImpl.java
@@ -254,7 +254,7 @@ public class DashboardRepositoryImpl implements DashboardRepository {
                                         TrackedEntityAttributeValue.builder()
                                                 .trackedEntityAttribute(programAttribute.trackedEntityAttribute().uid())
                                                 .trackedEntityInstance(teiUid)
-                                                .value("-")
+                                                .value("")
                                                 .build()
                                 );
                             }
