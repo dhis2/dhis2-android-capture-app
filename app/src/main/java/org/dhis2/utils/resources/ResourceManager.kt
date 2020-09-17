d2210e012195104c6d698bece7948b5f864c8649
Pablo <ppajueloc@gmail.com>
2020-09-17 08:31:13 +0200

    update sdk

--- app/src/main/java/org/dhis2/utils/resources/ResourceManager.kt
+++ app/src/main/java/org/dhis2/utils/resources/ResourceManager.kt
@@ -2,6 +2,7 @@ package org.dhis2.utils.resources
 
 import android.content.Context
 import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import org.dhis2.R
 
 class ResourceManager(val context: Context) {
@@ -11,11 +12,21 @@ class ResourceManager(val context: Context) {
             val iconName = if (icon.startsWith("ic_")) icon else "ic_$icon"
             var iconResource =
                 context.resources.getIdentifier(iconName, "drawable", context.packageName)
-            if (iconResource != 0 && iconResource != -1) {
+            if (iconResource != 0 && iconResource != -1 && drawableExists(iconResource)
+            ) {
                 iconResource
             } else {
                 R.drawable.ic_default_icon
             }
         } ?: defaultResource
+    }
+
+    private fun drawableExists(iconResource: Int): Boolean {
+        return try {
+            ContextCompat.getDrawable(context, iconResource)
+            true
+        } catch (e: Exception) {
+            false
+        }
     }
 }
