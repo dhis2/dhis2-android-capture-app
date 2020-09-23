18b42d8bb3f7da247f3e348b14ade6a8ecb87890
Andres Miguel <andres@xceed.me>
2020-09-23 11:31:07 +0200

    test: [ANDROAPP-3321] Flow UI test

--- app/src/androidTest/java/org/dhis2/AppTest.kt
+++ app/src/androidTest/java/org/dhis2/AppTest.kt
@@ -1,5 +1,7 @@
 package org.dhis2
 
+import androidx.test.espresso.idling.concurrent.IdlingThreadPoolExecutor
+import androidx.work.Configuration
 import org.dhis2.common.preferences.PreferencesTestingModule
 import org.dhis2.data.schedulers.SchedulerModule
 import org.dhis2.data.schedulers.SchedulersProviderImpl
@@ -8,9 +10,11 @@ import org.dhis2.data.service.workManager.WorkManagerModule
 import org.dhis2.data.user.UserModule
 import org.dhis2.utils.analytics.AnalyticsModule
 import org.hisp.dhis.android.core.D2Manager
-import org.hisp.dhis.android.core.arch.api.internal.ServerURLWrapper
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.TimeUnit
 
-class AppTest : App() {
+class AppTest : App(), Configuration.Provider {
 
     @Override
     override fun onCreate() {
@@ -54,5 +58,22 @@ class AppTest : App() {
     companion object {
         const val DB_TO_IMPORT = "127-0-0-1-8080_android_unencrypted.db"
         const val USERNAME = "android"
+        const val IDLING_THREAD = "IDLING_WORK_MANAGER"
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        // This value is the same as the core pool size for AsyncTask#THREAD_POOL_EXECUTOR.
+        val nThreads =
+            2.coerceAtLeast((Runtime.getRuntime().availableProcessors() - 1).coerceAtMost(4))
+        return Configuration.Builder()
+            .setExecutor(
+                IdlingThreadPoolExecutor(
+                    IDLING_THREAD,
+                    nThreads, nThreads,
+                    0L, TimeUnit.MILLISECONDS,
+                    LinkedBlockingQueue<Runnable>(),
+                    Executors.defaultThreadFactory()
+                )
+            ).build()
     }
 }
