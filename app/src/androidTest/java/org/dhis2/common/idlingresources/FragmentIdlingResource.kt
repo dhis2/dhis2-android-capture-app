74bf2a11b2ef96d063c38d63cd91e84d622a0967
jaimeToca <jaime.toca.munoz@gmail.com>
2020-09-24 18:39:11 +0200

    added debounce fragment transition

---
+++ app/src/androidTest/java/org/dhis2/common/idlingresources/FragmentIdlingResource.kt
@@ -0,0 +1,27 @@
+package org.dhis2.common.idlingresources
+
+import androidx.fragment.app.FragmentManager
+import androidx.test.espresso.IdlingResource
+
+class FragmentIdlingResource(
+    private val manager: FragmentManager,
+    private val tag: String) : IdlingResource {
+
+    private var resourceCallback: IdlingResource.ResourceCallback? = null
+
+    override fun getName() = "fragment idling resource"
+    override fun registerIdleTransitionCallback(
+        callback: IdlingResource.ResourceCallback?
+    ) {
+        resourceCallback = callback
+    }
+
+    override fun isIdleNow(): Boolean {
+        val fragment = manager.findFragmentByTag(tag)
+        val idle = (fragment == null)
+        if (idle) {
+            resourceCallback?.onTransitionToIdle()
+        }
+        return idle
+    }
+}
