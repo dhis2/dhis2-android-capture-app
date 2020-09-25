babe3ed9c22221352e058b602cd861af45466d8b
Andres Miguel <andres@xceed.me>
2020-09-25 11:05:37 +0200

    Idling SyncGranularWorker.kt

--- app/src/androidTest/java/org/dhis2/AppTest.kt
+++ app/src/androidTest/java/org/dhis2/AppTest.kt
@@ -1,7 +1,5 @@
 package org.dhis2
 
-import androidx.test.espresso.idling.concurrent.IdlingThreadPoolExecutor
-import androidx.work.Configuration
 import org.dhis2.common.preferences.PreferencesTestingModule
 import org.dhis2.data.schedulers.SchedulerModule
 import org.dhis2.data.schedulers.SchedulersProviderImpl
@@ -10,11 +8,8 @@ import org.dhis2.data.service.workManager.WorkManagerModule
 import org.dhis2.data.user.UserModule
 import org.dhis2.utils.analytics.AnalyticsModule
 import org.hisp.dhis.android.core.D2Manager
-import java.util.concurrent.Executors
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.TimeUnit
 
-class AppTest : App(), Configuration.Provider {
+class AppTest : App() {
 
     @Override
     override fun onCreate() {
@@ -58,22 +53,5 @@ class AppTest : App(), Configuration.Provider {
     companion object {
         const val DB_TO_IMPORT = "127-0-0-1-8080_android_unencrypted.db"
         const val USERNAME = "android"
-        const val IDLING_THREAD = "IDLING_WORK_MANAGER"
-    }
-
-    override fun getWorkManagerConfiguration(): Configuration {
-        // This value is the same as the core pool size for AsyncTask#THREAD_POOL_EXECUTOR.
-        val nThreads =
-            2.coerceAtLeast((Runtime.getRuntime().availableProcessors() - 1).coerceAtMost(4))
-        return Configuration.Builder()
-            .setExecutor(
-                IdlingThreadPoolExecutor(
-                    IDLING_THREAD,
-                    nThreads, nThreads,
-                    0L, TimeUnit.MILLISECONDS,
-                    LinkedBlockingQueue<Runnable>(),
-                    Executors.defaultThreadFactory()
-                )
-            ).build()
     }
 }
