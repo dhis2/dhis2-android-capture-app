6d607ea8dfaaec981cffb7a7756e92f93c42036d
mmmateos <mmm8228@gmail.com>
2020-10-14 13:57:05 +0200

    remove d2 from holder

--- app/src/main/java/org/dhis2/utils/filters/OrgUnitFilterHolder.java
+++ app/src/main/java/org/dhis2/utils/filters/OrgUnitFilterHolder.java
@@ -9,13 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.databinding.ObservableField;
 
-import org.dhis2.App;
 import org.dhis2.R;
 import org.dhis2.data.filter.FilterPresenter;
 import org.dhis2.databinding.ItemFilterOrgUnitBinding;
 import org.dhis2.utils.filters.ou.OUFilterAdapter;
 import org.dhis2.utils.filters.sorting.SortingItem;
-import org.hisp.dhis.android.core.D2;
 import org.hisp.dhis.android.core.organisationunit.OrganisationUnit;
 
 import java.util.ArrayList;
@@ -44,8 +42,6 @@ class OrgUnitFilterHolder extends FilterHolder {
     }
 
     private void setUpAdapter() {
-        D2 d2 = ((App) itemView.getContext().getApplicationContext()).serverComponent().userManager().getD2();
-
         ItemFilterOrgUnitBinding localBinding = (ItemFilterOrgUnitBinding) binding;
 
         OUFilterAdapter ouFilterAdapter = new OUFilterAdapter();
@@ -61,9 +57,8 @@ class OrgUnitFilterHolder extends FilterHolder {
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
                 if (charSequence.length() > 3) {
-                    List<OrganisationUnit> orgUnits = d2.organisationUnitModule().organisationUnits()
-                            .byDisplayName().like("%" + charSequence + "%")
-                            .blockingGet();
+                    List<OrganisationUnit> orgUnits = filterPresenter.getOrgUnitsByName(charSequence.toString());
+
                     currentOrgUnit = !orgUnits.isEmpty() ? orgUnits.get(0) : null;
                     List<String> orgUnitsNames = new ArrayList<>();
                     for (OrganisationUnit orgUnit: orgUnits) {
