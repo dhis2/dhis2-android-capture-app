3717ebef301b4b10cd1f776b40aa4e809a9edf20
andresmr <personal@andresmr.com>
2020-11-24 08:03:05 +0100

    Use FormView in EventCaptureFormFragment

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/eventCaptureFragment/EventCaptureFormFragment.java
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/eventCaptureFragment/EventCaptureFormFragment.java
@@ -1,28 +1,19 @@
 package org.dhis2.usescases.eventsWithoutRegistration.eventCapture.eventCaptureFragment;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import org.dhis2.Bindings.ViewExtensionsKt;
 import org.dhis2.R;
-import org.dhis2.data.forms.dataentry.DataEntryAdapter;
-import org.dhis2.data.forms.dataentry.DataEntryHeaderHelper;
 import org.dhis2.data.forms.dataentry.fields.FieldViewModel;
-import org.dhis2.data.forms.dataentry.fields.RowAction;
-import org.dhis2.data.tuples.Trio;
 import org.dhis2.databinding.SectionSelectorFragmentBinding;
 import org.dhis2.usescases.eventsWithoutRegistration.eventCapture.EventCaptureActivity;
 import org.dhis2.usescases.general.FragmentGlobalAbstract;
@@ -33,11 +24,6 @@ import java.util.List;
 
 import javax.inject.Inject;
 
-import io.reactivex.processors.FlowableProcessor;
-import io.reactivex.processors.PublishProcessor;
-
-import static android.text.TextUtils.isEmpty;
-
 public class EventCaptureFormFragment extends FragmentGlobalAbstract implements EventCaptureFormView {
 
     @Inject
@@ -45,10 +31,6 @@ public class EventCaptureFormFragment extends FragmentGlobalAbstract implements 
 
     private EventCaptureActivity activity;
     private SectionSelectorFragmentBinding binding;
-    private DataEntryAdapter dataEntryAdapter;
-    private DataEntryHeaderHelper dataEntryHeaderHelper;
-    private FlowableProcessor<RowAction> flowableProcessor;
-    private FlowableProcessor<Trio<String, String, Integer>> flowableOptions;
 
     public static EventCaptureFormFragment newInstance(String eventUid) {
         EventCaptureFormFragment fragment = new EventCaptureFormFragment();
@@ -75,8 +57,6 @@ public class EventCaptureFormFragment extends FragmentGlobalAbstract implements 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.section_selector_fragment, container, false);
         binding.setPresenter(activity.getPresenter());
-        this.flowableProcessor = PublishProcessor.create();
-        this.flowableOptions = PublishProcessor.create();
 
         binding.actionButton.setOnClickListener(view -> {
             view.requestFocus();
@@ -84,8 +64,7 @@ public class EventCaptureFormFragment extends FragmentGlobalAbstract implements 
             presenter.onActionButtonClick();
         });
 
-        dataEntryHeaderHelper = new DataEntryHeaderHelper(binding.headerContainer, binding.formRecycler);
-        dataEntryHeaderHelper.observeHeaderChanges(this);
+        binding.formView.init(this);
 
         presenter.init();
 
@@ -110,80 +89,6 @@ public class EventCaptureFormFragment extends FragmentGlobalAbstract implements 
 
     @Override
     public void showFields(@NonNull List<FieldViewModel> updates, @NonNull String lastFocusItem) {
-
-        if (!isEmpty(lastFocusItem)) {
-            dataEntryAdapter.setLastFocusItem(lastFocusItem);
-        }
-
-        if (dataEntryAdapter == null) {
-            createDataEntry();
-        }
-
-        if (dataEntryAdapter == null) {
-            createDataEntry();
-        }
-
-        dataEntryAdapter.swap(updates, () -> dataEntryHeaderHelper.onItemsUpdatedCallback());
-    }
-
-    private void createDataEntry() {
-
-        dataEntryAdapter = new DataEntryAdapter();
-
-        GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
-        layoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
-            @Override
-            public int getSpanSize(int position) {
-                return dataEntryAdapter.getItemSpan(position);
-            }
-        });
-
-        binding.formRecycler.setLayoutManager(layoutManager);
-        binding.formRecycler.setAdapter(dataEntryAdapter);
-        binding.formRecycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-                if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
-                    dataEntryAdapter.setLastFocusItem(null);
-                    InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(recyclerView.getWindowToken(), 0);
-                    binding.dummyFocusView.requestFocus();
-                }
-            }
-
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                dataEntryHeaderHelper.checkSectionHeader(recyclerView);
-            }
-        });
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            binding.formRecycler.setOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
-                checkLastItem();
-            });
-        } else {
-            binding.formRecycler.setOnScrollListener(new RecyclerView.OnScrollListener() {
-                @Override
-                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                    checkLastItem();
-                }
-            });
-        }
-    }
-
-    private void checkLastItem() {
-        GridLayoutManager layoutManager = (GridLayoutManager) binding.formRecycler.getLayoutManager();
-        int lastVisiblePosition = layoutManager.findLastVisibleItemPosition();
-        boolean shouldShowFab = lastVisiblePosition != -1 && (
-                lastVisiblePosition == dataEntryAdapter.getItemCount() - 1 ||
-                        dataEntryAdapter.getItemViewType(lastVisiblePosition) == 17);
-        animateFabButton(shouldShowFab);
-    }
-
-    private void animateFabButton(boolean sectionIsVisible) {
-        binding.actionButton.animate()
-                .translationX(sectionIsVisible ? 0 : 1000)
-                .setDuration(500)
-                .start();
+        binding.formView.render(updates);
     }
 }
