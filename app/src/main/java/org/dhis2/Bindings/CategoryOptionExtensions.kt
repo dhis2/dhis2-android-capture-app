4e797e02930381e3141e01a3f5801317f6e5ab6e
mmmateos <mmm8228@gmail.com>
2020-11-26 15:28:12 +0100

    fix: [ANDROAPP-1153] category options restricted by orgUnits

--- app/src/main/java/org/dhis2/Bindings/CategoryOptionExtensions.kt
+++ app/src/main/java/org/dhis2/Bindings/CategoryOptionExtensions.kt
@@ -1,11 +1,21 @@
 package org.dhis2.Bindings
 
+import org.hisp.dhis.android.core.arch.helpers.UidsHelper
 import java.util.Date
 import org.hisp.dhis.android.core.category.CategoryOption
+import org.hisp.dhis.android.core.common.ObjectWithUid
 
 fun CategoryOption.inDateRange(date: Date?): Boolean {
     return date?.let {
         (startDate() == null || date.after(startDate())) &&
             (endDate() == null || date.before(endDate()))
+    } ?: true
+}
+
+fun CategoryOption.inOrgUnit(orgUnitUid: String?): Boolean {
+    return orgUnitUid?.let {
+        organisationUnits()?.takeIf { it.isNotEmpty()}?.let{
+            orgUnitUid in UidsHelper.getUidsList(it)
+        } ?: true
     } ?: true
 }
