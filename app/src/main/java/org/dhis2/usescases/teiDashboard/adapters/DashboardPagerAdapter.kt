d3a79bdf48a25370d00e089b06a333390378e19b
Pablo <ppajueloc@gmail.com>
2020-11-27 09:01:29 +0100

    add constants for positions

--- app/src/main/java/org/dhis2/usescases/teiDashboard/adapters/DashboardPagerAdapter.kt
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/adapters/DashboardPagerAdapter.kt
@@ -75,16 +75,44 @@ class DashboardPagerAdapter(
 
     fun getNavigationPagePosition(navigationId: Int): Int {
         return when (navigationId) {
-            R.id.navigation_details -> if (isLandscape()) -1 else 0
-            R.id.navigation_events -> -1
-            R.id.navigation_analytics -> if (isLandscape()) 0 else 1
-            R.id.navigation_relationships -> if (isLandscape()) 1 else 2
-            R.id.navigation_notes -> if (isLandscape()) 2 else 3
-            else -> -1
+            R.id.navigation_details ->
+                if (isLandscape()) {
+                    NO_POSITION
+                } else {
+                    DETAILS_PORTRAIT_POSITION
+                }
+            R.id.navigation_events -> NO_POSITION
+            R.id.navigation_analytics ->
+                if (isLandscape()) {
+                    ANALYTICS_LANDSCAPE_POSITION
+                } else {
+                    ANALYTICS_PORTRAIT_POSITION
+                }
+            R.id.navigation_relationships ->
+                if (isLandscape()) {
+                    RELATIONSHIPS_LANDSCAPE_POSITION
+                } else {
+                    RELATIONSHIPS_PORTRAIT_POSITION
+                }
+            R.id.navigation_notes ->
+                if (isLandscape()) {
+                    NOTES_LANDSCAPE_POSITION
+                } else {
+                    NOTES_PORTRAIT_POSITION
+                }
+            else -> NO_POSITION
         }
     }
 
     companion object {
+        const val NO_POSITION = -1
+        const val DETAILS_PORTRAIT_POSITION = 0
+        const val ANALYTICS_LANDSCAPE_POSITION = 0
+        const val ANALYTICS_PORTRAIT_POSITION = 1
+        const val RELATIONSHIPS_LANDSCAPE_POSITION = 1
+        const val RELATIONSHIPS_PORTRAIT_POSITION = 2
+        const val NOTES_LANDSCAPE_POSITION = 2
+        const val NOTES_PORTRAIT_POSITION = 3
         const val PORTRAIT_DASHBOARD_SIZE = 4
         const val LANDSCAPE_DASHBOARD_SIZE = 3
         const val NO_FRAGMENT_DUE_TO_NO_PROGRAM_SELECTED = 0
