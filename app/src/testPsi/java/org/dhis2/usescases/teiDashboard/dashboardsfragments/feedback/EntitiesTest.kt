f354952938e07f3439364fb1027c48bbfaf7a940
Jorge Sánchez <xurxosf@gmail.com>
2020-11-30 11:51:47 +0100

    Order by FeedbackOrder in by technical area tab

--- app/src/testPsi/java/org/dhis2/usescases/teiDashboard/dashboardsfragments/feedback/EntitiesTest.kt
+++ app/src/testPsi/java/org/dhis2/usescases/teiDashboard/dashboardsfragments/feedback/EntitiesTest.kt
@@ -31,23 +31,28 @@ class FeedbackUtilsTest {
             FeedbackOrder("1.2.1.2"), FeedbackOrder("1.2.1.1")
         )
 
-        val orderedValues  = feedbackOrders.sorted()
+        val orderedValues = feedbackOrders.sorted()
 
-        Assert.assertEquals(listOf(
-            FeedbackOrder("1.2.1.1"),FeedbackOrder("1.2.1.2")
-        ), orderedValues)
+        Assert.assertEquals(
+            listOf(
+                FeedbackOrder("1.2.1.1"), FeedbackOrder("1.2.1.2")
+            ), orderedValues
+        )
     }
+
     @Test
     fun `FeedbackOrder list Should sort with expected order with items for the distinct level`() {
         val feedbackOrders = listOf(
             FeedbackOrder("1.2.1.1"), FeedbackOrder("1.2.1")
         )
 
-        val orderedValues  = feedbackOrders.sorted()
+        val orderedValues = feedbackOrders.sorted()
 
-        Assert.assertEquals(listOf(
-            FeedbackOrder("1.2.1"),FeedbackOrder("1.2.1.1")
-        ), orderedValues)
+        Assert.assertEquals(
+            listOf(
+                FeedbackOrder("1.2.1"), FeedbackOrder("1.2.1.1")
+            ), orderedValues
+        )
     }
 
     @Test
@@ -55,25 +60,44 @@ class FeedbackUtilsTest {
         val feedbackOrders = listOf(
             FeedbackOrder("20"), FeedbackOrder("20.1"),
             FeedbackOrder("1"), FeedbackOrder("1.1"),
-            FeedbackOrder("1.1.1"),  FeedbackOrder("2.1"),
-            FeedbackOrder("1.2"),FeedbackOrder("1.2.1"),
+            FeedbackOrder("1.1.1"), FeedbackOrder("2.1"),
+            FeedbackOrder("1.2"), FeedbackOrder("1.2.1"),
             FeedbackOrder("2"), FeedbackOrder("1.1.2"),
             FeedbackOrder("10"), FeedbackOrder("10.1"),
-            FeedbackOrder("3"), FeedbackOrder("3.1") ,
+            FeedbackOrder("3"), FeedbackOrder("3.1"),
             FeedbackOrder("2.1.1"), FeedbackOrder("2.1.2")
         )
 
-        val orderedValues  = feedbackOrders.sorted()
+        val orderedValues = feedbackOrders.sorted()
 
-        Assert.assertEquals(listOf(
-            FeedbackOrder("1"), FeedbackOrder("1.1"),
-            FeedbackOrder("1.1.1"),  FeedbackOrder("1.1.2"),
-            FeedbackOrder("1.2"),FeedbackOrder("1.2.1"),
-            FeedbackOrder("2"), FeedbackOrder("2.1"),
-            FeedbackOrder("2.1.1"), FeedbackOrder("2.1.2"),
-            FeedbackOrder("3"), FeedbackOrder("3.1"),
-            FeedbackOrder("10"), FeedbackOrder("10.1"),
-            FeedbackOrder("20"), FeedbackOrder("20.1")
-        ), orderedValues)
+        Assert.assertEquals(
+            listOf(
+                FeedbackOrder("1"), FeedbackOrder("1.1"),
+                FeedbackOrder("1.1.1"), FeedbackOrder("1.1.2"),
+                FeedbackOrder("1.2"), FeedbackOrder("1.2.1"),
+                FeedbackOrder("2"), FeedbackOrder("2.1"),
+                FeedbackOrder("2.1.1"), FeedbackOrder("2.1.2"),
+                FeedbackOrder("3"), FeedbackOrder("3.1"),
+                FeedbackOrder("10"), FeedbackOrder("10.1"),
+                FeedbackOrder("20"), FeedbackOrder("20.1")
+            ), orderedValues
+        )
+    }
+
+    @Test
+    fun `FeedbackOrder list Should sort with expected order with items for level 0`() {
+        val feedbackOrders = listOf(
+            FeedbackOrder("1"), FeedbackOrder("7"), FeedbackOrder("4"),
+            FeedbackOrder("5"), FeedbackOrder("6")
+        )
+
+        val orderedValues = feedbackOrders.sortedBy { it }
+
+        Assert.assertEquals(
+            listOf(
+                FeedbackOrder("1"), FeedbackOrder("4"), FeedbackOrder("5")
+                , FeedbackOrder("6"), FeedbackOrder("7")
+            ), orderedValues
+        )
     }
 }
