15168822c3b49c196e53d87e7fd5a45a5ff7e536
jaimeToca <jaime.toca.munoz@gmail.com>
2020-12-11 12:27:43 +0100

    fix klint

--- app/src/main/java/org/dhis2/data/forms/dataentry/fields/visualOptionSet/VisualOptionSetView.kt
+++ app/src/main/java/org/dhis2/data/forms/dataentry/fields/visualOptionSet/VisualOptionSetView.kt
@@ -8,8 +8,8 @@ import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.core.view.updatePadding
 import org.dhis2.Bindings.dp
-import org.dhis2.utils.customviews.FieldLayout
 import org.dhis2.data.forms.dataentry.fields.image.ImageCustomView
+import org.dhis2.utils.customviews.FieldLayout
 import org.hisp.dhis.android.core.option.Option
 
 class VisualOptionSetView @JvmOverloads constructor(
