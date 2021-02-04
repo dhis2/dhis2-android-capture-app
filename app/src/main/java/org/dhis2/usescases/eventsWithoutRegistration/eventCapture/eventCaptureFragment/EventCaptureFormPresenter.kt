23dab87f2aed3b3e8c6a4926ab216d1f440fb447
mmmateos <mmm8228@gmail.com>
2021-02-04 13:38:49 +0100

    fix: [ANDROAPP-3642] keyboard not closing after saving value

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/eventCaptureFragment/EventCaptureFormPresenter.kt
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/eventCaptureFragment/EventCaptureFormPresenter.kt
@@ -37,6 +37,10 @@ class EventCaptureFormPresenter(
 
                     when (rowAction.type) {
                         ActionType.ON_SAVE -> {
+                            if (rowAction.id == focusedItem?.id &&
+                                focusedItem?.type == ActionType.ON_FOCUS) {
+                                focusedItem = null
+                            }
                             if (rowAction.error != null) {
                                 if (itemsWithError.find { it.id == rowAction.id } == null) {
                                     itemsWithError.add(rowAction)
