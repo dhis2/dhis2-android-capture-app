11db0b73a28321dd67616419abebee335ff26c56
jaimeToca <jaime.toca.munoz@gmail.com>
2021-02-25 15:32:46 +0100

    fix: Date Range for filtering

--- app/src/main/java/org/dhis2/utils/DateUtils.java
+++ app/src/main/java/org/dhis2/utils/DateUtils.java
@@ -56,7 +56,7 @@ public class DateUtils {
                 return new Date[]{getFirstDayOfWeek(date), getLastDayOfWeek(date)};
             case DAILY:
             default:
-                return new Date[]{getDate(date), getNextDate(date)};
+                return new Date[]{getDate(date), getDate(date)};
         }
     }
 
