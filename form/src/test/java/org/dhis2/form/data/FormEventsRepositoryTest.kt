6bd8a5d3d2028dc71aa685679f6ead8d613eaa31
andresmr <personal@andresmr.com>
2021-04-30 11:33:22 +0200

    [ANDROAPP-3890] FormEventsRepositoryTest.kt

--- form/src/test/java/org/dhis2/form/data/FormEventsRepositoryTest.kt
+++ form/src/test/java/org/dhis2/form/data/FormEventsRepositoryTest.kt
@@ -1,10 +1,31 @@
 package org.dhis2.form.data
 
+import com.nhaarman.mockitokotlin2.mock
+import org.hamcrest.MatcherAssert.assertThat
 import org.hisp.dhis.android.core.D2
+import org.junit.Before
+import org.junit.Test
 import org.mockito.Mockito
 
 class FormEventsRepositoryTest {
 
     private val d2: D2 = Mockito.mock(D2::class.java, Mockito.RETURNS_DEEP_STUBS)
     private val recordUid = "ABCD"
+    private val formRepositoryImpl: FormRepositoryImpl = mock()
+
+    private lateinit var formEventsRepository: FormEventsRepository
+
+    @Before
+    fun setUp() {
+        formEventsRepository = FormEventsRepository(
+            formRepositoryImpl,
+            d2,
+            recordUid
+        )
+    }
+
+    /*@Test
+    fun `test`() {
+        assertThat(formEventsRepository.s)
+    }*/
 }
