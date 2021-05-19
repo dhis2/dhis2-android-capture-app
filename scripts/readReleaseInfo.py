ebb76a23093317ce36346faaaec717a372210a2f
FerdyRod <ferdyrodriguez@gmail.com>
2021-05-19 16:58:56 +0200

    updates script

--- scripts/readReleaseInfo.py
+++ scripts/readReleaseInfo.py
@@ -10,9 +10,9 @@ with open(RELEASE_INFO_PATH, 'r') as file:
         y = line.strip().split(":")
         release_versions[y[0]] = str(y[1])
 
-name = f"envman add --key NAME_RELEASE --value {release_versions.get('vName')}"
-branch = f"envman add --key NAME_RELEASE --value {release_versions.get('vBranch')}"
-sdk_version = f"envman add --key NAME_RELEASE --value {release_versions.get('sdkVersion')}"
+name = "envman add --key NAME_RELEASE --value {}".format(release_versions.get('vName'))
+branch = "envman add --key NAME_RELEASE --value {}".format(release_versions.get('vBranch'))
+sdk_version = "envman add --key NAME_RELEASE --value {}".format(release_versions.get('sdkVersion'))
 
 os.system(name)
 os.system(branch)
