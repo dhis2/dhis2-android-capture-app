65bf73dfc895a5da9fac8c6412b046f9b9f23a21
jaimeToca <jaime.toca.munoz@gmail.com>
2021-06-09 17:50:11 +0200

    add needed libraries for testing

---
+++ form/src/test/java/org/dhis2/form/data/FormViewModelTest.kt
@@ -0,0 +1,16 @@
+package org.dhis2.form.data
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Before
+import org.junit.Rule
+
+class FormViewModelTest {
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setup() {
+
+    }
+}
