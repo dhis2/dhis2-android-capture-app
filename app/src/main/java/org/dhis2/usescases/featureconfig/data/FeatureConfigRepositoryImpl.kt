d019a080bc84e5e1b20aee40706bdebb727c63d4
andresmr <personal@andresmr.com>
2021-06-16 09:29:27 +0200

    [ANDROAPP-4048] expose if feature is enabled in repository

--- app/src/main/java/org/dhis2/usescases/featureconfig/data/FeatureConfigRepositoryImpl.kt
+++ app/src/main/java/org/dhis2/usescases/featureconfig/data/FeatureConfigRepositoryImpl.kt
@@ -10,10 +10,13 @@ class FeatureConfigRepositoryImpl @Inject constructor(val preferences: Preferenc
 
     override val featuresList: List<FeatureState>
         get() = Feature.values().map {
-            FeatureState(it, preferences.getBoolean(it.name, false))
+            FeatureState(it, isFeatureEnable(it))
         }
 
     override fun updateItem(featureState: FeatureState) {
         preferences.setValue(featureState.feature.name, !featureState.enable)
     }
+
+    override fun isFeatureEnable(feature: Feature) =
+        preferences.getBoolean(feature.name, false)
 }
