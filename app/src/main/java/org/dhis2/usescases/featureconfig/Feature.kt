8914e6eb4d53297cb6ddea4aac5b2335b50533dd
andresmr <personal@andresmr.com>
2021-06-16 08:01:41 +0200

    [ANDROAPP-4048] Extract text to string resource

--- app/src/main/java/org/dhis2/usescases/featureconfig/Feature.kt
+++ app/src/main/java/org/dhis2/usescases/featureconfig/Feature.kt
@@ -5,7 +5,4 @@ package org.dhis2.usescases.featureconfig
  * description user explanation
  * ex: ANDROAPP_2557("Local analytics")
  */
-enum class Feature(val description: String) {
-    FEATURE_1("Feature 1"),
-    FEATURE_2("Feature 2")
-}
+enum class Feature(val description: String)
