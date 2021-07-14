681e833636f9dca556cbc3bef8500f0c54468ffd
Pablo <ppajueloc@gmail.com>
2021-07-14 13:44:32 +0200

    fix [ANDROAPP-4109] Fix toDate NullPointerException

--- app/src/main/java/org/dhis2/data/forms/dataentry/fields/age/AgeBindings.kt
+++ app/src/main/java/org/dhis2/data/forms/dataentry/fields/age/AgeBindings.kt
@@ -5,11 +5,12 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
-import java.util.Calendar
-import java.util.Date
 import org.dhis2.Bindings.toDate
+import org.dhis2.R
 import org.dhis2.data.forms.dataentry.fields.setInputStyle
 import org.dhis2.utils.DateUtils
+import java.util.Calendar
+import java.util.Date
 
 @BindingAdapter("onFocusChangeAgeView")
 fun onFocusChangesAgeView(editText: EditText, model: AgeViewModel) {
@@ -40,11 +41,15 @@ fun setInitialValueDate(editText: EditText, value: String?) {
     if (value.isNullOrEmpty()) {
         editText.text = null
     } else {
-        val initialDate = value.toDate()
-        val dateFormat = DateUtils.uiDateFormat()
-        val result = dateFormat.format(initialDate)
-        Calendar.getInstance().time = initialDate
-        editText.setText(result)
+        try {
+            val initialDate = value.toDate()
+            val dateFormat = DateUtils.uiDateFormat()
+            val result = dateFormat.format(initialDate)
+            Calendar.getInstance().time = initialDate
+            editText.setText(result)
+        } catch (e: Exception) {
+            editText.error = editText.context.getString(R.string.wrong_date_format).format(value)
+        }
     }
 }
 
