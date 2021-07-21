c104aab4bdadb3e23c008ad0e58baa1c01f2553a
andresmr <personal@andresmr.com>
2021-07-21 12:44:29 +0200

    [ANDROAPP-4099] Update SDK with allowEventCreation service

--- app/src/main/java/org/dhis2/usescases/teiDashboard/dashboardfragments/teidata/TEIDataPresenterImpl.java
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/dashboardfragments/teidata/TEIDataPresenterImpl.java
@@ -514,7 +514,7 @@ public class TEIDataPresenterImpl implements TEIDataContracts.Presenter {
     private boolean canAddNewEvents() {
         return d2.enrollmentModule()
                 .enrollmentService()
-                .allowEventCreation(
+                .blockingGetAllowEventCreation(
                         enrollmentUid,
                         stagesToHide
                 );
