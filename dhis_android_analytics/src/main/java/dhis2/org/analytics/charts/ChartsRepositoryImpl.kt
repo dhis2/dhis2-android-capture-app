44cbddb893be961e125fad08a6b4e734022d214a
Pablo <ppajueloc@gmail.com>
2021-09-15 15:34:41 +0200

    fix: klint

--- dhis_android_analytics/src/main/java/dhis2/org/analytics/charts/ChartsRepositoryImpl.kt
+++ dhis_android_analytics/src/main/java/dhis2/org/analytics/charts/ChartsRepositoryImpl.kt
@@ -170,7 +170,6 @@ class ChartsRepositoryImpl(
                     selectedRelativePeriod?.firstOrNull(),
                     selectedOrgUnits
                 ).let {
-
                 }
             }
         }
