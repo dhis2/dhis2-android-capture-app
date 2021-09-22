318599c93d09f2177cf2bcb05299e9e8ad091473
Pablo <ppajueloc@gmail.com>
2021-09-22 12:56:31 +0200

    fix: [ANDROAPP-4290] Update SerieColor list (#2285)

--- dhis_android_analytics/src/main/java/dhis2/org/analytics/charts/data/SerieColors.kt
+++ dhis_android_analytics/src/main/java/dhis2/org/analytics/charts/data/SerieColors.kt
@@ -5,15 +5,25 @@ import android.graphics.Color
 class SerieColors {
     companion object {
         fun getColors(): List<Int> = listOf(
-            Color.parseColor("#E71409"),
-            Color.parseColor("#49B044"),
-            Color.parseColor("#994BA5"),
-            Color.parseColor("#337DBA"),
-            Color.parseColor("#FF7F00"),
-            Color.parseColor("#999999"),
-            Color.parseColor("#F97FC0"),
-            Color.parseColor("#2C2C2C"),
-            Color.parseColor("#A85621")
+            Color.parseColor("#F44336"),
+            Color.parseColor("#E91E63"),
+            Color.parseColor("#9C27B0"),
+            Color.parseColor("#673AB7"),
+            Color.parseColor("#3F51B5"),
+            Color.parseColor("#2196F3"),
+            Color.parseColor("#03A9F4"),
+            Color.parseColor("#00BCD4"),
+            Color.parseColor("#009688"),
+            Color.parseColor("#4CAF50"),
+            Color.parseColor("#8BC34A"),
+            Color.parseColor("#CDDC39"),
+            Color.parseColor("#FFEB3B"),
+            Color.parseColor("#FFC107"),
+            Color.parseColor("#FF9800"),
+            Color.parseColor("#FF5722"),
+            Color.parseColor("#795548"),
+            Color.parseColor("#9E9E9E"),
+            Color.parseColor("#607D8B")
         )
     }
 }
