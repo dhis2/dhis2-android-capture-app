046e760266c2d876612729a482f7d7cfbc1c9192
FerdyRod <ferdyrodriguez@gmail.com>
2021-10-06 17:10:01 +0200

    fix: Shows toolbar loading progress when loading data on the TEI Dashboard.

--- app/src/main/java/org/dhis2/usescases/teiDashboard/TeiDashboardMobileActivity.java
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/TeiDashboardMobileActivity.java
@@ -285,8 +285,10 @@ public class TeiDashboardMobileActivity extends ActivityGlobalAbstract implement
 
     private void showLoadingProgress(boolean showProgress){
         if(showProgress){
+            binding.toolbarProgress.setVisibility(View.VISIBLE);
             binding.toolbarProgress.show();
         }else{
+            binding.toolbarProgress.setVisibility(View.GONE);
             binding.toolbarProgress.hide();
         }
     }
