df7598862f25f43e13d7444df28d6230cd91195a
jaimeToca <jaime.toca.munoz@gmail.com>
2021-11-10 13:11:12 +0100

    Remove strict mode

--- app/src/androidTest/java/org/dhis2/AppTest.kt
+++ app/src/androidTest/java/org/dhis2/AppTest.kt
@@ -26,7 +26,7 @@ class AppTest : App() {
 
     @Override
     override fun onCreate() {
-        enableStrictMode()
+   //     enableStrictMode()
         populateDBIfNeeded()
         super.onCreate()
     }
