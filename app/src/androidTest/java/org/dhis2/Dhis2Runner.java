6d4297cdfb667044987e2dfca79b02cace886787
jaimeToca <jaime.toca.munoz@gmail.com>
2021-11-11 10:33:29 +0100

    Fix ktlint

--- app/src/androidTest/java/org/dhis2/Dhis2Runner.java
+++ app/src/androidTest/java/org/dhis2/Dhis2Runner.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
+
 import androidx.test.runner.AndroidJUnitRunner;
 
 import com.squareup.rx2.idler.Rx2Idler;
@@ -11,6 +13,11 @@ import com.squareup.rx2.idler.Rx2Idler;
 import io.reactivex.plugins.RxJavaPlugins;
 
 public class Dhis2Runner extends AndroidJUnitRunner {
+    @Override
+    public boolean onException(Object obj, Throwable e) {
+        Log.e("Dhis2Runner", "Object: "+ obj.toString() + "Throwable: "+ e.toString());
+        return super.onException(obj, e);
+    }
 
     @Override
     public void onCreate(Bundle arguments) {
