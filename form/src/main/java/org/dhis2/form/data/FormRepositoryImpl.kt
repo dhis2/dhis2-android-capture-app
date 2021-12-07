e6b67556652351d776fbe233445dda3687cbef55
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2021-12-07 17:11:32 +0100

    fix: [ANDROAPP-4469] Adds check when trying to collapse an open section (#2436)

--- form/src/main/java/org/dhis2/form/data/FormRepositoryImpl.kt
+++ form/src/main/java/org/dhis2/form/data/FormRepositoryImpl.kt
@@ -2,12 +2,14 @@ package org.dhis2.form.data
 
 import org.dhis2.form.model.ActionType
 import org.dhis2.form.model.FieldUiModel
+import org.dhis2.form.model.FieldUiModelImpl
 import org.dhis2.form.model.RowAction
 import org.dhis2.form.model.StoreResult
 import org.dhis2.form.model.ValueStoreResult
 import org.dhis2.form.ui.provider.DisplayNameProvider
 import org.dhis2.form.ui.validation.FieldErrorMessageProvider
 import org.hisp.dhis.android.core.common.ValueType
+import org.hisp.dhis.android.core.common.ValueType.LONG_TEXT
 
 private const val loopThreshold = 5
 
@@ -105,9 +107,15 @@ class FormRepositoryImpl(
         if (isEmpty()) {
             return this
         }
-        val lastItem = getLastSectionItem(this)
-        return if (usesKeyboard(lastItem.valueType) && lastItem.valueType != ValueType.LONG_TEXT) {
-            updated(indexOf(lastItem), lastItem.setKeyBoardActionDone())
+        // TODO: This check was added when trying to collapse an open section.
+        //       When SectionViewModel is removed then this needs to be looked at.
+        return if (!this.none { it is FieldUiModelImpl }) {
+            val lastItem = getLastSectionItem(this)
+            return if (usesKeyboard(lastItem.valueType) && lastItem.valueType != LONG_TEXT) {
+                updated(indexOf(lastItem), lastItem.setKeyBoardActionDone())
+            } else {
+                this
+            }
         } else {
             this
         }
