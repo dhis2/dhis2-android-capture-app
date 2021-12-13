b69df85f3a2503e416dcd9fa4c1ee65e6fc510ce
Pablo <ppajueloc@gmail.com>
2021-12-13 10:02:56 +0100

    fix: [ANDROAPP-4481] Fix crash in event and multi-section enrollment fields

--- form/src/main/java/org/dhis2/form/data/FormValueStore.kt
+++ form/src/main/java/org/dhis2/form/data/FormValueStore.kt
@@ -1,10 +1,12 @@
 package org.dhis2.form.data
 
+import io.reactivex.Flowable
 import org.dhis2.form.model.StoreResult
 
 interface FormValueStore {
 
     fun save(uid: String, value: String?, extraData: String?): StoreResult
+    fun saveWithTypeCheck(uid: String, value: String?): Flowable<StoreResult>
     fun deleteOptionValues(optionCodeValuesToDelete: List<String>)
     fun deleteOptionValueIfSelected(field: String, optionUid: String): StoreResult
     fun deleteOptionValueIfSelectedInGroup(
