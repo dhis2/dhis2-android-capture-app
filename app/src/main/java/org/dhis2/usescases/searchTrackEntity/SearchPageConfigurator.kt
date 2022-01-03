fbb07e9e916b78b3fbde4cbba6006d053ed889ab
jaimeToca <jaime.toca.munoz@gmail.com>
2022-01-03 15:46:43 +0100

    test: moving it async

--- app/src/main/java/org/dhis2/usescases/searchTrackEntity/SearchPageConfigurator.kt
+++ app/src/main/java/org/dhis2/usescases/searchTrackEntity/SearchPageConfigurator.kt
@@ -1,9 +1,12 @@
 package org.dhis2.usescases.searchTrackEntity
 
+import io.reactivex.Single
+import org.dhis2.commons.schedulers.SchedulerProvider
 import org.dhis2.utils.customviews.navigationbar.NavigationPageConfigurator
 
 class SearchPageConfigurator(
-    val searchRepository: SearchRepository
+    val searchRepository: SearchRepository,
+    val schedulerProvider: SchedulerProvider
 ) : NavigationPageConfigurator {
 
     override fun displayListView(): Boolean {
@@ -15,7 +18,16 @@ class SearchPageConfigurator(
     }
 
     override fun displayMapView(): Boolean {
-        return searchRepository.programHasCoordinates()
+       return Single.fromCallable { searchRepository.programHasCoordinates() }
+           .subscribeOn(schedulerProvider.ui())
+           .observeOn(schedulerProvider.ui())
+           .blockingGet()
+      /*  Observable.fromCallable(searchRepository.programHasCoordinates())
+            .subscribeOn(schedulerProvider.io())
+            .observeOn(schedulerProvider.ui())
+*/
+
+        //Observable.fromCallable(() -> decompress(decodeData(inputData).getBytes()))
     }
 
     override fun displayAnalytics(): Boolean {
