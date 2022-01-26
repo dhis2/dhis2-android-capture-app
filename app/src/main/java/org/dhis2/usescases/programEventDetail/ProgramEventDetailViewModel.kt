62e99d00b3841be672d60db48f9b0ec4fa56cb4a
Pablo <ppajueloc@gmail.com>
2022-01-26 10:27:13 +0100

    fix: [ANDROAPP-4541] Error in the event navigation panel

--- app/src/main/java/org/dhis2/usescases/programEventDetail/ProgramEventDetailViewModel.kt
+++ app/src/main/java/org/dhis2/usescases/programEventDetail/ProgramEventDetailViewModel.kt
@@ -6,14 +6,17 @@ import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class ProgramEventDetailViewModel : ViewModel() {
-    private val progress = MutableLiveData<Boolean>(true)
-    val writePermission = MutableLiveData<Boolean>()
+    private val progress = MutableLiveData(true)
+    val writePermission = MutableLiveData(false)
     val eventSyncClicked = MutableLiveData<String?>(null)
     val eventClicked = MutableLiveData<Pair<String, String>?>(null)
     var updateEvent: String? = null
-    private val _showMap = MutableLiveData(false)
-    val showMap: LiveData<Boolean>
-        get() = Transformations.distinctUntilChanged(_showMap)
+    enum class EventProgramScreen {
+        LIST, MAP, ANALYTICS
+    }
+    private val _currentScreen = MutableLiveData(EventProgramScreen.LIST)
+    val currentScreen: LiveData<EventProgramScreen>
+        get() = Transformations.distinctUntilChanged(_currentScreen)
 
     fun setProgress(showProgress: Boolean) {
         progress.value = showProgress
@@ -22,10 +25,14 @@ class ProgramEventDetailViewModel : ViewModel() {
     fun progress(): LiveData<Boolean> = progress
 
     fun showList() {
-        _showMap.value = false
+        _currentScreen.value = EventProgramScreen.LIST
     }
 
     fun showMap() {
-        _showMap.value = true
+        _currentScreen.value = EventProgramScreen.MAP
+    }
+
+    fun showAnalytics() {
+        _currentScreen.value = EventProgramScreen.ANALYTICS
     }
 }
