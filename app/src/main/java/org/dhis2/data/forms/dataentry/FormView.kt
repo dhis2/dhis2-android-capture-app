fbe9f93cf96307d3ec9aa32e49dd5e584eb3921d
Victor Garcia <vgarciabnz@gmail.com>
2022-03-03 13:18:25 +0100

    Add requireContext() for FileResourceDirectoryHelper

--- app/src/main/java/org/dhis2/data/forms/dataentry/FormView.kt
+++ app/src/main/java/org/dhis2/data/forms/dataentry/FormView.kt
@@ -706,7 +706,7 @@ class FormView(
                                 requireContext(),
                                 BuildConfig.APPLICATION_ID + ".provider",
                                 File(
-                                    FileResourceDirectoryHelper.getFileResourceDirectory(context),
+                                    FileResourceDirectoryHelper.getFileResourceDirectory(requireContext()),
                                     "tempFile.png"
                                 )
                             )
