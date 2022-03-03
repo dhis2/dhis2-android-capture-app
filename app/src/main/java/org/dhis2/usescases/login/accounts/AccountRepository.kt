dfed67ea5da53cc479d6cb862d656b52de2ead6f
Ferdy Rodriguez <ferdyrodriguez@gmail.com>
2022-03-03 08:55:33 +0100

    feat: [ANDROAPP-653] Implements multi-user accounts (#2536)

--- app/src/main/java/org/dhis2/usescases/login/accounts/AccountRepository.kt
+++ app/src/main/java/org/dhis2/usescases/login/accounts/AccountRepository.kt
@@ -5,9 +5,8 @@ import org.hisp.dhis.android.core.D2
 class AccountRepository(val d2: D2) {
 
     fun getLoggedInAccounts(): List<AccountModel> {
-        return listOf(
-            AccountModel("userName 1", "serverUrl 1"),
-            AccountModel("userName 2", "serverUrl 2")
-        )
+        return d2.userModule().accountManager().getAccounts().map {
+            AccountModel(it.username(), it.serverUrl())
+        }
     }
 }
