25ce208af803a16959c412049f4948c83432d991
Pablo <ppajueloc@gmail.com>
2022-03-08 09:12:55 +0100

    refactor: [ANDROSDK-1494] klint

--- app/src/main/java/org/dhis2/data/forms/dataentry/FormView.kt
+++ app/src/main/java/org/dhis2/data/forms/dataentry/FormView.kt
@@ -706,7 +706,9 @@ class FormView(
                                 requireContext(),
                                 BuildConfig.APPLICATION_ID + ".provider",
                                 File(
-                                    FileResourceDirectoryHelper.getFileResourceDirectory(requireContext()),
+                                    FileResourceDirectoryHelper.getFileResourceDirectory(
+                                        requireContext()
+                                    ),
                                     "tempFile.png"
                                 )
                             )
