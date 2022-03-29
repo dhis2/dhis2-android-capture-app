f418afffa5d5db92d780df095db3215f85e0a8af
Pablo <ppajueloc@gmail.com>
2022-03-29 11:19:16 +0200

    fix: klint

--- app/src/main/java/org/dhis2/usescases/searchTrackEntity/SearchTEUi.kt
+++ app/src/main/java/org/dhis2/usescases/searchTrackEntity/SearchTEUi.kt
@@ -11,11 +11,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
