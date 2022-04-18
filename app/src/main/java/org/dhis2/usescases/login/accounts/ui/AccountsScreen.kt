7de1a6c65425e1a73bd7c0c46aaa8c1a308505db
FerdyRod <ferdyrodriguez@gmail.com>
2022-04-18 12:21:57 +0200

    ktlint

--- app/src/main/java/org/dhis2/usescases/login/accounts/ui/AccountsScreen.kt
+++ app/src/main/java/org/dhis2/usescases/login/accounts/ui/AccountsScreen.kt
@@ -19,9 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.font.*
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.toUpperCase
