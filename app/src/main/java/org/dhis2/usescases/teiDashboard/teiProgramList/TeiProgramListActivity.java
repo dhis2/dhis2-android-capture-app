bcfdf2acddfd0bf64b61835f2fdda6cde665d67e
Pablo <ppajueloc@gmail.com>
2022-05-17 13:52:36 +0200

    fix: [ANDROAPP-4534] Remove unused imports

--- app/src/main/java/org/dhis2/usescases/teiDashboard/teiProgramList/TeiProgramListActivity.java
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/teiProgramList/TeiProgramListActivity.java
@@ -9,12 +9,10 @@ import androidx.databinding.DataBindingUtil;
 
 import org.dhis2.App;
 import org.dhis2.R;
-import org.dhis2.commons.resources.ColorUtils;
 import org.dhis2.databinding.ActivityTeiProgramListBinding;
 import org.dhis2.ui.ThemeManager;
 import org.dhis2.usescases.general.ActivityGlobalAbstract;
 import org.dhis2.usescases.main.program.ProgramViewModel;
-import org.dhis2.utils.Constants;
 
 import java.util.List;
 
@@ -40,7 +38,6 @@ public class TeiProgramListActivity extends ActivityGlobalAbstract implements Te
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_tei_program_list);
         binding.setPresenter(presenter);
-
     }
 
     @Override
