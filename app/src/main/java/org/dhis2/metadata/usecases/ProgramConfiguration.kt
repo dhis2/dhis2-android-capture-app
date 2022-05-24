91182b397f483c3d6d3b8d6b5e6ef30b20f21762
Pablo <ppajueloc@gmail.com>
2022-05-24 08:38:05 +0200

    fix[ANDROAPP-4750, ANDROAPP-4753]: Tei download crashes

--- app/src/main/java/org/dhis2/metadata/usecases/ProgramConfiguration.kt
+++ app/src/main/java/org/dhis2/metadata/usecases/ProgramConfiguration.kt
@@ -1,6 +1,7 @@
 package org.dhis2.metadata.usecases
 
 import org.hisp.dhis.android.core.D2
+import org.hisp.dhis.android.core.program.Program
 
 class ProgramConfiguration(private val d2: D2) {
 
@@ -11,4 +12,14 @@ class ProgramConfiguration(private val d2: D2) {
     fun getProgramIcon(programUid: String) = getProgramStyle(programUid).icon()
 
     fun getProgramColor(programUid: String) = getProgramStyle(programUid).color()
+
+    fun canEnrollNewTei(programUid: String): Boolean {
+        val selectedProgram: Program =
+            d2.programModule().programs().uid(programUid).blockingGet()
+        val programAccess = selectedProgram.access().data().write() == true
+        val teTypeAccess: Boolean = d2.trackedEntityModule().trackedEntityTypes().uid(
+            selectedProgram.trackedEntityType()!!.uid()
+        ).blockingGet().access().data().write()
+        return programAccess && teTypeAccess
+    }
 }
