83a6056b69faa6feb4e330eeea221a217edd308e
Pablo <ppajueloc@gmail.com>
2022-06-03 07:19:11 +0200

    fix: [ANDROAPP-4792] Use une textWatcher for coordinates

--- form/src/main/java/org/dhis2/form/ui/DataEntryAdapter.kt
+++ form/src/main/java/org/dhis2/form/ui/DataEntryAdapter.kt
@@ -14,6 +14,7 @@ import org.dhis2.form.model.SectionUiModelImpl
 import org.dhis2.form.ui.FormViewHolder.FieldItemCallback
 import org.dhis2.form.ui.event.RecyclerViewUiEvents
 import org.dhis2.form.ui.intent.FormIntent
+import org.hisp.dhis.android.core.common.ValueType
 
 class DataEntryAdapter(private val searchStyle: Boolean) :
     ListAdapter<FieldUiModel, FormViewHolder>(DataEntryDiff()),
@@ -48,6 +49,11 @@ class DataEntryAdapter(private val searchStyle: Boolean) :
         }
     }
 
+    val coordinateWatcher = LatitudeLongitudeTextWatcher { coordinates ->
+        currentList.find { it.focused && it.valueType == ValueType.COORDINATE }
+            ?.onTextChange(coordinates)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FormViewHolder {
         val layoutInflater =
             if (refactoredViews.contains(viewType) && searchStyle) {
@@ -69,7 +75,7 @@ class DataEntryAdapter(private val searchStyle: Boolean) :
         if (getItem(position) is SectionUiModelImpl) {
             updateSectionData(position, false)
         }
-        holder.bind(getItem(position), this, textWatcher)
+        holder.bind(getItem(position), this, textWatcher, coordinateWatcher)
     }
 
     fun updateSectionData(position: Int, isHeader: Boolean) {
---
+++ form/src/main/java/org/dhis2/form/ui/LatitudeLongitudeTextWatcher.kt
@@ -0,0 +1,71 @@
+package org.dhis2.form.ui
+
+import android.text.Editable
+import android.text.TextWatcher
+import org.hisp.dhis.android.core.arch.helpers.GeometryHelper
+
+class LatitudeLongitudeTextWatcher(val onValueChanged: (value: String?) -> Unit) {
+
+    private var currentLatitude: String? = null
+    private var currentLongitude: String? = null
+
+    private val latitudeWatcher = object : TextWatcher {
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (validateFilledCoordinates(text.toString(), currentLongitude ?: "")) {
+                val lon = currentLongitude?.toDoubleOrNull()
+                val latitude = text.toString().toDoubleOrNull()
+                val value = if (lon != null && latitude != null) {
+                    GeometryHelper.createPointGeometry(lon, latitude)?.coordinates()
+                } else {
+                    null
+                }
+                onValueChanged(value)
+            }
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+        }
+    }
+
+    private val longitudeTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (validateFilledCoordinates(currentLatitude ?: "", text.toString())) {
+                val lon = text.toString().toDoubleOrNull()
+                val latitude = currentLatitude?.toDoubleOrNull()
+                val value = if (lon != null && latitude != null) {
+                    GeometryHelper.createPointGeometry(lon, latitude)?.coordinates()
+                } else {
+                    null
+                }
+                onValueChanged(value)
+            }
+        }
+
+        override fun afterTextChanged(p0: Editable?) {
+        }
+    }
+
+    private fun validateFilledCoordinates(lat: String, long: String) =
+        lat.isNotEmpty() && long.isNotEmpty() ||
+            lat.isEmpty() && long.isEmpty()
+
+    fun latitudeWatcher(): TextWatcher {
+        return latitudeWatcher
+    }
+
+    fun longitudeWatcher(): TextWatcher {
+        return longitudeTextWatcher
+    }
+
+    fun resetCurrentValues(newLatitude: String? = null, newLongitude: String? = null) {
+        currentLatitude = newLatitude
+        currentLongitude = newLongitude
+    }
+}
