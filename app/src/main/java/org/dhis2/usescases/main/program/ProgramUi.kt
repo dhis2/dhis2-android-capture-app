5c0492152c1af48baa87c19ce1c5846b8d4292c1
Pablo <ppajueloc@gmail.com>
2022-06-08 11:34:49 +0200

    fix: do not show progress if syncing

--- app/src/main/java/org/dhis2/usescases/main/program/ProgramUi.kt
+++ app/src/main/java/org/dhis2/usescases/main/program/ProgramUi.kt
@@ -176,21 +176,6 @@ fun ProgramItem(
                 tint = Color.Unspecified
             )
         }
-        when (programViewModel.downloadState) {
-            ProgramDownloadState.DOWNLOADING -> DownloadingProgress()
-            ProgramDownloadState.DOWNLOADED -> DownloadedIcon(programViewModel)
-            ProgramDownloadState.NONE -> StateIcon(programViewModel.state) {
-                onGranularSyncClick(programViewModel)
-            }
-        }
-
-        if (programViewModel.hasOverdueEvent) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_overdue),
-                contentDescription = "Overdue",
-                tint = Color.Unspecified
-            )
-        }
     }
 }
 
