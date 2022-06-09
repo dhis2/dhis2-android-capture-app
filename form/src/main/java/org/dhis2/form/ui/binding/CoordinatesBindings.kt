f7e342a9a194c24efc79f6449a6e1805e5de32ec
Pablo <ppajueloc@gmail.com>
2022-06-09 14:22:32 +0200

    fix: [ANDROAPP-3825] Add progress while loading coordinates

--- form/src/main/java/org/dhis2/form/ui/binding/CoordinatesBindings.kt
+++ form/src/main/java/org/dhis2/form/ui/binding/CoordinatesBindings.kt
@@ -3,12 +3,14 @@ package org.dhis2.form.ui.binding
 import android.content.Context
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.google.android.material.textfield.TextInputEditText
 import org.dhis2.commons.extensions.closeKeyboard
 import org.dhis2.form.R
 import org.dhis2.form.model.FieldUiModel
+import org.dhis2.form.model.UiEventType
 import org.dhis2.form.model.UiRenderType
 import org.dhis2.form.ui.LatitudeLongitudeTextWatcher
 import org.dhis2.form.ui.intent.FormIntent.SaveCurrentLocation
@@ -164,6 +166,20 @@ fun TextInputEditText.setOnGeometryLongitudeEditorActionListener(
         }
         true
     }
+}
+
+@BindingAdapter(value = ["model", "loadingView"], requireAll = true)
+fun ImageButton.setOnLocationClick(model: FieldUiModel, progress: View) {
+    setOnClickListener {
+        this.visibility = View.GONE
+        progress.visibility = View.VISIBLE
+        model.invokeUiEvent(UiEventType.REQUEST_CURRENT_LOCATION)
+    }
+}
+
+@BindingAdapter("progressVisibility")
+fun View.progressVisibility(model: FieldUiModel) {
+    visibility = View.GONE
 }
 
 private fun onFilledCoordinate(
