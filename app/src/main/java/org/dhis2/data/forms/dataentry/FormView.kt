4bab7db774a695f5a304290ee32d3e7e2b960da3
Jaime Toca <jaime.toca.munoz@gmail.com>
2022-06-15 12:28:11 +0200

    fix: move view model init. Crash lateinit (#2751)
    * fix: move view model init
    
    * fix ktlint

--- app/src/main/java/org/dhis2/data/forms/dataentry/FormView.kt
+++ app/src/main/java/org/dhis2/data/forms/dataentry/FormView.kt
@@ -24,7 +24,6 @@ import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -46,7 +45,6 @@ import org.dhis2.data.forms.ScanContract
 import org.dhis2.data.location.LocationProvider
 import org.dhis2.data.location.LocationSettingLauncher
 import org.dhis2.databinding.ViewFormBinding
-import org.dhis2.form.Injector
 import org.dhis2.form.data.DataIntegrityCheckResult
 import org.dhis2.form.data.FormRepository
 import org.dhis2.form.data.RulesUtilsProviderConfigurationError
@@ -176,9 +174,7 @@ class FormView : Fragment() {
             }
         }
 
-    private val viewModel: FormViewModel by viewModels {
-        Injector.provideFormViewModelFactory(formRepository, dispatchers)
-    }
+    private lateinit var viewModel: FormViewModel
 
     private lateinit var binding: ViewFormBinding
     private lateinit var dataEntryHeaderHelper: DataEntryHeaderHelper
@@ -219,6 +215,7 @@ class FormView : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewModel = FormViewModel(formRepository, dispatchers)
         FormCountingIdlingResource.increment()
         dataEntryHeaderHelper.observeHeaderChanges(viewLifecycleOwner)
         adapter = DataEntryAdapter(needToForceUpdate)
