452ae739f6aa6119fb2502715f0daac1af2b2e0b
FerdyRod <ferdyrodriguez@gmail.com>
2022-06-23 11:04:57 +0200

    Fixes ui tests

--- app/src/main/java/org/dhis2/usescases/teiDashboard/teiProgramList/ui/EnrollToProgram.kt
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/teiProgramList/ui/EnrollToProgram.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -56,6 +58,7 @@ fun EnrollToProgram(
         )
         Button(
             modifier = Modifier
+                .semantics { testTag = PROGRAM_TO_ENROLL.format(programViewModel.title) }
                 .height(36.dp)
                 .weight(1.2f, true)
                 .padding(end = 16.dp),
@@ -102,3 +105,5 @@ private fun testingProgramModel(downloadState: ProgramDownloadState) = ProgramVi
     false,
     downloadState = downloadState
 )
+
+const val PROGRAM_TO_ENROLL = "PROGRAM_TO_ENROLL_%s"
