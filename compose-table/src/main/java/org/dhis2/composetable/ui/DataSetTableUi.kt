28b2dcf2a90fb685a0f37eb1eb3c2df274cbed7c
manu <mmunoz@quadram.mobi>
2022-09-02 14:58:11 +0200

    import

--- compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableUi.kt
+++ compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableUi.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.rememberScrollState
