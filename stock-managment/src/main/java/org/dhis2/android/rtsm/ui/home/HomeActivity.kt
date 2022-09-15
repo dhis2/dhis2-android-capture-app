4b3d9fdbd5bde70bb41eeda79033131449b82e56
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-09-15 10:35:22 +0200

    creating activity home for compose

---
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/HomeActivity.kt
@@ -0,0 +1,73 @@
+package org.dhis2.android.rtsm.ui.home
+
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
+import dagger.hilt.android.AndroidEntryPoint
+import org.dhis2.android.rtsm.R
+import org.dhis2.android.rtsm.data.TransactionType
+import org.dhis2.android.rtsm.ui.home.screens.Backdrop
+
+@AndroidEntryPoint
+class HomeActivity: ComponentActivity() {
+
+    private val viewModel: HomeViewModel by viewModels()
+    private var themeColor =  R.color.colorPrimary
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                updateTheme(viewModel.transactionType.value)
+                Backdrop(
+                    viewModel,
+                    Color(colorResource(themeColor).toArgb()),
+                ) { finish() }
+            }
+        }
+    }
+
+    private fun updateTheme(type: TransactionType) {
+        val color: Int
+        val theme: Int
+        viewModel.setToolbarTitle(type)
+        when (type) {
+            TransactionType.DISTRIBUTION -> {
+                color = R.color.colorPrimary
+                theme = R.style.AppTheme
+            }
+            TransactionType.DISCARD -> {
+                color = R.color.discard_color
+                theme = R.style.RedTheme
+            }
+            TransactionType.CORRECTION -> {
+                color = R.color.colorPrimary_fbc
+                theme = R.style.colorPrimary_fbc
+            }
+        }
+        if (color != -1) {
+            this.theme.applyStyle(theme, true)
+
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            val typedValue = TypedValue()
+            val a = obtainStyledAttributes(typedValue.data, intArrayOf(R.attr.colorPrimaryDark))
+            val colorToReturn = a.getColor(0, 0)
+            a.recycle()
+            window.statusBarColor = colorToReturn
+            themeColor = color
+        }
+    }
+}
