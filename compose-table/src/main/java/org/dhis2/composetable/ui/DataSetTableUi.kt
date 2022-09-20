9b4af41bb472cb8c55d690e0bef752e3a476afcb
JaimeToca <jaime.toca.munoz@gmail.com>
2022-09-20 09:19:42 +0200

    fix: sonarqube

--- compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableUi.kt
+++ compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableUi.kt
@@ -49,7 +49,6 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
-import androidx.compose.ui.semantics.selected
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
