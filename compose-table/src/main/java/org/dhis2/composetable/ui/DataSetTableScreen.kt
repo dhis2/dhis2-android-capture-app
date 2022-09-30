d7a140ad80c04127b0268c62f303baaea74e753f
Pablo <ppajueloc@gmail.com>
2022-09-30 10:31:16 +0200

    ktlint

--- compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableScreen.kt
+++ compose-table/src/main/java/org/dhis2/composetable/ui/DataSetTableScreen.kt
@@ -25,9 +25,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
