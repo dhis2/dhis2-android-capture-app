bb8de545277bddba85cf18291dac9871f2996fab
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-10-14 16:12:33 +0200

    Returning local density

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/FilterComponents.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/FilterComponents.kt
@@ -1,11 +1,23 @@
 package org.dhis2.android.rtsm.ui.home.screens.components
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.dhis2.android.rtsm.R
 import org.dhis2.android.rtsm.data.OperationState
@@ -21,14 +33,33 @@ fun FilterList(
     themeColor: Color,
     isFacilitySelected: (value: Boolean) -> Unit = {},
     isDestinationSelected: (value: Boolean) -> Unit = {}
-) {
+): Dp {
     val facilities = viewModel.facilities.collectAsState().value
     val destinations = viewModel.destinationsList.collectAsState().value
     val showDestination = viewModel.isDistribution.collectAsState().value
 
+    // get local density from composable
+    val localDensity = LocalDensity.current
+    var heightIs by remember {
+        mutableStateOf(0.dp)
+    }
+
     LazyColumn(
         verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
+        contentPadding = PaddingValues(vertical = 16.dp),
+        modifier = Modifier
+            .onSizeChanged { coordinates ->
+                heightIs = with(localDensity) { coordinates.height.toDp() }
+            }
+            .animateContentSize(
+                animationSpec = tween(
+                    delayMillis = 400,
+                    easing = LinearOutSlowInEasing
+                )
+            )
+            .onGloballyPositioned { coordinates ->
+                heightIs = with(localDensity) { coordinates.size.height.toDp() }
+            }
     ) {
         item {
             DropdownComponent(
@@ -63,6 +94,7 @@ fun FilterList(
             }
         }
     }
+    return heightIs
 }
 
 private fun mapTransaction(): MutableList<TransactionItem> {
