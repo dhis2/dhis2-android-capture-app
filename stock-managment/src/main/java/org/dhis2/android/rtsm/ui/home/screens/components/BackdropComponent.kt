daa8b9dc4cda2e4820a2f4a886d6b49e743806fc
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-10-14 16:13:34 +0200

    Adding frontLayer logic according to backLayer

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
@@ -1,13 +1,11 @@
 package org.dhis2.android.rtsm.ui.home.screens.components
 
 import android.app.Activity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BackdropScaffold
 import androidx.compose.material.BackdropValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
 import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -15,10 +13,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
+import org.dhis2.android.rtsm.data.TransactionType
 import org.dhis2.android.rtsm.ui.home.HomeViewModel
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -30,10 +29,14 @@ fun Backdrop(
     scaffoldState: ScaffoldState,
     syncAction: (scope: CoroutineScope, scaffoldState: ScaffoldState) -> Unit = { _, _ -> }
 ) {
-    val backdropState = rememberBackdropScaffoldState(BackdropValue.Concealed)
+    val backdropState = rememberBackdropScaffoldState(BackdropValue.Revealed)
 
     var hasFacilitySelected by remember { mutableStateOf(false) }
     var hasDestinationSelected by remember { mutableStateOf<Boolean?>(null) }
+    var toolbarTitle by remember {
+         mutableStateOf(TransactionType.DISTRIBUTION.name)
+    }
+    var dpHeight by remember { mutableStateOf<Dp?>(0.dp) }
 
     BackdropScaffold(
         appBar = {
@@ -51,10 +54,11 @@ fun Backdrop(
                 hasFacilitySelected,
                 hasDestinationSelected
             )
+            toolbarTitle =  viewModel.toolbarTitle.collectAsState().value.name
         },
         backLayerBackgroundColor = themeColor,
         backLayerContent = {
-            FilterList(
+            dpHeight = FilterList(
                 viewModel, themeColor,
                 { hasFacilitySelected = it },
                 { hasDestinationSelected = it }
@@ -62,11 +66,21 @@ fun Backdrop(
         },
         frontLayerElevation = 5.dp,
         frontLayerContent = {
-            Column(Modifier.padding(16.dp)) {
-                Text(text = "Main content")
-            }
+            MainContent(dpHeight!!, backdropState, toolbarTitle)
         },
         scaffoldState = backdropState,
-        gesturesEnabled = false
+        gesturesEnabled = false,
+        frontLayerScrimColor = if (toolbarTitle == TransactionType.DISTRIBUTION.name) {
+            if (hasFacilitySelected && hasDestinationSelected == true)
+                Color.Unspecified
+            else
+                MaterialTheme.colors.surface.copy(alpha = 0.60f)
+
+        } else {
+            if (!hasFacilitySelected)
+                MaterialTheme.colors.surface.copy(alpha = 0.60f)
+            else
+                Color.Unspecified
+        }
     )
 }
