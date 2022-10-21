feec726b09d3c54bd139c52f56df7e11eb21dabb
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-10-21 15:13:21 +0200

    Androapp 5000 (#16)
    Create DataentryFragment

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
@@ -1,29 +1,31 @@
 package org.dhis2.android.rtsm.ui.home.screens.components
 
+import android.annotation.SuppressLint
 import android.app.Activity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BackdropScaffold
 import androidx.compose.material.BackdropValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
 import androidx.compose.material.rememberBackdropScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.dhis2.android.rtsm.data.TransactionType
 import org.dhis2.android.rtsm.ui.home.HomeActivity
 import org.dhis2.android.rtsm.ui.home.HomeViewModel
 
+@SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun Backdrop(
@@ -37,8 +39,13 @@ fun Backdrop(
 ) {
     val backdropState = rememberBackdropScaffoldState(BackdropValue.Concealed)
 
+    var isFrontLayerDisabled by remember { mutableStateOf<Boolean?>(null) }
     var hasFacilitySelected by remember { mutableStateOf(false) }
     var hasDestinationSelected by remember { mutableStateOf<Boolean?>(null) }
+    var toolbarTitle by remember {
+        mutableStateOf(TransactionType.DISTRIBUTION.name)
+    }
+    val scope = rememberCoroutineScope()
 
     BackdropScaffold(
         appBar = {
@@ -56,10 +63,11 @@ fun Backdrop(
                 hasFacilitySelected,
                 hasDestinationSelected
             )
+            toolbarTitle = viewModel.toolbarTitle.collectAsState().value.name
         },
         backLayerBackgroundColor = themeColor,
         backLayerContent = {
-            FilterList(
+            val height = FilterList(
                 viewModel,
                 themeColor,
                 supportFragmentManager,
@@ -67,14 +75,32 @@ fun Backdrop(
                 { hasFacilitySelected = it },
                 { hasDestinationSelected = it }
             )
+            if (height > 160.dp) {
+                scope.launch { backdropState.reveal() }
+            }
         },
         frontLayerElevation = 5.dp,
         frontLayerContent = {
-            Column(Modifier.padding(16.dp)) {
-                Text(text = "Main content")
-            }
+            MainContent(backdropState, isFrontLayerDisabled, themeColor)
         },
         scaffoldState = backdropState,
-        gesturesEnabled = false
+        gesturesEnabled = false,
+        frontLayerScrimColor = if (toolbarTitle == TransactionType.DISTRIBUTION.name) {
+            if (hasFacilitySelected && hasDestinationSelected == true) {
+                isFrontLayerDisabled = false
+                Color.Unspecified
+            } else {
+                isFrontLayerDisabled = true
+                MaterialTheme.colors.surface.copy(alpha = 0.60f)
+            }
+        } else {
+            if (!hasFacilitySelected) {
+                isFrontLayerDisabled = true
+                MaterialTheme.colors.surface.copy(alpha = 0.60f)
+            } else {
+                isFrontLayerDisabled = false
+                Color.Unspecified
+            }
+        }
     )
 }
