3317b88931e1342bd174b33361b37870313414f6
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2022-11-01 14:27:34 +0200

    Androapp 5025 (#17)
    Move Search box and scan code buttons to Dataentry section

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/MainContent.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/MainContent.kt
@@ -1,23 +1,50 @@
 package org.dhis2.android.rtsm.ui.home.screens.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.BackdropScaffoldState
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
 import org.dhis2.android.rtsm.R
+import timber.log.Timber
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -28,33 +55,139 @@ fun MainContent(
 ) {
     val scope = rememberCoroutineScope()
     val resource = painterResource(R.drawable.ic_arrow_up)
-    val commentsAlpha = if (backdropState.isRevealed) 1f else 0f
-    Row(
+    val qrcodeResource = painterResource(R.drawable.ic_qr_code_scanner)
+    val searchResource = painterResource(R.drawable.ic_search)
+    val closeResource = painterResource(R.drawable.ic_close)
+    var closeButtonVisibility by remember { mutableStateOf(0f) }
+    val weightValue = if (backdropState.isRevealed) 0.15f else 0.10f
+    val weightValueArrow = if (backdropState.isRevealed) 0.10f else 0.05f
+    val weightValueArrowStatus = backdropState.isRevealed
+    val focusManager = LocalFocusManager.current
+
+    Column(
         modifier = Modifier.fillMaxSize(),
-        horizontalArrangement = Arrangement.End,
-        verticalAlignment = Alignment.Top
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        IconButton(
-            onClick = {
-                scope.launch { backdropState.conceal() }
-            },
+        Row(
             modifier = Modifier
-                .alpha(commentsAlpha)
-                .padding(8.dp)
+                .padding(16.dp)
+                .fillMaxWidth()
+                .height(60.dp),
+            horizontalArrangement = Arrangement.SpaceAround,
+            verticalAlignment = Alignment.Top
         ) {
-            if (isFrontLayerDisabled == true) {
+            var search by remember { mutableStateOf("") }
+            OutlinedTextField(
+                value = search,
+                onValueChange = {
+                    search = it
+                    closeButtonVisibility = when (search) {
+                        "" -> 0f
+                        else -> 1f
+                    }
+                },
+                modifier = Modifier
+                    .padding(horizontal = 5.dp)
+                    .background(Color.White, shape = CircleShape)
+                    .shadow(
+                        elevation = 8.dp,
+                        ambientColor = Color.Black.copy(alpha = 0.1f),
+                        shape = RoundedCornerShape(30.dp),
+                        clip = false
+                    )
+                    .offset(0.dp, 0.dp)
+                    .background(color = Color.White, shape = RoundedCornerShape(30.dp))
+                    .weight(1 - (weightValue + weightValueArrow))
+                    .alignBy(FirstBaseline),
+                shape = RoundedCornerShape(30.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.White,
+                    unfocusedBorderColor = Color.White,
+                    cursorColor = themeColor
+                ),
+                textStyle = LocalTextStyle.current.copy(fontSize = 14.sp),
+                enabled = isFrontLayerDisabled != true,
+                leadingIcon = {
+                    Icon(
+                        painter = searchResource,
+                        contentDescription = "",
+                        tint = themeColor
+                    )
+                },
+                trailingIcon = {
+                    IconButton(
+                        modifier = Modifier
+                            .alpha(closeButtonVisibility),
+                        onClick = {
+                            search = ""
+                            closeButtonVisibility = 0f
+                        }
+                    ) {
+                        Icon(
+                            painter = closeResource,
+                            contentDescription = ""
+                        )
+                    }
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        Timber.tag("SEARCH_DATA").v(search)
+                    },
+                    onDone = {
+                        focusManager.clearFocus()
+                    }
+                ),
+                singleLine = true,
+                placeholder = {
+                    Text(text = stringResource(id = R.string.search_placeholder))
+                }
+            )
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .weight(weightValue)
+                    .alignBy(FirstBaseline)
+            ) {
                 Icon(
-                    resource,
-                    contentDescription = null,
-                    tint = themeColor
-                )
-            } else {
-                Icon(
-                    resource,
-                    contentDescription = null,
+                    painter = qrcodeResource,
+                    contentDescription = "",
                     tint = themeColor
                 )
             }
+            AnimatedVisibility(
+                visible = backdropState.isRevealed
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch { backdropState.conceal() }
+                    },
+                    modifier = Modifier
+                        .weight(weightValueArrow, weightValueArrowStatus)
+                        .alignBy(FirstBaseline)
+                ) {
+                    if (isFrontLayerDisabled == true) {
+                        Icon(
+                            resource,
+                            contentDescription = null,
+                            tint = themeColor
+                        )
+                    } else {
+                        Icon(
+                            resource,
+                            contentDescription = null,
+                            tint = themeColor
+                        )
+                    }
+                }
+            }
+        }
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Table content", textAlign = TextAlign.Center)
         }
     }
 }
