e49481292393663aca3981f7abc86fdf8c95e031
andresmr <andres@andresmr.com>
2022-11-24 12:50:33 +0100

    fix GranularSyncPresenterImpl.kt

--- app/src/main/java/org/dhis2/utils/granularsync/GranularSyncPresenterImpl.kt
+++ app/src/main/java/org/dhis2/utils/granularsync/GranularSyncPresenterImpl.kt
@@ -402,7 +402,6 @@ class GranularSyncPresenterImpl(
                     updateStatusToSentBySMS()
                 }
             }
-            else -> {}
         }
         restartSmsSender()
     }
