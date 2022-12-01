4df3858f9133486fcb2578e7bea6885fcddf81ba
Carlos Macaneta <carlosmacaneta@hotmail.com>
2022-12-01 11:14:18 +0200

    Implementing table dimensions

--- stock-managment/src/main/java/org/dhis2/android/rtsm/ui/managestock/components/ManageStockTable.kt
+++ stock-managment/src/main/java/org/dhis2/android/rtsm/ui/managestock/components/ManageStockTable.kt
@@ -3,9 +3,12 @@ package org.dhis2.android.rtsm.ui.managestock.components
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.unit.dp
 import com.google.android.material.composethemeadapter.MdcTheme
 import org.dhis2.android.rtsm.ui.managestock.ManageStockViewModel
 import org.dhis2.composetable.ui.DataSetTableScreen
+import org.dhis2.composetable.ui.TableDimensions
+import org.dhis2.composetable.ui.TableTheme
 
 @Composable
 fun ManageStockTable(
@@ -15,17 +18,22 @@ fun ManageStockTable(
     val screenState by viewModel.screenState.observeAsState()
 
     MdcTheme {
-        DataSetTableScreen(
-            tableScreenState = screenState!!,
-            onCellClick = { _, cell ->
-                viewModel.onCellClick(cell)
-            },
-            onEdition = { isEditing ->
-                editingCellValue(isEditing, concealBackdropState)
-            },
-            onCellValueChange = viewModel::onCellValueChanged,
-            onSaveValue = viewModel::onSaveValueChange
-        )
+        TableTheme(
+            tableColors = null,
+            tableDimensions = TableDimensions(defaultRowHeaderWidth = 200.dp)
+        ) {
+            DataSetTableScreen(
+                tableScreenState = screenState!!,
+                onCellClick = { _, cell ->
+                    viewModel.onCellClick(cell)
+                },
+                onEdition = { isEditing ->
+                    editingCellValue(isEditing, concealBackdropState)
+                },
+                onCellValueChange = viewModel::onCellValueChanged,
+                onSaveValue = viewModel::onSaveValueChange
+            )
+        }
     }
 }
 
