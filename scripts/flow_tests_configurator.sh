41fee9adb3bc94939c54e5df5a5d1961e7474f94
andresmr <personal@andresmr.com>
2022-12-05 08:57:35 +0100

    Update to pixel 3 tier 3

--- scripts/flow_tests_configurator.sh
+++ scripts/flow_tests_configurator.sh
@@ -8,4 +8,4 @@ sed -i -e 's/build_time_average=660/build_time_average=100/g' config.init
 sed -i -e 's/org.dhis2.usescases.UseCaseTestsSuite/org.dhis2.usescases.FlowTestsSuite/g' config.init
 
 #Change device to pixel 3a
-sed -i -e 's/Samsung Galaxy S10/Google Pixel 3a/g' config.init
+sed -i -e 's/Samsung Galaxy S10/Google Pixel 3/g' config.init
