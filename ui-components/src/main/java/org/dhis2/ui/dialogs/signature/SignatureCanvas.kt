31f6387de696071da923604eac969dd3c4a44315
manu <mmunoz@quadram.mobi>
2023-01-26 11:40:10 +0100

    feat: [ANDROAPP-4986] fit to design

--- ui-components/src/main/java/org/dhis2/ui/dialogs/signature/SignatureCanvas.kt
+++ ui-components/src/main/java/org/dhis2/ui/dialogs/signature/SignatureCanvas.kt
@@ -4,6 +4,8 @@ import android.view.MotionEvent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
@@ -16,34 +18,32 @@ import androidx.compose.ui.input.pointer.pointerInteropFilter
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun SignatureCanvas(modifier: Modifier = Modifier, path: Path, onDraw: () -> Unit) {
-    val movePath = remember { mutableStateOf<Offset?>(null) }
-
+fun SignatureCanvas(modifier: Modifier = Modifier, drawing: MutableState<Offset?>) {
+    val path by remember { mutableStateOf(Path()) }
     Canvas(
         modifier = modifier
             .fillMaxSize()
             .pointerInteropFilter {
                 when (it.action) {
                     MotionEvent.ACTION_DOWN -> {
-                        movePath.value = Offset(it.x, it.y)
+                        drawing.value = Offset(it.x, it.y)
                         path.moveTo(it.x, it.y)
                     }
                     MotionEvent.ACTION_MOVE -> {
-                        movePath.value = Offset(it.x, it.y)
+                        drawing.value = Offset(it.x, it.y)
                         path.lineTo(it.x, it.y)
-                        onDraw()
                     }
                 }
                 true
             }
     ) {
-        movePath.value?.let {
+        drawing.value?.let {
             drawPath(
                 path = path,
                 color = Color.Black,
                 alpha = 1f,
                 style = Stroke(7f)
             )
-        }
+        } ?: path.reset()
     }
 }
--- ui-components/src/main/java/org/dhis2/ui/dialogs/signature/SignatureDialogUi.kt
+++ ui-components/src/main/java/org/dhis2/ui/dialogs/signature/SignatureDialogUi.kt
@@ -14,7 +14,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
@@ -22,9 +25,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.layout.boundsInRoot
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalView
@@ -47,15 +50,18 @@ fun SignatureDialogUi(
 ) {
 
     var capturingViewBounds: Rect? = null
-    val path by remember { mutableStateOf(Path()) }
     val view = LocalView.current
-    var isSigned by remember { mutableStateOf(false) }
+
+    var capturing by remember { mutableStateOf(false) }
+    val drawing = remember { mutableStateOf<Offset?>(null) }
+    val isSigned by remember { derivedStateOf { drawing.value != null } }
+
 
     Column(
         modifier = Modifier
             .background(Color.White, RoundedCornerShape(16.dp))
             .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Text(
             text = title,
@@ -64,76 +70,82 @@ fun SignatureDialogUi(
                 color = textSecondary
             )
         )
-        Text(
-            modifier = Modifier
-                .align(Alignment.End)
-                .background(
-                    MaterialTheme.colorScheme.primary,
-                    RoundedCornerShape(6.dp, 6.dp, 6.dp, 0.dp)
-                )
-                .padding(8.dp, 4.dp),
-            text = stringResource(R.string.draw_here),
-            style = TextStyle(
-                fontSize = 10.sp,
-                color = Color.White
-            ),
-        )
-        SignatureCanvas(
-            modifier = Modifier
-                .height(200.dp)
-                .dashedBorder(
-                    strokeWidth = 1.dp,
-                    color = textSecondary.copy(alpha = 0.3f),
-                    cornerRadiusDp = 8.dp
-                )
-                .onGloballyPositioned {
-                    capturingViewBounds = it.boundsInRoot()
-                },
-            path = path,
-            onDraw = { isSigned = true }
-        )
-        Row {
-            IconButton(
-                enabled = isSigned,
-                onClick = {
-                    path.reset()
-                    isSigned = false
-                }) {
-                Icon(
-                    imageVector = Icons.Default.Clear,
-                    contentDescription = stringResource(R.string.clear),
-                    tint = MaterialTheme.colorScheme.primary
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End)
-            ) {
-                Button(
-                    onClick = onCancel,
-                    colors = ButtonDefaults.buttonColors(
-                        contentColor = MaterialTheme.colorScheme.primary,
-                        containerColor = Color.White,
-                        disabledContainerColor = Color.White
+        Box {
+            SignatureCanvas(
+                modifier = Modifier
+                    .height(200.dp)
+                    .dashedBorder(
+                        strokeWidth = 1.dp,
+                        color = textSecondary.copy(alpha = 0.3f),
+                        cornerRadiusDp = 8.dp
                     )
-                ) {
-                    Text(text = stringResource(R.string.cancel))
-                }
-                Button(
-                    onClick = {
-                        capturingViewBounds
-                            ?.captureBitmap(view)
-                            ?.let { onSave(it) }
+                    .onGloballyPositioned {
+                        capturingViewBounds = it.boundsInRoot()
                     },
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = MaterialTheme.colorScheme.primary
-                    ),
-                    enabled = isSigned
-                ) {
-                    Text(text = stringResource(R.string.save))
+                drawing = drawing
+            )
+            if (!capturing) {
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.TopEnd)
+                        .background(
+                            MaterialTheme.colorScheme.primary,
+                            RoundedCornerShape(6.dp, 6.dp, 6.dp, 0.dp)
+                        )
+                        .padding(8.dp, 4.dp),
+                    text = stringResource(R.string.draw_here),
+                    style = TextStyle(
+                        fontSize = 10.sp,
+                        color = MaterialTheme.colorScheme.onPrimary
+                    )
+                )
+                IconButton(
+                    modifier = Modifier.align(Alignment.BottomEnd),
+                    enabled = isSigned,
+                    onClick = {
+                        drawing.value = null
+                    }) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = stringResource(R.string.clear),
+                        tint = MaterialTheme.colorScheme.primary
+                    )
                 }
             }
         }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.End)
+        ) {
+            TextButton(
+                onClick = onCancel
+            ) {
+                Text(
+                    text = stringResource(R.string.cancel),
+                    color = MaterialTheme.colorScheme.primary
+                )
+            }
+            Button(
+                onClick = {
+                    capturing = true
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                enabled = isSigned
+            ) {
+                Text(text = stringResource(R.string.save))
+            }
+        }
+    }
+
+    LaunchedEffect(capturing) {
+        capturingViewBounds
+            ?.takeIf { capturing }
+            ?.captureBitmap(view)
+            ?.let { onSave(it) }
+
     }
 }
 
