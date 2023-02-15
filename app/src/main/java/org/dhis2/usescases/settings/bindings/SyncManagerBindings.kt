54db327fae38ef146f8e3053f8192c29e5424e96
andresmr <andres@dhis2.org>
2023-02-15 12:47:31 +0100

    [ANDROAPP-5032] add text style

--- app/src/main/java/org/dhis2/usescases/settings/bindings/SyncManagerBindings.kt
+++ app/src/main/java/org/dhis2/usescases/settings/bindings/SyncManagerBindings.kt
@@ -3,13 +3,24 @@ package org.dhis2.usescases.settings.bindings
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.databinding.BindingAdapter
+import org.dhis2.R
+import org.dhis2.ui.theme.colorPrimary
 
 @BindingAdapter("addSyncButton")
 fun ComposeView.addSyncButton(onClick: () -> Unit) {
     setContent {
         TextButton(onClick = onClick) {
-            Text(text = "Sync data now")
+            Text(
+                text = "Sync data now".uppercase(),
+                color = colorPrimary,
+                fontSize = 12.sp,
+                fontFamily = FontFamily(Font(R.font.rubik_medium))
+            )
         }
     }
 }
