8f364f2ac7b2a3300725e13d226588141fa92893
andresmr <andres@dhis2.org>
2023-02-17 11:53:24 +0100

    [ANDROAPP-5116] Replace icons

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/EventCaptureActivity.java
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/EventCaptureActivity.java
@@ -191,7 +191,7 @@ public class EventCaptureActivity extends ActivityGlobalAbstract implements Even
                     getString(R.string.title_delete_go_back),
                     getString(R.string.discard_go_back),
                     null,
-                    R.drawable.ic_alert,
+                    R.drawable.ic_error_outline,
                     Collections.emptyList(),
                     new DialogButtonStyle.MainButton(R.string.keep_editing),
                     new DialogButtonStyle.DiscardButton()
