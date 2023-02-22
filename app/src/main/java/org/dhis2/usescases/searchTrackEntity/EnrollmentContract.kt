75d9d74101a0312b8857a4ea75b5ff721f14a784
Pablo <ppajueloc@gmail.com>
2023-02-22 15:21:03 +0100

    fix: [ANDROAPP-5132] Go back to relationship fragment when TEI is selected/created

---
+++ app/src/main/java/org/dhis2/usescases/searchTrackEntity/EnrollmentContract.kt
@@ -0,0 +1,44 @@
+package org.dhis2.usescases.searchTrackEntity
+
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+import org.dhis2.usescases.enrollment.EnrollmentActivity
+
+const val TEI_A_UID = "TEI_A_UID"
+
+class EnrollmentContract : ActivityResultContract<EnrollmentInput, EnrollmentResult>() {
+    private lateinit var enrollmentUid: String
+    override fun createIntent(context: Context, input: EnrollmentInput): Intent {
+        enrollmentUid = input.enrollmentUid
+        return EnrollmentActivity.getIntent(
+            context,
+            input.enrollmentUid,
+            input.programUid,
+            input.enrollmentMode,
+            input.forRelationship
+        )
+    }
+
+    override fun parseResult(resultCode: Int, intent: Intent?): EnrollmentResult {
+        return intent?.getStringExtra(TEI_A_UID)?.let {
+            EnrollmentResult.RelationshipResult(it)
+        } ?: EnrollmentResult.Success(enrollmentUid)
+    }
+}
+
+data class EnrollmentInput(
+    val enrollmentUid: String,
+    val programUid: String,
+    val enrollmentMode: EnrollmentActivity.EnrollmentMode,
+    val forRelationship: Boolean
+)
+
+sealed class EnrollmentResult {
+    data class Success(val enrollmentUid: String) : EnrollmentResult()
+    data class RelationshipResult(val teiUid: String) : EnrollmentResult() {
+        fun data() = Intent().apply {
+            putExtra(TEI_A_UID, teiUid)
+        }
+    }
+}
