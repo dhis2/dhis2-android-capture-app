baea2bb68094b1c77640e36989603883175821c5
Pablo <ppajueloc@gmail.com>
2023-02-24 10:28:20 +0100

    fix: Restore null max account (#3049)

--- app/src/main/java/org/dhis2/usescases/login/accounts/ui/AccountsScreen.kt
+++ app/src/main/java/org/dhis2/usescases/login/accounts/ui/AccountsScreen.kt
@@ -57,7 +57,7 @@ fun AccountsScreen(
                     .clip(RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp))
                     .background(Color.White)
             ) {
-                LazyColumn(Modifier.padding(top = 16.dp, bottom = 48.dp)) {
+                LazyColumn(Modifier.weight(1f).padding(top = 16.dp)) {
                     items(accounts) {
                         AccountItem(
                             Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
@@ -69,8 +69,7 @@ fun AccountsScreen(
                 Column(Modifier.padding(16.dp)) {
                     Button(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 22.dp),
+                            .fillMaxWidth(),
                         shape = RoundedCornerShape(8.dp),
                         colors = ButtonDefaults.buttonColors(
                             backgroundColor = colorResource(id = R.color.colorPrimary),
@@ -126,12 +125,28 @@ fun LoginHeader() {
 @Preview(showBackground = true)
 @Composable
 fun AccountsPreview() {
+    val accounts = mutableListOf<AccountModel>().apply {
+        repeat(13) {
+            add(AccountModel("android", "https://play.dhis2.com/android-dev"))
+        }
+    }
     AccountsScreen(
-        listOf(
-            AccountModel("android", "https://play.dhis2.com/android-dev"),
-            AccountModel("android", "https://play.dhis2.com/android-current"),
-            AccountModel("admin", "https://play.dhis2.com/android-dev")
-        ),
+        accounts,
+        {},
+        {}
+    )
+}
+@ExperimentalMaterialApi
+@Preview(showBackground = true)
+@Composable
+fun FewAccountsPreview() {
+    val accounts = mutableListOf<AccountModel>().apply {
+        repeat(3) {
+            add(AccountModel("android", "https://play.dhis2.com/android-dev"))
+        }
+    }
+    AccountsScreen(
+        accounts,
         {},
         {}
     )
