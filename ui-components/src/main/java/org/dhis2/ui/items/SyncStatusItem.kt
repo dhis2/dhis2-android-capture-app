9589011bb53d5797ebe7a3e8ead805ebdb0e3047
Pablo <ppajueloc@gmail.com>
2023-03-02 16:10:25 +0100

    clip click on item

--- ui-components/src/main/java/org/dhis2/ui/items/SyncStatusItem.kt
+++ ui-components/src/main/java/org/dhis2/ui/items/SyncStatusItem.kt
@@ -2,15 +2,21 @@ package org.dhis2.ui.items
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,7 +38,9 @@ fun SyncStatusItem(
             .fillMaxWidth()
             .background(
                 color = MaterialTheme.colorScheme.surface
-            ).clickable { onClick() },
+            )
+            .clip(RoundedCornerShape(8.dp))
+            .clickable{ onClick() },
         horizontalArrangement = spacedBy(8.dp)
     ) {
         syncStatusIcon()
@@ -56,7 +64,7 @@ fun SyncStatusItem(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun SyncStatusItemPreview() {
     SyncStatusItem(
