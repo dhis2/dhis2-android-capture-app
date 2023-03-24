11aaf7922038416b82b31df26302402b14b60285
Pablo <ppajueloc@gmail.com>
2023-03-24 13:31:11 +0100

    fix: missing mock response in login test mock server

--- app/src/androidTest/java/org/dhis2/usescases/login/LoginTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/login/LoginTest.kt
@@ -19,7 +19,6 @@ import org.hisp.dhis.android.core.D2Manager
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.API_ME_PATH
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.API_SYSTEM_INFO_PATH
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.GET
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -42,10 +41,10 @@ class LoginTest : BaseTest() {
         D2Manager.removeCredentials()
     }
 
-    @Ignore("To be fixed in the mock server issue")
     @Test
     fun shouldLoginSuccessfullyWhenCredentialsAreRight() {
         mockWebServerRobot.addResponse(GET, API_ME_PATH, API_ME_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, PATH_APPS, API_ME_RESPONSE_OK)
         mockWebServerRobot.addResponse(GET, API_SYSTEM_INFO_PATH, API_SYSTEM_INFO_RESPONSE_OK)
         mockWebServerRobot.addResponse(GET, PATH_WEBAPP_REGEX, API_METADATA_SETTINGS_RESPONSE_ERROR)
 
@@ -164,9 +163,13 @@ class LoginTest : BaseTest() {
         )
     }
 
-    @Ignore("To be fixed in the mock server issue")
     @Test
     fun shouldDisplayShareDataDialogAndOpenPrivacyPolicy() {
+        mockWebServerRobot.addResponse(GET, API_ME_PATH, API_ME_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, PATH_APPS, API_ME_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, API_SYSTEM_INFO_PATH, API_SYSTEM_INFO_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, PATH_WEBAPP_REGEX, API_METADATA_SETTINGS_RESPONSE_ERROR)
+
         enableIntents()
         startLoginActivity()
 
@@ -206,6 +209,7 @@ class LoginTest : BaseTest() {
         const val API_METADATA_SETTINGS_DATASET_RESPONSE_ERROR =
             "mocks/settingswebapp/datasetsettings_404.json"
         const val PATH_WEBAPP_REGEX = "/api/dataStore/ANDROID_SETTING_APP/general_settings?.*"
+        const val PATH_APPS = "/api/apps?.*"
         const val DB_GENERATED_BY_LOGIN = "127-0-0-1-8080_test_unencrypted.db"
         const val PIN_PASSWORD = 1234
 
