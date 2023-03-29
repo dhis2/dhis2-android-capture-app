6ed672d28840f352a84b813441ce246e0d81054d
Pablo <ppajueloc@gmail.com>
2023-03-29 07:46:19 +0200

    fix: missing mock response in login test mock server (#3097)
    Co-authored-by: andresmr <andres@dhis2.org>

--- app/src/androidTest/java/org/dhis2/usescases/login/LoginTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/login/LoginTest.kt
@@ -6,25 +6,21 @@ import android.content.Intent
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import org.dhis2.commons.Constants.EXTRA_DATA
 import org.dhis2.commons.prefs.Preference.Companion.PIN
 import org.dhis2.commons.prefs.Preference.Companion.SESSION_LOCKED
 import org.dhis2.usescases.BaseTest
 import org.dhis2.usescases.main.MainActivity
 import org.dhis2.usescases.qrScanner.ScanActivity
-import org.dhis2.commons.Constants.EXTRA_DATA
 import org.hamcrest.CoreMatchers.allOf
 import org.hisp.dhis.android.core.D2Manager
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.API_ME_PATH
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.API_SYSTEM_INFO_PATH
 import org.hisp.dhis.android.core.mockwebserver.ResponseController.GET
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class LoginTest : BaseTest() {
 
     @get:Rule
@@ -42,12 +38,13 @@ class LoginTest : BaseTest() {
         D2Manager.removeCredentials()
     }
 
-    @Ignore("To be fixed in the mock server issue")
     @Test
     fun shouldLoginSuccessfullyWhenCredentialsAreRight() {
         mockWebServerRobot.addResponse(GET, API_ME_PATH, API_ME_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, PATH_APPS, API_ME_RESPONSE_OK)
         mockWebServerRobot.addResponse(GET, API_SYSTEM_INFO_PATH, API_SYSTEM_INFO_RESPONSE_OK)
-        mockWebServerRobot.addResponse(GET, PATH_WEBAPP_REGEX, API_METADATA_SETTINGS_RESPONSE_ERROR)
+        mockWebServerRobot.addResponse(GET, PATH_WEBAPP_GENERAL_SETTINGS, API_METADATA_SETTINGS_RESPONSE_ERROR)
+        mockWebServerRobot.addResponse(GET, PATH_WEBAPP_INFO, API_METADATA_SETTINGS_INFO_ERROR)
 
         enableIntents()
         startLoginActivity()
@@ -164,9 +161,13 @@ class LoginTest : BaseTest() {
         )
     }
 
-    @Ignore("To be fixed in the mock server issue")
     @Test
     fun shouldDisplayShareDataDialogAndOpenPrivacyPolicy() {
+        mockWebServerRobot.addResponse(GET, API_ME_PATH, API_ME_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, PATH_APPS, API_ME_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, API_SYSTEM_INFO_PATH, API_SYSTEM_INFO_RESPONSE_OK)
+        mockWebServerRobot.addResponse(GET, PATH_WEBAPP_GENERAL_SETTINGS, API_METADATA_SETTINGS_RESPONSE_ERROR)
+
         enableIntents()
         startLoginActivity()
 
@@ -205,7 +206,10 @@ class LoginTest : BaseTest() {
             "mocks/settingswebapp/programsettings_404.json"
         const val API_METADATA_SETTINGS_DATASET_RESPONSE_ERROR =
             "mocks/settingswebapp/datasetsettings_404.json"
-        const val PATH_WEBAPP_REGEX = "/api/dataStore/ANDROID_SETTING_APP/general_settings?.*"
+        const val API_METADATA_SETTINGS_INFO_ERROR = "mocks/settingswebapp/infosettings_404.json"
+        const val PATH_WEBAPP_GENERAL_SETTINGS = "/api/dataStore/ANDROID_SETTING_APP/general_settings?.*"
+        const val PATH_WEBAPP_INFO = "/api/dataStore/ANDROID_SETTING_APP/info?.*"
+        const val PATH_APPS = "/api/apps?.*"
         const val DB_GENERATED_BY_LOGIN = "127-0-0-1-8080_test_unencrypted.db"
         const val PIN_PASSWORD = 1234
 
