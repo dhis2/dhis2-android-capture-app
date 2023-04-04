c1d5e034cad79fa987368a08587993afbf170b16
FerdyRod <ferdyrodriguez@gmail.com>
2023-04-04 14:04:31 +0200

    fix: fix ktlint

--- ui-components/src/main/java/org/dhis2/ui/IconTextButton.kt
+++ ui-components/src/main/java/org/dhis2/ui/IconTextButton.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
