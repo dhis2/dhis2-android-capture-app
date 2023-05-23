a2e961cfb21dc6bb195245bf18522539e9324c8c
Carlos Macaneta <carlosmacaneta@hotmail.com>
2023-05-23 12:04:36 +0200

    Revert "improving to padding container best practices"
    This reverts commit 242c77c2260d5b7ada125313a124cdf8a730b2f5.

--- stock-usecase/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
+++ stock-usecase/src/main/java/org/dhis2/android/rtsm/ui/home/screens/components/BackdropComponent.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentManager
@@ -43,7 +42,6 @@ import org.dhis2.ui.dialogs.bottomsheet.DialogButtonStyle
 @Composable
 fun Backdrop(
     activity: Activity,
-    modifier: Modifier = Modifier,
     viewModel: HomeViewModel,
     manageStockViewModel: ManageStockViewModel,
     themeColor: Color,
@@ -57,9 +55,9 @@ fun Backdrop(
     val settingsUiState by viewModel.settingsUiState.collectAsState()
     val dataEntryUiState by manageStockViewModel.dataEntryUiState.collectAsState()
     val scope = rememberCoroutineScope()
-    val bottomSheetState by manageStockViewModel.bottomSheetState.collectAsState()
+    val bottomSheetState = manageStockViewModel.bottomSheetState.collectAsState()
 
-    if (bottomSheetState) {
+    if (bottomSheetState.value) {
         launchBottomSheet(
             activity.getString(R.string.not_saved),
             activity.getString(R.string.transaction_not_confirmed),
@@ -81,7 +79,6 @@ fun Backdrop(
     manageStockViewModel.backToListing()
     DisplaySnackBar(manageStockViewModel, scaffoldState)
     BackdropScaffold(
-        modifier = modifier,
         appBar = {
             Toolbar(
                 settingsUiState.transactionType.name,
@@ -196,7 +193,6 @@ private fun launchBottomSheet(
         onSecondaryButtonClicked = { onDiscard.invoke() }
     ).apply {
         this.show(supportFragmentManager.beginTransaction(), "DIALOG")
-        this.isCancelable = false
     }
 }
 
