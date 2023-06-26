c7d87c5ef388507d7dfc2a8f0cbfcf904417b0f9
Pablo <ppajueloc@gmail.com>
2023-06-26 16:09:27 +0200

    migrate settings.gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+include(":app", ":viewpagerdotsindicator",
+        ":dhis_android_analytics", ":form", ":commons",
+        ":dhis2_android_maps", ":compose-table", ":ui-components",
+        ":stock-usecase")
