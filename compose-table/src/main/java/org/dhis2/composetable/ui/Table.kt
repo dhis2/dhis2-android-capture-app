3ecef95eb9b45bd786009c9ddb8f1f6234443d81
FerdyRod <ferdyrodriguez@gmail.com>
2023-07-11 15:51:18 +0200

    remove unused imports

--- compose-table/src/main/java/org/dhis2/composetable/ui/Table.kt
+++ compose-table/src/main/java/org/dhis2/composetable/ui/Table.kt
@@ -4,13 +4,10 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
