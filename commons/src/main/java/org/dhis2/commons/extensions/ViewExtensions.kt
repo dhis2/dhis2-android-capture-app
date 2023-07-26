cebdc5987761ba815d5e0237c540b4646035fdac
Andrés Miguel Rubio <andres@dhis2.org>
2023-07-26 12:48:22 +0200

    [ANDROAPP-5403] post closeKeyboard (#3235)

--- commons/src/main/java/org/dhis2/commons/extensions/ViewExtensions.kt
+++ commons/src/main/java/org/dhis2/commons/extensions/ViewExtensions.kt
@@ -5,13 +5,15 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 
 fun View.closeKeyboard() {
-    val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(windowToken, 0)
+    post {
+        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(windowToken, 0)
+    }
 }
 
 fun View.openKeyboard() {
     post {
         val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(this, InputMethodManager.SHOW_FORCED)
+        imm.showSoftInput(this, 0)
     }
 }
