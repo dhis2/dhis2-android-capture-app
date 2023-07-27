4b96a131a354ee8827f8d5172d7ce67906d7e6b4
Xavier Molloy <xavi.molloy@gmail.com>
2023-07-28 00:00:43 +0200

    code style correction

--- app/src/test/java/org/dhis2/bindings/TEICardExtensionsTest.kt
+++ app/src/test/java/org/dhis2/bindings/TEICardExtensionsTest.kt
@@ -2,11 +2,8 @@ package org.dhis2.bindings
 
 import android.content.Context
 import org.dhis2.Bindings.getEnrollmentIconsData
-import org.dhis2.commons.data.EnrollmentIconData
 import org.dhis2.commons.resources.ColorUtils
 import org.dhis2.commons.resources.ResourceManager
-import org.dhis2.maps.carousel.CarouselTeiHolder
-import org.hisp.dhis.android.core.enrollment.Enrollment
 import org.hisp.dhis.android.core.program.Program
 import org.junit.Test
 import org.mockito.kotlin.doReturn
@@ -15,23 +12,20 @@ import org.mockito.kotlin.whenever
 
 class TEICardExtensionsTest {
 
-
     private val program: Program = mock()
-    private val programInfo : List<Program> = listOf(program)
+    private val programInfo: List<Program> = listOf(program)
     private val context: Context = mock()
 
-    //TODO finish implementing unit tests for following cases:
-    //less than 4 enrollments, all enrollmentdata objects should  be icons.
+    // TODO finish implementing unit tests for following cases:
+    // less than 4 enrollments, all enrollmentdata objects should  be icons.
     // 4 enrollments, all should be icons
-    //more than 4 enrollments, should return data list of four elements but last element should
-    //not be an icon (enrollmentIconData.isIcon == false).
+    // more than 4 enrollments, should return data list of four elements but last element should
+    // not be an icon (enrollmentIconData.isIcon == false).
     @Test
     fun `Should return list of enrollment data icon objects smaller than 4 `() {
-
         mockEnrollmentIconsData(context, "programUID1")
-        val result = programInfo.getEnrollmentIconsData(context,"programUID1")
-        assert(result.size<=4)
-
+        val result = programInfo.getEnrollmentIconsData(context, "programUID1")
+        assert(result.size <= 4)
     }
 
     private fun mockEnrollmentIconsData(context: Context, currentProgram: String?) {
@@ -40,11 +34,12 @@ class TEICardExtensionsTest {
         whenever(program.style().color()).doReturn("color")
         whenever(program.style().icon()).doReturn("icon")
 
-        whenever(ColorUtils.getColorFrom("color",mock())).doReturn(mock())
+        whenever(ColorUtils.getColorFrom("color", mock())).doReturn(mock())
         whenever(ColorUtils.getPrimaryColor(context, ColorUtils.ColorType.PRIMARY)).doReturn(mock())
 
-        whenever(ResourceManager(context).getObjectStyleDrawableResource(program.style().icon(), mock())).doReturn(444)
-        whenever(ColorUtils.getColorFrom(mock(),mock())).doReturn(mock())
-
+        whenever(
+            ResourceManager(context).getObjectStyleDrawableResource(program.style().icon(), mock())
+        ).doReturn(444)
+        whenever(ColorUtils.getColorFrom(mock(), mock())).doReturn(mock())
     }
 }
