c9a40858313d2e71af2cdfc6a95f46a10006944d
FerdyRod <ferdyrodriguez@gmail.com>
2023-08-04 12:39:39 +0200

    test will be done on another issue.

--- app/src/test/java/org/dhis2/bindings/TEICardExtensionsTest.kt
+++ app/src/test/java/org/dhis2/bindings/TEICardExtensionsTest.kt
@@ -1,28 +1,12 @@
 package org.dhis2.bindings
 
 import android.content.Context
-import android.content.res.TypedArray
-import android.util.TypedValue
-import org.dhis2.Bindings.getEnrollmentIconsData
-import org.dhis2.commons.resources.ColorUtils
-import org.dhis2.commons.resources.ResourceManager
-import org.hisp.dhis.android.core.program.Program
-import org.junit.Ignore
-import org.junit.Test
-import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
-
 
 class TEICardExtensionsTest {
 
-    private val program: Program = mock()
-    private val programInfo: List<Program> = listOf(program,program, program, program)
     private val context: Context = mock()
-    private val  typedValue : TypedValue = mock();
-    private val intArray: IntArray = intArrayOf(4)
-    private  val typedIntArray: TypedArray = mock()
-    private val typedArray: TypedArray = mock()
+    /*private val colorUtils: ColorUtils = mock()
 
     // TODO finish implementing unit tests for following cases:
     // less than 4 enrollments, all enrollmentdata objects should  be icons.
@@ -30,30 +14,37 @@ class TEICardExtensionsTest {
     // more than 4 enrollments, should return data lis]t of four elements but last element should
     // not be an icon (enrollmentIconData.isIcon == false).
 
-    @Ignore
+    //@Ignore
     @Test
     fun `Should return list of enrollment data icon objects smaller than 4 `() {
-        mockEnrollmentIconsData(context, "programUID1")
-        val result = programInfo.getEnrollmentIconsData(context, "programUID1")
+        val programs: List<Program> = listOf(
+            createProgram("uid_1", "blue", "icon"),
+            createProgram("uid_2", "green", "icon"),
+            createProgram("uid_3", "red", "icon"),
+            createProgram("uid_4", "orange", "icon"),
+        )
+        mockEnrollmentIconsData(programs)
+        val result = programs.getEnrollmentIconsData(context, "uid_1")
         assert(result.size <= 4)
     }
 
-    private fun mockEnrollmentIconsData(context: Context, currentProgram: String?) {
-        whenever(program.uid()).doReturn("programUID2")
-        whenever(program.style()).doReturn(mock())
-        whenever(program.style().color()).doReturn("color")
-        whenever(program.style().icon()).doReturn("icon")
-        whenever(context.obtainStyledAttributes(null, intArray )).doReturn(typedArray)
-        whenever(typedArray.getColor(0,0) ).doReturn(777)
+    private fun mockEnrollmentIconsData(programs: List<Program>) {
 
         //this is where the problem occurs
         //todo migrate ColorUtils file to kotlin to avoid null pointer exception
         //whenever(typedValue).doReturn(mock())
-        whenever(ColorUtils.getPrimaryColor(context, ColorUtils.ColorType.PRIMARY)).doReturn(4)
+        whenever(ColorUtils.getColorFrom("blue", 1)) doReturn  1
+        whenever(ColorUtils.getPrimaryColor(context, ColorUtils.ColorType.PRIMARY)) doReturn 1
 
         // whenever(ColorUtils.getColorFrom("color", 444)).doReturn(4)
         // whenever(ColorUtils.getPrimaryColor(context, ColorUtils.ColorType.PRIMARY)).doReturn(mock())
-        whenever(ResourceManager(context).getObjectStyleDrawableResource(program.style().icon(), mock()) ).doReturn(444)
+        //whenever(ResourceManager(context).getObjectStyleDrawableResource("icon", 1)) doReturn any()
         //whenever(ColorUtils.getColorFrom(mock(), mock())).doReturn(mock())
     }
+
+    private fun createProgram(uid: String, color: String, icon: String) = Program
+        .builder()
+        .uid(uid)
+        .style(ObjectStyle.builder().color(color).icon(icon).build())
+        .build()*/
 }
