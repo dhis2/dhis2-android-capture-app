0d609735ef233729a0a561a654bb2a166a8d9d5a
FerdyRod <ferdyrodriguez@gmail.com>
2023-08-08 16:47:37 +0200

    fixes conflicts

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventDetails/injection/EventDetailsModule.kt
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventDetails/injection/EventDetailsModule.kt
@@ -9,7 +9,6 @@ import org.dhis2.commons.locationprovider.LocationProvider
 import org.dhis2.commons.network.NetworkUtils
 import org.dhis2.commons.prefs.PreferenceProvider
 import org.dhis2.commons.resources.ColorUtils
-import org.dhis2.commons.resources.D2ErrorUtils
 import org.dhis2.commons.resources.ResourceManager
 import org.dhis2.data.dhislogic.DhisPeriodUtils
 import org.dhis2.form.data.GeometryController
