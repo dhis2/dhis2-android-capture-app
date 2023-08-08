7f889ac16b9163a64134fb9ddc25727210685d0b
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-08-08 10:55:38 +0200

    Update HomeProgramViewModel.kt

--- app/src/main/java/org/dhis2/usescases/uiboost/viewmodels/HomeProgramViewModel.kt
+++ app/src/main/java/org/dhis2/usescases/uiboost/viewmodels/HomeProgramViewModel.kt
@@ -1,8 +1,11 @@
 package org.dhis2.usescases.uiboost.viewmodels
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.processors.PublishProcessor
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import org.dhis2.commons.filters.FilterManager
 import org.dhis2.commons.matomo.MatomoAnalyticsController
 import org.dhis2.commons.schedulers.SchedulerProvider
@@ -10,8 +13,10 @@ import org.dhis2.data.service.SyncStatusController
 import org.dhis2.usescases.main.program.IdentifyProgramType
 import org.dhis2.usescases.main.program.ProgramRepository
 import org.dhis2.usescases.main.program.ProgramView
+import org.dhis2.usescases.main.program.ProgramViewModel
 import org.dhis2.usescases.main.program.StockManagementMapper
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class HomeProgramViewModel @Inject internal constructor(
@@ -23,61 +28,69 @@ class HomeProgramViewModel @Inject internal constructor(
     private val syncStatusController: SyncStatusController,
     private val identifyProgramType: IdentifyProgramType,
     private val stockManagementMapper: StockManagementMapper
-): ViewModel() {
+) : ViewModel() {
     private val refreshData = PublishProcessor.create<Unit>()
     var disposable: CompositeDisposable = CompositeDisposable()
+
+    val _programs = MutableStateFlow<List<ProgramViewModel>>(emptyList())
+    val programs: StateFlow<List<ProgramViewModel>> = _programs
+
+    init {
+        getPrograms()
+    }
 
     fun getPrograms() {
         val applyFiler = PublishProcessor.create<FilterManager>()
         programRepository.clearCache()
 
-//        disposable.add(
-//            applyFiler
-//                .switchMap {
-//                    refreshData.debounce(
-//                        500,
-//                        TimeUnit.MILLISECONDS,
-//                        schedulerProvider.io()
-//                    ).startWith(Unit).switchMap {
-//                        programRepository.homeItems(
-//                            syncStatusController.observeDownloadProcess().value!!
-//                        )
-//                    }
-//                }
-//                .subscribeOn(schedulerProvider.io())
-//                .observeOn(schedulerProvider.ui())
-//                .subscribe(
-//                    { programs ->
-//                        this.programs.postValue(programs)
-//                        view.swapProgramModelData(programs)
-//                    },
-//                    { throwable -> Timber.d(throwable) },
-//                    { Timber.tag("INIT DATA").d("LOADING ENDED") }
-//                )
-//        )
-//
-//        disposable.add(
-//            filterManager.asFlowable()
-//                .startWith(filterManager)
-//                .subscribeOn(schedulerProvider.io())
-//                .observeOn(schedulerProvider.ui())
-//                .subscribe(
-//                    {
-//                        view.showFilterProgress()
-//                        applyFiler.onNext(filterManager)
-//                    },
-//                    { Timber.e(it) }
-//                )
-//        )
-//
-//        disposable.add(
-//            filterManager.ouTreeFlowable()
-//                .subscribeOn(schedulerProvider.io())
-//                .observeOn(schedulerProvider.ui())
-//                .subscribe(
-//                    { view.openOrgUnitTreeSelector() },
-//                    { Timber.e(it) }
-//                )
-//        )
+        disposable.add(
+            applyFiler
+                .switchMap {
+                    refreshData.debounce(
+                        500,
+                        TimeUnit.MILLISECONDS,
+                        schedulerProvider.io()
+                    ).startWith(Unit).switchMap {
+                        programRepository.homeItems(
+                            syncStatusController.observeDownloadProcess().value!!
+                        )
+                    }
+                }
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+                .subscribe(
+                    { programs ->
+                        _programs.value = programs
+                        view.swapProgramModelData(programs)
+                        Timber.tag("PGRAM").d("${programs}")
+                    },
+                    { throwable -> Timber.d(throwable) },
+                    { Timber.tag("INIT DATA").d("LOADING ENDED") }
+                )
+        )
+
+        disposable.add(
+            filterManager.asFlowable()
+                .startWith(filterManager)
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+                .subscribe(
+                    {
+                        view.showFilterProgress()
+                        applyFiler.onNext(filterManager)
+                    },
+                    { Timber.e(it) }
+                )
+        )
+
+        disposable.add(
+            filterManager.ouTreeFlowable()
+                .subscribeOn(schedulerProvider.io())
+                .observeOn(schedulerProvider.ui())
+                .subscribe(
+                    { view.openOrgUnitTreeSelector() },
+                    { Timber.e(it) }
+                )
+        )
     }
 }
