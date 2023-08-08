1beae913975994bd3d5025de3ee875e3a749e173
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-08-08 10:55:12 +0200

    Update Program.kt

--- app/src/main/java/org/dhis2/usescases/uiboost/data/model/Program.kt
+++ app/src/main/java/org/dhis2/usescases/uiboost/data/model/Program.kt
@@ -1,8 +1,11 @@
 package org.dhis2.usescases.uiboost.data.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonIncludeProperties
 import kotlinx.serialization.Serializable
 
 @Serializable
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Program(
     val hidden: String,
     val icon: String,
