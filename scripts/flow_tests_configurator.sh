94cf2fe5bd0e01eaee259031ffd93618d84a6f62
manu <mmunoz@quadram.mobi>
2023-08-08 11:16:28 +0200

    test: [ANDROAPP-5001] set devices

--- scripts/flow_tests_configurator.sh
+++ scripts/flow_tests_configurator.sh
@@ -6,3 +6,6 @@ sed -i -e 's/build_time_average=660/build_time_average=100/g' config.init
 
 # Change test suite to execute flows instead of use cases
 sed -i -e 's/org.dhis2.usescases.UseCaseTestsSuite/org.dhis2.usescases.FlowTestsSuite/g' config.init
+
+#Change device
+sed -i -e 's/Google Pixel 5/Samsung Galaxy A11/g' config.init
