41e3c12e2c3830036bd8e43afc86cfd1ae7be0db
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2023-08-08 10:55:17 +0200

    Update ProgramGroup.kt

--- app/src/main/java/org/dhis2/usescases/uiboost/data/model/ProgramGroup.kt
+++ app/src/main/java/org/dhis2/usescases/uiboost/data/model/ProgramGroup.kt
@@ -1,8 +1,10 @@
 package org.dhis2.usescases.uiboost.data.model
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import kotlinx.serialization.Serializable
 
 @Serializable
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ProgramGroup(
     val label: String,
     val order: Int,
