921cc0c8923bed276b0b793f528ff26a8bf7b3ec
andresmr <andres@dhis2.org>
2023-09-13 14:38:14 +0200

    [ANDROAPP-5469] Map no header items in FormSectionMapper

--- form/src/main/java/org/dhis2/form/ui/mapper/FormSectionMapper.kt
+++ form/src/main/java/org/dhis2/form/ui/mapper/FormSectionMapper.kt
@@ -10,24 +10,41 @@ class FormSectionMapper {
 
     fun mapFromFieldUiModelList(items: List<FieldUiModel>): List<FormSection> {
         val sections = mutableListOf<FormSection>()
-        items.forEach { item ->
-            if (item is SectionUiModelImpl) {
-                sections.add(
-                    FormSection(
-                        uid = item.uid,
-                        title = item.label,
-                        description = item.description,
-                        state = when (item.isOpen) {
-                            true -> SectionState.OPEN
-                            false -> SectionState.CLOSE
-                            null -> SectionState.FIXED
-                        },
-                        fields = items.filterIsInstance<FieldUiModelImpl>()
-                            .filter { it.programStageSection == item.uid },
-                    ),
-                )
+        if (hasSections(items)) {
+            items.forEach { item ->
+                if (item is SectionUiModelImpl) {
+                    sections.add(
+                        FormSection(
+                            uid = item.uid,
+                            title = item.label,
+                            description = item.description,
+                            state = when (item.isOpen) {
+                                true -> SectionState.OPEN
+                                false -> SectionState.CLOSE
+                                null -> SectionState.FIXED
+                            },
+                            fields = items.filterIsInstance<FieldUiModelImpl>()
+                                .filter { it.programStageSection == item.uid },
+                        ),
+                    )
+                }
             }
+        } else {
+            sections.add(
+                FormSection(
+                    uid = "DUMMY",
+                    title = "TITLE",
+                    description = null,
+                    state = SectionState.NO_HEADER,
+                    fields = items.filterIsInstance<FieldUiModelImpl>(),
+                ),
+            )
         }
+
         return sections
+    }
+
+    private fun hasSections(items: List<FieldUiModel>): Boolean {
+        return items.filterIsInstance<SectionUiModelImpl>().isNotEmpty()
     }
 }
