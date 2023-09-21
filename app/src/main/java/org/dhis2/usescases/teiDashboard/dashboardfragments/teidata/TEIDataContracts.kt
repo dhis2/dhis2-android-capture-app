f62ebe38868144fa2c758bc577fb9919805966be
Pablo <ppajueloc@gmail.com>
2023-09-21 09:07:22 +0200

    null safety in TEIDataFragment and TEIDataPresenter

--- app/src/main/java/org/dhis2/usescases/teiDashboard/dashboardfragments/teidata/TEIDataContracts.kt
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/dashboardfragments/teidata/TEIDataContracts.kt
@@ -1,7 +1,7 @@
 package org.dhis2.usescases.teiDashboard.dashboardfragments.teidata
 
 import android.content.Intent
-import android.os.Bundle
+import androidx.core.app.ActivityOptionsCompat
 import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.functions.Consumer
@@ -27,12 +27,12 @@ class TEIDataContracts {
         fun switchFollowUp(followUp: Boolean)
         fun displayGenerateEvent(eventUid: String)
         fun restoreAdapter(programUid: String, teiUid: String, enrollmentUid: String)
-        fun seeDetails(intent: Intent, bundle: Bundle)
-        fun openEventDetails(intent: Intent, bundle: Bundle)
+        fun seeDetails(intent: Intent, options: ActivityOptionsCompat)
+        fun openEventDetails(intent: Intent, options: ActivityOptionsCompat)
         fun openEventInitial(intent: Intent)
         fun openEventCapture(intent: Intent)
-        fun showTeiImage(fileName: String, defaultIcon: String)
-        fun setFilters(filterItems: MutableList<FilterItem>)
+        fun showTeiImage(filePath: String, defaultIcon: String)
+        fun setFilters(filterItems: List<FilterItem>)
         fun hideFilters()
         fun observeStageSelection(
             currentProgram: Program,
