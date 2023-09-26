11f98f8f926db5f0c5d6a78dd73ff9a63a5cc233
andresmr <andres@dhis2.org>
2023-09-26 14:29:42 +0200

    Add default IME action to long text to allow ENTER

--- form/src/main/java/org/dhis2/form/ui/provider/FieldProvider.kt
+++ form/src/main/java/org/dhis2/form/ui/provider/FieldProvider.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.viewinterop.AndroidViewBinding
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -401,6 +402,7 @@ private fun ProvideLongText(
                 ),
             )
         },
+        imeAction = ImeAction.Default,
     )
 }
 
