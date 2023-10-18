a7b69f8ecbd6a17b8f9a7ff7d08def7887d602b0
manu <mmunoz@quadram.mobi>
2023-10-18 12:44:25 +0200

    fix: [ANDROAPP-5622] implement input dropdown

---
+++ form/src/main/java/org/dhis2/form/ui/provider/inputfield/DropdownProvider.kt
@@ -0,0 +1,73 @@
+package org.dhis2.form.ui.provider.inputfield
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import org.dhis2.form.extensions.inputState
+import org.dhis2.form.extensions.legend
+import org.dhis2.form.extensions.supportingText
+import org.dhis2.form.model.FieldUiModel
+import org.dhis2.form.model.OptionSetConfiguration
+import org.dhis2.form.model.UiEventType
+import org.hisp.dhis.mobile.ui.designsystem.component.InputDropDown
+
+@Composable
+fun ProvideDropdownInput(
+    modifier: Modifier,
+    fieldUiModel: FieldUiModel
+) {
+    var selectedItem by remember(fieldUiModel) {
+        mutableStateOf(fieldUiModel.displayName)
+    }
+    var expanded by remember { mutableStateOf(false) }
+
+    Column {
+        InputDropDown(
+            modifier = modifier,
+            title = fieldUiModel.label,
+            state = fieldUiModel.inputState(),
+            selectedItem = selectedItem,
+            supportingTextData = fieldUiModel.supportingText(),
+            legendData = fieldUiModel.legend(),
+            isRequiredField = fieldUiModel.mandatory,
+            onResetButtonClicked = { fieldUiModel.onClear() },
+            onArrowDropDownButtonClicked = {
+                fieldUiModel.onItemClick()
+                expanded = !expanded
+            }
+        )
+        if (expanded) {
+            when (val optionSetConfig = fieldUiModel.optionSetConfiguration) {
+                is OptionSetConfiguration.BigOptionSet -> {
+                    fieldUiModel.invokeUiEvent(UiEventType.OPTION_SET)
+                    expanded = false
+                }
+                is OptionSetConfiguration.DefaultOptionSet -> {
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                    ) {
+                        optionSetConfig.optionsToDisplay().forEach {
+                            DropdownMenuItem(
+                                content = { Text(it.displayName() ?: it.code() ?: "") },
+                                onClick = {
+                                    expanded = false
+                                    selectedItem = it.displayName()
+                                    fieldUiModel.onSave(it.code())
+                                },
+                            )
+                        }
+                    }
+                }
+                null -> throw IllegalArgumentException("Unsupported OptionSetConfiguration")
+            }
+        }
+    }
+}
