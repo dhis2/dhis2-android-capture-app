5017585bc46a3b1ac42ab7dab52c4853b3ebc419
Víctor García <vgarciabnz@gmail.com>
2023-10-19 15:05:15 +0200

    Analytics: evaluate isLogged and dhis2Version in each failed call  (#3342)
    * Evaluate isLogged in each failed call
    
    * Evaluate dhis2Version in each failed call

--- app/src/main/java/org/dhis2/utils/analytics/AnalyticsInterceptor.kt
+++ app/src/main/java/org/dhis2/utils/analytics/AnalyticsInterceptor.kt
@@ -32,19 +32,13 @@ import org.hisp.dhis.android.core.D2Manager
 
 class AnalyticsInterceptor(private val analyticHelper: AnalyticsHelper) : Interceptor {
 
-    val appVersionName = BuildConfig.VERSION_NAME
-    var serverVersionName: String? = null
-    var isLogged = false
+    private val appVersionName = BuildConfig.VERSION_NAME
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
         val response = chain.proceed(request)
 
-        if (response.code >= 400 && !isLogged) {
-            isLogged = D2Manager.getD2().userModule().blockingIsLogged()
-        }
-
-        if (response.code >= 400 && isLogged) {
+        if (response.code >= 400 && isLogged()) {
             analyticHelper.trackMatomoEvent(
                 API_CALL,
                 "${request.method}_${request.url}",
@@ -55,10 +49,10 @@ class AnalyticsInterceptor(private val analyticHelper: AnalyticsHelper) : Interc
     }
 
     private fun getDhis2Version(): String? {
-        if (serverVersionName == null) {
-            serverVersionName =
-                D2Manager.getD2().systemInfoModule().systemInfo().blockingGet()?.version()
-        }
-        return serverVersionName
+        return D2Manager.getD2().systemInfoModule().systemInfo().blockingGet()?.version()
+    }
+
+    private fun isLogged(): Boolean {
+        return D2Manager.getD2().userModule().blockingIsLogged()
     }
 }
