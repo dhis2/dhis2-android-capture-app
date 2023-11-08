6c539e31d13fa9488ed966bbfaf8cabc8a7ed1b7
Manu Muñoz <manu.munoz@dhis2.org>
2023-11-08 14:41:39 +0100

    fix: [ANDROAPP-5596] sections review (#3371)

--- form/src/main/java/org/dhis2/form/ui/Form.kt
+++ form/src/main/java/org/dhis2/form/ui/Form.kt
@@ -4,7 +4,6 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,14 +12,13 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
 import org.dhis2.commons.resources.ResourceManager
 import org.dhis2.form.model.FieldUiModel
 import org.dhis2.form.model.FormSection
@@ -28,6 +26,7 @@ import org.dhis2.form.ui.event.RecyclerViewUiEvents
 import org.dhis2.form.ui.intent.FormIntent
 import org.dhis2.form.ui.provider.inputfield.FieldProvider
 import org.hisp.dhis.mobile.ui.designsystem.component.Section
+import org.hisp.dhis.mobile.ui.designsystem.component.SectionState
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -39,7 +38,6 @@ fun Form(
 ) {
     val scrollState = rememberLazyListState()
     val context = LocalContext.current
-    val coroutineScope = rememberCoroutineScope()
     val focusManager = LocalFocusManager.current
     val callback = remember {
         object : FieldUiModel.Callback {
@@ -56,15 +54,19 @@ fun Form(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
-            .padding(horizontal = 16.dp),
+            .padding(horizontal = 16.dp, vertical = 16.dp),
         state = scrollState,
-        verticalArrangement = spacedBy(24.dp),
     ) {
         if (sections.isNotEmpty()) {
             this.itemsIndexed(
                 items = sections,
                 key = { _, fieldUiModel -> fieldUiModel.uid },
             ) { _, section ->
+                LaunchedEffect(section) {
+                    if (section.state == SectionState.OPEN) {
+                        scrollState.animateScrollToItem(sections.indexOf(section))
+                    }
+                }
                 Section(
                     title = section.title,
                     isLastSection = getNextSection(section, sections) == null,
@@ -76,17 +78,11 @@ fun Form(
                     warningCount = section.warningCount(),
                     onNextSection = {
                         getNextSection(section, sections)?.let {
-                            coroutineScope.launch {
-                                scrollState.animateScrollToItem(sections.indexOf(it))
-                                intentHandler.invoke(FormIntent.OnSection(it.uid))
-                            }
+                            intentHandler.invoke(FormIntent.OnSection(it.uid))
                         }
                     },
                     onSectionClick = {
-                        coroutineScope.launch {
-                            scrollState.animateScrollToItem(sections.indexOf(section))
-                            intentHandler.invoke(FormIntent.OnSection(section.uid))
-                        }
+                        intentHandler.invoke(FormIntent.OnSection(section.uid))
                     },
                     content = {
                         section.fields.forEach { fieldUiModel ->
@@ -105,7 +101,6 @@ fun Form(
                                 resources = resources,
                                 focusManager = focusManager,
                             )
-                            Spacer(modifier = Modifier.height(24.dp))
                         }
                     },
                 )
