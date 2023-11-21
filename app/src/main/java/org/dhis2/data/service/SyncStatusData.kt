ac5468e58028635f8a9cca5d97d2b25692570a5c
Manu Muñoz <manu.munoz@dhis2.org>
2023-11-21 11:05:35 +0100

    fix: [ANDROAPP-5627] open single program once (#3402)
    * fix: [ANDROAPP-5627] open single program once
    
    * [ANDROAPP-5627] sonar fix
    
    * [ANDROAPP-5627] manage saved instance state for single program navigation
    
    ---------
    
    Co-authored-by: Xavier Molloy <xavi.molloy@gmail.com>

--- app/src/main/java/org/dhis2/data/service/SyncStatusData.kt
+++ app/src/main/java/org/dhis2/data/service/SyncStatusData.kt
@@ -4,7 +4,7 @@ import org.hisp.dhis.android.core.arch.call.D2ProgressStatus
 import org.hisp.dhis.android.core.arch.call.D2ProgressSyncStatus
 
 data class SyncStatusData(
-    val running: Boolean,
+    val running: Boolean? = null,
     val downloadingMedia: Boolean = false,
     val programSyncStatusMap: Map<String, D2ProgressStatus> = emptyMap(),
     val isInitialSync: Boolean = false,
