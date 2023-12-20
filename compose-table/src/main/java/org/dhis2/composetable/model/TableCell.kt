385e391b20e728ebc9c969756816e431a04f3e20
Manu Muñoz <manu.munoz@dhis2.org>
2023-12-20 15:46:43 +0100

    fix: [ANDROAPP-5249] resizing button over cell (#3421)

--- compose-table/src/main/java/org/dhis2/composetable/model/TableCell.kt
+++ compose-table/src/main/java/org/dhis2/composetable/model/TableCell.kt
@@ -1,13 +1,12 @@
 package org.dhis2.composetable.model
 
 import kotlinx.serialization.Serializable
-import org.dhis2.composetable.ui.SelectionState
 
 @Serializable
 data class TableCell(
     val id: String? = null,
     val row: Int? = null,
-    val column: Int? = null,
+    val column: Int,
     val value: String?,
     val editable: Boolean = true,
     val mandatory: Boolean? = false,
@@ -15,11 +14,6 @@ data class TableCell(
     val warning: String? = null,
     val legendColor: Int? = null,
 ) {
-    fun isSelected(selectionState: SelectionState): Boolean {
-        return selectionState.cellOnly &&
-            selectionState.row == row &&
-            selectionState.column == column
-    }
 
     fun hasErrorOrWarning() = errorOrWarningMessage() != null
     fun errorOrWarningMessage() = error ?: warning
