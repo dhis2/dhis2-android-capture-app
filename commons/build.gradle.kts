4cee495c4a5e5b72f58d107d182ca23b444aa82e
Xavier Molloy <xavi.molloy@gmail.com>
2023-12-29 16:36:49 +0100

    feat: [ANDROAPP-5824] add core library desugaring

--- commons/build.gradle.kts
+++ commons/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     flavorDimensions += listOf("default")
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -123,4 +124,5 @@ dependencies {
     api(libs.dhis2.mobile.designsystem) {
         isChanging = true
     }
+    coreLibraryDesugaring(libs.desugar)
 }
