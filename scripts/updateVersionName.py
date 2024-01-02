577890c4b9c1f46f3821e441e89d87126a2a133b
andresmr <andres@dhis2.org>
2024-01-02 14:13:23 +0100

    update python script

--- scripts/updateVersionName.py
+++ scripts/updateVersionName.py
@@ -1,15 +1,15 @@
 import toml
 
+
 # Specify the path to your TOML file
-file_path = '../gradle/libs.versions.toml'
+file_path = 'gradle/libs.versions.toml'
 
 
 # Load the TOML file
 with open(file_path, 'r') as file:
     data = toml.load(file)
 
-print("*** Original version ***")
-print(data['vName'])
+print("Current value of vName:", data['vName'])
 
 # Update the desired value
 data['vName'] = '2.10'
@@ -22,5 +22,4 @@ with open(file_path, 'w') as file:
 with open(file_path, 'r') as file:
     updated_data = toml.load(file)
 
-print("*** Updated version ***")
-print(updated_data['vName'])
+print("New value of vName:", data['vName'])
