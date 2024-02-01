9c543f6ff327f07412e89420c9e59c4594219a3f
manu <mmunoz@quadram.mobi>
2024-02-01 17:28:28 +0100

    feat: [ANDROAPP-5802] add tests

--- app/src/androidTest/java/org/dhis2/usescases/teidashboard/dialogs/scheduling/SchedulingDialogUiTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/teidashboard/dialogs/scheduling/SchedulingDialogUiTest.kt
@@ -1,6 +1,7 @@
 package org.dhis2.usescases.teidashboard.dialogs.scheduling
 
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,6 +17,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class SchedulingDialogUiTest {
@@ -125,8 +127,8 @@ class SchedulingDialogUiTest {
         }
 
         composeTestRule.onNodeWithText("Program stage").performClick()
-        composeTestRule.onNodeWithText("PS B").performClick()
+        composeTestRule.onNodeWithTag("INPUT_DROPDOWN_MENU_ITEM_1").performClick()
 
-        assert(viewModel.programStage.value == programStages[1])
+        verify(viewModel).updateStage(programStages[1])
     }
 }
