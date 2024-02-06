a412e2b5218aaa0f9a1cccab4958fe9fe21df597
andresmr <andres@dhis2.org>
2024-02-06 17:10:01 +0100

    [ANDROAPP-500] Implement SnackBar for min attributes search

--- app/src/main/java/org/dhis2/usescases/searchTrackEntity/searchparameters/SearchParametersScreen.kt
+++ app/src/main/java/org/dhis2/usescases/searchTrackEntity/searchparameters/SearchParametersScreen.kt
@@ -8,15 +8,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.SnackbarHost
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Cancel
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import org.dhis2.usescases.searchTrackEntity.SearchDispatchers
 import org.dhis2.usescases.searchTrackEntity.searchparameters.provider.provideParameterSelectorItem
 import org.hisp.dhis.android.core.D2Manager
@@ -33,50 +38,77 @@ fun SearchParametersScreen(
     onSearchClick: () -> Unit,
     onClear: () -> Unit,
 ) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.White, shape = Shape.LargeTop),
+    val scaffoldState = rememberScaffoldState()
+    val snackBarHostState = scaffoldState.snackbarHostState
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        backgroundColor = Color.Transparent,
+        scaffoldState = scaffoldState,
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackBarHostState,
+                modifier = Modifier.padding(
+                    start = 8.dp,
+                    top = 8.dp,
+                    end = 8.dp,
+                    bottom = 48.dp,
+                ),
+            )
+        },
     ) {
         Column(
             modifier = Modifier
-                .weight(1F)
-                .verticalScroll(rememberScrollState()),
+                .fillMaxSize()
+                .background(color = Color.White, shape = Shape.LargeTop)
+                .padding(it),
         ) {
-            viewModel.uiState.items.forEach { searchParameter ->
-                ParameterSelectorItem(
-                    model = provideParameterSelectorItem(
-                        searchParameter = searchParameter,
-                        onValueChange = onValueChange,
-                    ),
-                )
+            Column(
+                modifier = Modifier
+                    .weight(1F)
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                viewModel.uiState.items.forEach { searchParameter ->
+                    ParameterSelectorItem(
+                        model = provideParameterSelectorItem(
+                            searchParameter = searchParameter,
+                            onValueChange = onValueChange,
+                        ),
+                    )
+                }
+
+                Button(
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally),
+                    style = ButtonStyle.TEXT,
+                    text = "Clear search",
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Cancel,
+                            contentDescription = "Clear search",
+                            tint = SurfaceColor.Primary,
+                        )
+                    },
+                ) {
+                    onClear()
+                }
             }
 
             Button(
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally),
-                style = ButtonStyle.TEXT,
-                text = "Clear search",
-                icon = {
-                    Icon(
-                        imageVector = Icons.Outlined.Cancel,
-                        contentDescription = "Clear search",
-                        tint = SurfaceColor.Primary,
-                    )
-                },
+                    .fillMaxWidth()
+                    .padding(16.dp, 8.dp, 16.dp, 8.dp),
+                style = ButtonStyle.FILLED,
+                text = "Search",
             ) {
-                onClear()
+//                onSearchClick()
+                /*val minAttributes= 2
+                val message = stringResource(R.string.search_min_attributes_message)
+                    .format("$minAttributes")*/
+                coroutineScope.launch {
+                    snackBarHostState.showSnackbar("This is a snackBar")
+                }
             }
-        }
-
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp, 8.dp, 16.dp, 8.dp),
-            style = ButtonStyle.FILLED,
-            text = "Search",
-        ) {
-            onSearchClick()
         }
     }
 }
