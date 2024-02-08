a4883ac456aa610b4960fc18faf1cfcc373284b5
andresmr <andres@dhis2.org>
2024-02-08 16:59:20 +0100

    [ANDROAPP-5800] handle focus

--- app/src/main/java/org/dhis2/usescases/searchTrackEntity/searchparameters/provider/ParameterSelectorItemProvider.kt
+++ app/src/main/java/org/dhis2/usescases/searchTrackEntity/searchparameters/provider/ParameterSelectorItemProvider.kt
@@ -5,6 +5,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.input.TextFieldValue
 import org.dhis2.usescases.searchTrackEntity.searchparameters.model.SearchParameter
 import org.hisp.dhis.mobile.ui.designsystem.component.InputShellState
@@ -17,20 +21,42 @@ fun provideParameterSelectorItem(
     searchParameter: SearchParameter,
     onValueChange: (uid: String, value: String?) -> Unit,
 ): ParameterSelectorItemModel {
+    val focusRequester = remember { FocusRequester() }
+
     var value by remember(searchParameter.value) {
         mutableStateOf(TextFieldValue(searchParameter.value ?: ""))
     }
+
+    var status by remember {
+        mutableStateOf(
+            if (value.text.isEmpty()) {
+                ParameterSelectorItemModel.Status.CLOSED
+            } else {
+                ParameterSelectorItemModel.Status.UNFOCUSED
+            },
+        )
+    }
+
+    val modifierWithFocus = Modifier
+        .focusRequester(focusRequester)
+        .onFocusChanged {
+            status = if (it.isFocused) {
+                ParameterSelectorItemModel.Status.FOCUSED
+            } else {
+                ParameterSelectorItemModel.Status.UNFOCUSED
+            }
+        }
 
     return ParameterSelectorItemModel(
         label = searchParameter.label,
         helper = searchParameter.helper,
         inputField = {
-            InputText(
-                title = searchParameter.label,
-                state = InputShellState.UNFOCUSED,
-                inputTextFieldValue = value,
-                inputStyle = InputStyle.ParameterInputStyle(),
-                onValueChanged = {
+            ProvideInputField(
+                modifier = modifierWithFocus,
+                searchParameter = searchParameter,
+                status = status,
+                value = value,
+                onValueChange = {
                     value = it ?: TextFieldValue()
                     onValueChange(
                         searchParameter.uid,
@@ -39,10 +65,33 @@ fun provideParameterSelectorItem(
                 },
             )
         },
-        status = if (value.text.isEmpty()) {
-            ParameterSelectorItemModel.Status.CLOSED
-        } else {
-            ParameterSelectorItemModel.Status.OPENED
+        status = status,
+        onExpand = {
+            status = ParameterSelectorItemModel.Status.UNFOCUSED
+        },
+    )
+}
+
+@Composable
+fun ProvideInputField(
+    modifier: Modifier,
+    searchParameter: SearchParameter,
+    status: ParameterSelectorItemModel.Status,
+    value: TextFieldValue?,
+    onValueChange: (value: TextFieldValue?) -> Unit,
+
+) {
+    InputText(
+        modifier = modifier,
+        title = searchParameter.label,
+        state = when (status) {
+            ParameterSelectorItemModel.Status.FOCUSED -> InputShellState.FOCUSED
+            else -> InputShellState.UNFOCUSED
+        },
+        inputTextFieldValue = value,
+        inputStyle = InputStyle.ParameterInputStyle(),
+        onValueChanged = {
+            onValueChange.invoke(it)
         },
     )
 }
