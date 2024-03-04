1548fabe5045827b25a14a4a6786c95f2d37c2fe
Pablo <pablo@dhis2.org>
2024-03-04 17:09:29 +0100

    [ANDROAPP-4457] Configurable basemaps (#3509)
    Signed-off-by: Pablo <pablo@dhis2.org>

--- dhis2_android_maps/src/main/java/org/dhis2/maps/usecases/MapStyleConfiguration.kt
+++ dhis2_android_maps/src/main/java/org/dhis2/maps/usecases/MapStyleConfiguration.kt
@@ -7,6 +7,7 @@ import org.hisp.dhis.android.core.map.layer.MapLayerImageryProvider
 
 class MapStyleConfiguration(private val d2: D2) {
     fun fetchMapStyles(): List<BaseMapStyle> {
+        val defaultMap = d2.settingModule().systemSetting().defaultBaseMap().blockingGet()?.value()
         return d2.mapsModule().mapLayers().withImageryProviders().blockingGet()
             .map { mapLayer ->
                 val id = mapLayer.displayName()
@@ -16,19 +17,43 @@ class MapStyleConfiguration(private val d2: D2) {
                     mapLayer.subdomains(),
                 )
                 val attribution = mapLayer.imageryProviders().mapAttribution()
-                build(id, tileUrls, attribution)
+                build(id, tileUrls, attribution, defaultMap == mapLayer.uid())
             }
     }
 }
 
 fun String.mapTileUrls(subdomainPlaceholder: String?, subdomains: List<String>?): List<String> {
-    return subdomains
-        .takeIf { subdomainPlaceholder != null && !it.isNullOrEmpty() }
-        ?.map { subdomain ->
-            this.replace(subdomainPlaceholder!!, subdomain)
-        }
-        ?: listOf(this)
+    return when {
+        subdomainPlaceholder != null && !subdomains.isNullOrEmpty() ->
+            subdomains.map { subdomain -> this.replace(subdomainPlaceholder, subdomain) }
+
+        this.contains("{subdomain}") ->
+            possibleSubdomainsForSubdomain().map { subdomain ->
+                this.replace("{subdomain}", subdomain)
+            }
+
+        this.contains("{s}") ->
+            possibleSubdomainsForS().map { subdomain ->
+                this.replace("{s}", subdomain)
+            }
+
+        else -> listOf(this)
+    }
 }
+
+private fun possibleSubdomainsForS() = listOf(
+    "a",
+    "b",
+    "c",
+    "d",
+)
+
+private fun possibleSubdomainsForSubdomain() = listOf(
+    "t0",
+    "t1",
+    "t2",
+    "t3",
+)
 
 fun List<MapLayerImageryProvider>?.mapAttribution(): String {
     if (this == null) return ""
