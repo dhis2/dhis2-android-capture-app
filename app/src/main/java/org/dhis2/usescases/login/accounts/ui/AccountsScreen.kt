8b1fa8d526698f29e51911073138a4b7004b4eeb
= <david.aparicio@dhis2.org>
2024-03-08 11:31:46 +0100

    update: [ANDROAPP-5013] code formatted

--- app/src/main/java/org/dhis2/usescases/login/accounts/ui/AccountsScreen.kt
+++ app/src/main/java/org/dhis2/usescases/login/accounts/ui/AccountsScreen.kt
@@ -30,8 +30,6 @@ import androidx.compose.ui.viewinterop.AndroidView
 import org.dhis2.R
 import org.dhis2.bindings.buildInfo
 import org.dhis2.usescases.login.accounts.AccountModel
-import org.hisp.dhis.mobile.ui.designsystem.component.Button
-import org.hisp.dhis.mobile.ui.designsystem.component.ButtonStyle
 import org.dhis2.usescases.login.ui.LoginTopBar
 import org.hisp.dhis.mobile.ui.designsystem.component.Button
 import org.hisp.dhis.mobile.ui.designsystem.component.ButtonStyle
