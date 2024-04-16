98a5765bc30bbc4b6ad428b323cce195a2a3b003
manu <mmunoz@quadram.mobi>
2024-04-16 12:28:28 +0200

    fix: [ANDROAPP-6103] month value in scheduling dialog

--- app/src/main/java/org/dhis2/usescases/teiDashboard/dialogs/scheduling/SchedulingViewModel.kt
+++ app/src/main/java/org/dhis2/usescases/teiDashboard/dialogs/scheduling/SchedulingViewModel.kt
@@ -148,7 +148,7 @@ class SchedulingViewModel(
 
     fun onDateSet(year: Int, month: Int, day: Int) {
         val calendar = Calendar.getInstance()
-        calendar[year, month, day, 0, 0] = 0
+        calendar[year, month - 1, day, 0, 0] = 0
         calendar[Calendar.MILLISECOND] = 0
         val selectedDate = calendar.time
         setUpEventReportDate(selectedDate)
