7b1739a78c0de879058df9ff3d8f32b53bc349ef
Xavier Molloy <xavi@dhis2.org>
2024-06-26 16:24:37 +0200

    ci: [ANDROAPP-5753] Format value depending on value type, migrate ValueUtils to kotlin, fix sonar smells in DashboardRepositoryImpl

--- app/src/main/java/org/dhis2/utils/ValueUtils.kt
+++ app/src/main/java/org/dhis2/utils/ValueUtils.kt
@@ -1,52 +1,125 @@
-package org.dhis2.utils;
+package org.dhis2.utils
 
-import org.hisp.dhis.android.core.D2;
-import org.hisp.dhis.android.core.common.ValueType;
-import org.hisp.dhis.android.core.option.Option;
-import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValue;
-
-import java.util.Objects;
+import org.dhis2.commons.date.DateUtils
+import org.hisp.dhis.android.core.D2
+import org.hisp.dhis.android.core.common.ValueType
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityAttributeValue
+import java.text.ParseException
 
 /**
  * QUADRAM. Created by ppajuelo on 25/09/2018.
  */
-
-public class ValueUtils {
-
-    private ValueUtils() {
-        throw new IllegalStateException("Utility class");
+class ValueUtils private constructor() {
+    init {
+        throw IllegalStateException("Utility class")
     }
 
-    public static TrackedEntityAttributeValue transform(D2 d2, TrackedEntityAttributeValue attributeValue, ValueType valueType, String optionSetUid) {
-        String transformedValue = transformValue(d2, attributeValue.value(), valueType, optionSetUid);
+    companion object {
+        fun transform(
+            d2: D2,
+            attributeValue: TrackedEntityAttributeValue,
+            valueType: ValueType?,
+            optionSetUid: String?,
+        ): TrackedEntityAttributeValue {
+            val transformedValue =
+                transformValue(d2, attributeValue.value(), valueType, optionSetUid)
 
-        if (!Objects.equals(transformedValue, attributeValue.value())) {
-            return attributeValue.toBuilder()
+            return if (transformedValue != attributeValue.value()) {
+                attributeValue.toBuilder()
                     .value(transformedValue)
-                    .build();
-        } else {
-            return attributeValue;
-        }
-    }
-
-    public static String transformValue(D2 d2, String value, ValueType valueType, String optionSetUid) {
-        String teAttrValue = value;
-        if (valueType.equals(ValueType.ORGANISATION_UNIT)) {
-            if (!d2.organisationUnitModule().organisationUnits().byUid().eq(value).blockingIsEmpty()) {
-                String orgUnitName = d2.organisationUnitModule().organisationUnits()
-                        .byUid().eq(value)
-                        .one().blockingGet().displayName();
-                teAttrValue = orgUnitName;
+                    .build()
+            } else {
+                attributeValue
             }
-        } else if (optionSetUid != null) {
-            String optionCode = value;
-            if (optionCode != null) {
-                Option option = d2.optionModule().options().byOptionSetUid().eq(optionSetUid).byCode().eq(optionCode).one().blockingGet();
-                if (option != null && (Objects.equals(option.code(), optionCode) || Objects.equals(option.name(), optionCode))) {
-                    teAttrValue = option.displayName();
+        }
+
+        fun transformValue(
+            d2: D2,
+            value: String?,
+            valueType: ValueType?,
+            optionSetUid: String?,
+        ): String? {
+            var teAttrValue = value
+            when (valueType) {
+                ValueType.ORGANISATION_UNIT -> {
+                    if (!d2.organisationUnitModule().organisationUnits().byUid().eq(value)
+                            .blockingIsEmpty()
+                    ) {
+                        val orgUnitName = d2.organisationUnitModule().organisationUnits()
+                            .byUid().eq(value)
+                            .one().blockingGet()!!.displayName()!!
+                        teAttrValue = orgUnitName
+                    }
+                }
+                ValueType.DATE, ValueType.AGE -> {
+                    teAttrValue = formatDate(teAttrValue)
+                }
+                ValueType.DATETIME -> {
+                    teAttrValue = formatDateTime(teAttrValue)
+                }
+
+                ValueType.PERCENTAGE -> {
+                    teAttrValue?.let {
+                        if (it.isNotEmpty()) {
+                            teAttrValue = "$it %"
+                        }
+                    }
+                }
+                else -> {
+                    teAttrValue = transformOptionSet(optionSetUid, d2, value)
                 }
             }
+            return teAttrValue
         }
-        return teAttrValue;
+
+        private fun transformOptionSet(optionSetUid: String?, d2: D2, value: String?): String? {
+            var teAttrValue = value
+            if (optionSetUid != null) {
+                val optionCode = value
+                if (optionCode != null) {
+                    val option =
+                        d2.optionModule().options().byOptionSetUid().eq(optionSetUid).byCode()
+                            .eq(optionCode).one().blockingGet()
+                    if (option != null && (option.code() == optionCode || option.name() == optionCode)) {
+                        teAttrValue = option.displayName()
+                    }
+                }
+            }
+            return teAttrValue
+        }
+
+        private fun formatDate(value: String?): String {
+            return if (value?.isNotEmpty() == true) {
+                var formattedDate = ""
+                val date = try {
+                    DateUtils.oldUiDateFormat().parse(value)
+                } catch (e: ParseException) {
+                    null
+                }
+                date?.let {
+                    formattedDate = DateUtils.uiDateFormat().format(date)
+                }
+                formattedDate
+            } else {
+                ""
+            }
+        }
+
+        private fun formatDateTime(value: String?): String {
+            return if (value?.isNotEmpty() == true) {
+                var formattedDate = ""
+                val date = try {
+                    DateUtils.databaseDateFormatNoSeconds().parse(value)
+                } catch (e: ParseException) {
+                    null
+                }
+                date?.let {
+                    formattedDate = DateUtils.uiDateTimeFormat().format(date)
+                }
+                formattedDate
+            } else {
+                ""
+            }
+        }
     }
 }
