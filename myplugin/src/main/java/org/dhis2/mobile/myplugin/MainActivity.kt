6226a97a0614543eb13269121a4d5533e31d950c
andresmr <andres@dhis2.org>
2024-07-31 13:52:27 +0200

    Use Mobile UI in plugin
    Signed-off-by: andresmr <andres@dhis2.org>

--- myplugin/src/main/java/org/dhis2/mobile/myplugin/MainActivity.kt
+++ myplugin/src/main/java/org/dhis2/mobile/myplugin/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,12 +16,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Dhis2androidcaptureappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    PluginImpl().Show()
+//                }
             }
         }
     }
--- myplugin/src/main/java/org/dhis2/mobile/myplugin/PluginImpl.kt
+++ myplugin/src/main/java/org/dhis2/mobile/myplugin/PluginImpl.kt
@@ -1,16 +1,41 @@
 package org.dhis2.mobile.myplugin
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Android
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import org.dhis2.commons.plugin.PluginInterface
+import org.hisp.dhis.mobile.ui.designsystem.component.AdditionalInfoItem
+import org.hisp.dhis.mobile.ui.designsystem.component.AdditionalInfoItemColor
+import org.hisp.dhis.mobile.ui.designsystem.component.CardDetail
 
 class PluginImpl : PluginInterface {
     @Composable
     override fun Show() {
-        Text(
-            text = "This is my amazing plugin!!!",
-            modifier = Modifier
-        )
+        MainScreen()
+    }
+
+    @Composable
+    fun MainScreen() {
+        Column {
+            CardDetail(
+                title = "This is my amazing plugin!!!",
+                additionalInfoList = listOf(
+                    AdditionalInfoItem(
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Outlined.Android,
+                                contentDescription = "This is my amazing plugin!!!",
+                                tint = AdditionalInfoItemColor.SUCCESS.color,
+                            )
+                        },
+                        value = "Lets Rock",
+                        color = AdditionalInfoItemColor.ERROR.color,
+                        isConstantItem = true,
+                    )
+                )
+            )
+        }
     }
 }
