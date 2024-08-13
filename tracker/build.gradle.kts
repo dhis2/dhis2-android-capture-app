494f8002c7e157a81899af0e84c0db6355260c15
FerdyRod <ferdy@dhis2.org>
2024-08-13 14:24:38 +0200

    removes duplicated parsing

---
+++ tracker/build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("kotlin-parcelize")
+    id("kotlinx-serialization")
+    alias(libs.plugins.kotlin.compose.compiler)
+}
+apply(from = "${project.rootDir}/jacoco/jacoco.gradle.kts")
+
+android {
+    compileSdk = libs.versions.sdk.get().toInt()
+    namespace = "org.dhis2.tracker"
+
+    defaultConfig {
+        minSdk = libs.versions.minSdk.get().toInt()
+        testOptions.targetSdk = libs.versions.sdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("debug") {
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    flavorDimensions += listOf("default")
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
+}
+
+dependencies {
+
+    debugApi(libs.bundles.uicomponents.debugapi)
+    androidTestImplementation(libs.bundles.uicomponents.androidtest)
+}
---
+++ tracker/src/test/java/org/dhis2/tracker/ExampleUnitTest.kt
@@ -0,0 +1,8 @@
+package org.dhis2.tracker
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest
