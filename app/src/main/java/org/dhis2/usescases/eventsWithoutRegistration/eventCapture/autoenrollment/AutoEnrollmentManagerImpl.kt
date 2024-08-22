e3366bb207304a2d1b3307ebc0995df4729d8ddf
ASIMWELA <augastinesimwela@gmail.com>
2024-08-22 14:42:37 +0200

    Create an implmentation for getting current event data values

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/autoenrollment/AutoEnrollmentManagerImpl.kt
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/autoenrollment/AutoEnrollmentManagerImpl.kt
@@ -1,4 +1,29 @@
 package org.dhis2.usescases.eventsWithoutRegistration.eventCapture.autoenrollment
 
-class AutoEnrollmentManagerImpl : AutoEnrollmentManager {
+import io.reactivex.Flowable
+import org.dhis2.usescases.eventsWithoutRegistration.eventCapture.autoenrollment.model.AutoEnrollmentConfig
+import org.hisp.dhis.android.core.D2
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValue
+
+class AutoEnrollmentManagerImpl(private val d2:D2) : AutoEnrollmentManager {
+    override fun getCurrentEventDataValues(eventUid: String?): Flowable<List<TrackedEntityDataValue>> {
+        return d2.trackedEntityModule().trackedEntityDataValues().byEvent().eq(eventUid).get()
+            .toFlowable()
+    }
+
+    override fun getCurrentEventTrackedEntityInstance(eventUid: String?): Flowable<String?>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun getAutoEnrollmentConfiguration(): Flowable<AutoEnrollmentConfig> {
+        TODO("Not yet implemented")
+    }
+
+    override fun createEnrollments(
+        programIds: List<String>,
+        entity: String?,
+        orgUnit: String?
+    ): Flowable<String> {
+        TODO("Not yet implemented")
+    }
 }
