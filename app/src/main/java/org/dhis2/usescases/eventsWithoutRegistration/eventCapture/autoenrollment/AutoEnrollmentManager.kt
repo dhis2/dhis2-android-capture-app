2e140e52e1890aae4ef1a20a70c8f3295665d0a7
ASIMWELA <augastinesimwela@gmail.com>
2024-08-22 14:39:58 +0200

    Create a function defition for for auto enrollment interface

--- app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/autoenrollment/AutoEnrollmentManager.kt
+++ app/src/main/java/org/dhis2/usescases/eventsWithoutRegistration/eventCapture/autoenrollment/AutoEnrollmentManager.kt
@@ -1,4 +1,21 @@
 package org.dhis2.usescases.eventsWithoutRegistration.eventCapture.autoenrollment
 
+import io.reactivex.Flowable
+import org.dhis2.usescases.eventsWithoutRegistration.eventCapture.autoenrollment.model.AutoEnrollmentConfig
+import org.hisp.dhis.android.core.trackedentity.TrackedEntityDataValue
+
 interface AutoEnrollmentManager {
+
+    fun getCurrentEventDataValues(eventUid: String?): Flowable<List<TrackedEntityDataValue>>
+
+    fun getCurrentEventTrackedEntityInstance(eventUid: String?): Flowable<String?>?
+
+    fun getAutoEnrollmentConfiguration(): Flowable<AutoEnrollmentConfig>
+
+
+    fun createEnrollments(
+        programIds: List<String>,
+        entity: String?,
+        orgUnit: String?
+    ): Flowable<String>
 }
