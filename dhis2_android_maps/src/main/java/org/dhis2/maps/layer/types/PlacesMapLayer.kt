aa8aef780936a7c6ba721d96bc4a21682f305ae6
Pablo <pablo@dhis2.org>
2024-09-30 08:29:33 +0200

    [ANDROAPP-6114] Restore polygon capture
    Signed-off-by: Pablo <pablo@dhis2.org>

--- dhis2_android_maps/src/main/java/org/dhis2/maps/layer/types/PlacesMapLayer.kt
+++ dhis2_android_maps/src/main/java/org/dhis2/maps/layer/types/PlacesMapLayer.kt
@@ -1,19 +1,26 @@
 package org.dhis2.maps.layer.types
 
+import android.graphics.Color
 import com.mapbox.geojson.Feature
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.style.expressions.Expression
+import com.mapbox.mapboxsdk.style.layers.FillLayer
 import com.mapbox.mapboxsdk.style.layers.Layer
+import com.mapbox.mapboxsdk.style.layers.LineLayer
 import com.mapbox.mapboxsdk.style.layers.Property
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource
 import org.dhis2.maps.layer.MapLayer
 import org.dhis2.maps.layer.MapLayerManager
+import org.dhis2.maps.layer.isPoint
+import org.dhis2.maps.layer.isPolygon
 
 private const val SELECTED_PLACE_LAYER_ID = "SELECTED_PLACE_LAYER_ID"
 const val PLACES_LAYER_ID = "PLACES_LAYER_ID"
 const val PLACES_SOURCE_ID = "PLACES_SOURCE_ID"
+const val POLYGON_PLACE_LAYER_ID = "POLYGON_PLACE_LAYER_ID"
+const val POLYGON_PLACE_BORDER_LAYER_ID = "POLYGON_PLACE_BORDER_LAYER_ID"
 
 class PlacesMapLayer(
     val style: Style,
@@ -33,6 +40,7 @@ class PlacesMapLayer(
                     Expression.all(
                         Expression.eq(Expression.literal("places"), Expression.literal(true)),
                         Expression.eq(Expression.literal("selected"), Expression.literal(false)),
+                        isPoint(),
                     ),
                 )
 
@@ -47,12 +55,30 @@ class PlacesMapLayer(
                     Expression.all(
                         Expression.eq(Expression.literal("places"), Expression.literal(true)),
                         Expression.eq(Expression.literal("selected"), Expression.literal(true)),
+                        isPoint(),
                     ),
                 )
+
+    private val polygonLayer: Layer
+        get() = style.getLayer(POLYGON_PLACE_LAYER_ID)
+            ?: FillLayer(POLYGON_PLACE_LAYER_ID, PLACES_SOURCE_ID)
+                .withProperties(
+                    PropertyFactory.fillColor(Color.parseColor("#66007DEB")),
+                ).withFilter(isPolygon())
+
+    private val polygonBorderLayer: Layer
+        get() = style.getLayer(POLYGON_PLACE_BORDER_LAYER_ID)
+            ?: LineLayer(POLYGON_PLACE_BORDER_LAYER_ID, PLACES_SOURCE_ID)
+                .withProperties(
+                    PropertyFactory.lineColor(Color.parseColor("#007DEB")),
+                    PropertyFactory.lineWidth(2f),
+                ).withFilter(isPolygon())
 
     init {
         style.addLayer(placesLayer)
         style.addLayerAbove(selectedPlaceLayer, placesLayer.id)
+        style.addLayerBelow(polygonBorderLayer, placesLayer.id)
+        style.addLayerAbove(polygonLayer, polygonBorderLayer.id)
     }
 
     override fun showLayer() {
