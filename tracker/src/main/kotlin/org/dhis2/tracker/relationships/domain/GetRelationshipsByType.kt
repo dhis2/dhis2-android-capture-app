b0fa7d62f2750bda806dadf62e6cc57b09325d2a
andresmr <andres@dhis2.org>
2024-10-04 21:58:55 +0200

    feat: [ANDROAPP-5125] Add profile picture
    Signed-off-by: andresmr <andres@dhis2.org>

--- tracker/src/main/kotlin/org/dhis2/tracker/relationships/domain/GetRelationshipsByType.kt
+++ tracker/src/main/kotlin/org/dhis2/tracker/relationships/domain/GetRelationshipsByType.kt
@@ -2,8 +2,11 @@ package org.dhis2.tracker.relationships.domain
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import org.dhis2.commons.data.RelationshipDirection
 import org.dhis2.tracker.relationships.data.RelationshipsRepository
+import org.dhis2.tracker.relationships.model.RelationShipItem
 import org.dhis2.tracker.relationships.model.RelationshipSection
+import org.dhis2.ui.avatar.AvatarProviderConfiguration
 
 /*
  * This use case fetches all the relationships that the tei has access to grouped by their type.
@@ -26,7 +29,35 @@ class GetRelationshipsByType(
                     val teiTypeUid = type.second
                     RelationshipSection(
                         relationshipType = relationshipType,
-                        relationships = relationships.filter { it.relationshipType.uid() == type.first.uid() },
+                        relationships = relationships.filter {
+                            it.relationshipType.uid() == type.first.uid()
+                        }.map {
+                            // here we have RelationshipViewModel and we want to map it to RelationShipItem
+                            val (attributes, profilePath) = when (it.direction) {
+                                RelationshipDirection.FROM -> Pair(
+                                    it.fromValues,
+                                    it.fromImage,
+                                )
+
+                                RelationshipDirection.TO -> Pair(
+                                    it.toValues,
+                                    it.toImage,
+                                )
+                            }
+
+                            RelationShipItem(
+                                title = it.displayRelationshipName(),
+                                attributes = attributes,
+                                ownerType = it.ownerType,
+                                ownerUid = it.ownerUid,
+                                avatar = AvatarProviderConfiguration.ProfilePic(
+                                    profilePicturePath = profilePath ?: "",
+                                    firstMainValue = attributes.firstOrNull()?.second?.firstOrNull()
+                                        ?.toString() ?: "",
+                                ),
+                                canOpen = it.canBeOpened,
+                            )
+                        },
                         teiTypeUid = teiTypeUid
                     )
                 }
