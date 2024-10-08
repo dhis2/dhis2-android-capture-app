3fe37872b471300edb59b516baa6806eab0697fa
Xavier Molloy <xavi@dhis2.org>
2024-10-08 13:46:12 +0200

    feat: [ANDROAPP-5841] Add BottomNavigation bar for analytics screen (#3795)
    * feat: [ANDROAPP-5841] Add BottomNavigation bar for analytics screen
    
    * feat: [ANDROAPP-5841] fix unit tests
    
    * refactor: [ANDROAPP-6283] migrate EventCaptureFormFragment to kotlin and rename ResultDialogUiProvider for new usage
    
    * feat: [ANDROAPP-5841] refactor and code clean up
    
    * feat: [ANDROAPP-6283] add new unit tests for event completion provider, fix other tests
    
    * feat: [ANDROAPP-5841] add test for new functionality, fix erroneous behaviour with tablet, address code review comments

---
+++ stock-usecase/src/main/java/org/dhis2/android/rtsm/ui/home/screens/AnalyticsScreen.kt
@@ -0,0 +1,76 @@
+package org.dhis2.android.rtsm.ui.home.screens
+
+import android.widget.FrameLayout
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BackdropScaffold
+import androidx.compose.material.BackdropValue
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberBackdropScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.ViewCompat
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dhis2.org.analytics.charts.ui.GroupAnalyticsFragment
+import kotlinx.coroutines.CoroutineScope
+import org.dhis2.android.rtsm.ui.home.HomeViewModel
+import org.dhis2.android.rtsm.ui.home.screens.components.AnalyticsTopBar
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun AnalyticsScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel(),
+    backAction: () -> Unit,
+    themeColor: Color,
+    scaffoldState: ScaffoldState,
+    syncAction: (scope: CoroutineScope, scaffoldState: ScaffoldState) -> Unit = { _, _ -> },
+    supportFragmentManager: FragmentManager,
+) {
+    val backdropState = rememberBackdropScaffoldState(BackdropValue.Revealed)
+    val settingsUiState by viewModel.settingsUiState.collectAsState()
+    BackdropScaffold(
+        modifier = modifier,
+        appBar = {
+            AnalyticsTopBar(
+                title = settingsUiState.programName,
+                themeColor = themeColor,
+                backAction = {
+                    backAction.invoke()
+                },
+                scaffoldState = scaffoldState,
+                syncAction = syncAction,
+            )
+        },
+        backLayerBackgroundColor = themeColor,
+        backLayerContent = {
+        },
+        frontLayerElevation = 0.dp,
+        frontLayerContent = {
+            var frameId = 0
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = { context ->
+                    FrameLayout(context).apply {
+                        id = ViewCompat.generateViewId()
+                        frameId = id
+                    }
+                },
+                update = {
+                    supportFragmentManager.beginTransaction().add(frameId, GroupAnalyticsFragment.forProgram(settingsUiState.programUid))
+                        .commit()
+                },
+            )
+        },
+        scaffoldState = backdropState,
+        gesturesEnabled = false,
+        frontLayerBackgroundColor = Color.White,
+        frontLayerScrimColor = Color.Unspecified,
+    )
+}
