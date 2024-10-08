bf8edb2cace9d81d611ba88eacd7687b1b4fb43f
andresmr <andres@dhis2.org>
2024-10-08 15:50:50 +0200

    test: fix checkShareDataDialogIsDisplayed in LoginTest
    Signed-off-by: andresmr <andres@dhis2.org>

--- app/src/androidTest/java/org/dhis2/usescases/login/LoginRobot.kt
+++ app/src/androidTest/java/org/dhis2/usescases/login/LoginRobot.kt
@@ -153,7 +153,9 @@ class LoginRobot(val composeTestRule: ComposeTestRule) : BaseRobot() {
     }
 
     fun checkShareDataDialogIsDisplayed() {
-        onView(withId(android.R.id.content)).check(matches(isDisplayed()))
+        val title = InstrumentationRegistry.getInstrumentation()
+            .targetContext.getString(R.string.improve_app_msg_title)
+        composeTestRule.onNodeWithText(title)
     }
 
     fun clickOnPrivacyPolicy() {
