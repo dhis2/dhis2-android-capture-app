f8e7272c30eacfbe35d2005570bea5c0bd5c7e17
FerdyRod <ferdy@dhis2.org>
2024-10-14 08:58:26 +0200

    Set up 15 minutes of inactivity time

--- commons/src/main/java/org/dhis2/commons/service/SessionManagerServiceImpl.kt
+++ commons/src/main/java/org/dhis2/commons/service/SessionManagerServiceImpl.kt
@@ -10,6 +10,7 @@ import org.dhis2.commons.prefs.PreferenceProvider
 import org.dhis2.commons.schedulers.SchedulerProvider
 import org.hisp.dhis.android.core.D2
 import timber.log.Timber
+import kotlin.time.Duration.Companion.minutes
 
 class SessionManagerServiceImpl(
     private val d2: D2,
@@ -41,8 +42,7 @@ class SessionManagerServiceImpl(
     }
 
     companion object {
-        // todo request configured session time out duration when ready for production
-        private const val SESSION_TIMEOUT_DURATION = 10 * 1000 // 10 seconds
+        private val SESSION_TIMEOUT_DURATION: Long = 15.minutes.inWholeMilliseconds
     }
 
     private fun logoutUser(navigateAction: (Int) -> Unit, scope: LifecycleCoroutineScope) {
