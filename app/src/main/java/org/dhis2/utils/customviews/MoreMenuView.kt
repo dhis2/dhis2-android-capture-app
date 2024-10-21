e16b6e287f326eee240c730adf9e1b1915ccbd79
Xavier Molloy <xavi@dhis2.org>
2024-10-21 10:51:05 +0200

    fix: [ANDROAPP-6364] modify more options icon button padding

--- app/src/main/java/org/dhis2/utils/customviews/MoreMenuView.kt
+++ app/src/main/java/org/dhis2/utils/customviews/MoreMenuView.kt
@@ -1,8 +1,5 @@
 package org.dhis2.utils.customviews
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.Icon
@@ -11,7 +8,6 @@ import androidx.compose.ui.Modifier
 import org.hisp.dhis.mobile.ui.designsystem.component.IconButton
 import org.hisp.dhis.mobile.ui.designsystem.component.menu.DropDownMenu
 import org.hisp.dhis.mobile.ui.designsystem.component.menu.MenuItemData
-import org.hisp.dhis.mobile.ui.designsystem.theme.Spacing
 import org.hisp.dhis.mobile.ui.designsystem.theme.SurfaceColor
 
 @Composable
@@ -21,29 +17,25 @@ fun <T> MoreOptionsWithDropDownMenuButton(
     onMenuToggle: (Boolean) -> Unit,
     onItemClick: (T) -> Unit,
 ) {
-    Box(
-        modifier = Modifier.padding(end = Spacing.Spacing16),
+    IconButton(
+        modifier = Modifier,
+        icon = {
+            Icon(
+                imageVector = Icons.Outlined.MoreVert,
+                tint = SurfaceColor.SurfaceBright,
+                contentDescription = null,
+            )
+        },
     ) {
-        IconButton(
-            modifier = Modifier.offset(x = Spacing.Spacing16),
-            icon = {
-                Icon(
-                    imageVector = Icons.Outlined.MoreVert,
-                    tint = SurfaceColor.SurfaceBright,
-                    contentDescription = null,
-                )
-            },
-        ) {
-            onMenuToggle(!expanded)
-        }
+        onMenuToggle(!expanded)
+    }
 
-        DropDownMenu(
-            items = dropDownMenuItems,
-            expanded = expanded,
-            onDismissRequest = { onMenuToggle(false) },
-        ) { itemId ->
-            onMenuToggle(false)
-            onItemClick(itemId)
-        }
+    DropDownMenu(
+        items = dropDownMenuItems,
+        expanded = expanded,
+        onDismissRequest = { onMenuToggle(false) },
+    ) { itemId ->
+        onMenuToggle(false)
+        onItemClick(itemId)
     }
 }
