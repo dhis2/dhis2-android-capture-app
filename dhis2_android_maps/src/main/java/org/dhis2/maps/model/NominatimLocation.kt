5ed771f85e747a60f243ba4729950b32ffba5b5c
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2024-11-06 13:19:26 +0100

    add JsonProperty annotation

--- dhis2_android_maps/src/main/java/org/dhis2/maps/model/NominatimLocation.kt
+++ dhis2_android_maps/src/main/java/org/dhis2/maps/model/NominatimLocation.kt
@@ -1,8 +1,10 @@
 package org.dhis2.maps.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class NominatimLocation(
     val lat: String,
     val lon: String,
     val name: String,
-    val displayName: String?,
+    @JsonProperty("display_name") val displayName: String?,
 )
