5ace581022e1ae1433b32b64c209ff5e74f03aff
FerdyRod <ferdy@dhis2.org>
2024-11-18 14:10:56 +0100

    Adds distributor broadcast receivers

---
+++ app/src/main/java/org/dhis2/usescases/pushnotifications/distributor/Constants.kt
@@ -0,0 +1,22 @@
+package org.dhis2.usescases.pushnotifications.distributor
+
+/**
+ * Constants as defined on the specs
+ * https://github.com/UnifiedPush/UP-spec/blob/main/specifications.md
+ */
+
+const val ACTION_NEW_ENDPOINT = "org.unifiedpush.android.connector.NEW_ENDPOINT"
+const val ACTION_REGISTRATION_FAILED = "org.unifiedpush.android.connector.REGISTRATION_FAILED"
+const val ACTION_UNREGISTERED = "org.unifiedpush.android.connector.UNREGISTERED"
+const val ACTION_MESSAGE = "org.unifiedpush.android.connector.MESSAGE"
+
+const val ACTION_REGISTER = "org.unifiedpush.android.distributor.REGISTER"
+const val ACTION_UNREGISTER = "org.unifiedpush.android.distributor.UNREGISTER"
+
+const val FEATURE_BYTES_MESSAGE = "org.unifiedpush.android.distributor.feature.BYTES_MESSAGE"
+
+const val EXTRA_APPLICATION = "application"
+const val EXTRA_TOKEN = "token"
+const val EXTRA_ENDPOINT = "endpoint"
+const val EXTRA_MESSAGE = "message"
+const val EXTRA_BYTES_MESSAGE = "bytesMessage"
