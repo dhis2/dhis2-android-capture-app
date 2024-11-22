bbfe0bda76227d8b11fd6a7b91a3ebc003063dc4
Xavier Molloy <xavi@dhis2.org>
2024-11-22 13:12:13 +0100

    fix: [ANDROAPP-6644] resolve code smells

--- commons/src/androidTest/java/org/dhis2/commons/date/DateUtilsTest.java
+++ commons/src/androidTest/java/org/dhis2/commons/date/DateUtilsTest.java
@@ -1,6 +1,6 @@
 package org.dhis2.commons.date;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Assertions;
 import org.junit.Test;
 import java.util.Calendar;
 import java.util.Date;
@@ -14,13 +14,13 @@ public class DateUtilsTest {
         Calendar calendar = Calendar.getInstance();
         //should return false for current date
         calendar.setTime(new Date());
-        assertEquals(false, DateUtils.getInstance().isEventDueDateOverdue(calendar.getTime()));
+        Assertions.assertFalse(DateUtils.getInstance().isEventDueDateOverdue(calendar.getTime()));
         //false for future date
         calendar.add(Calendar.DAY_OF_MONTH, 10);
-        assertEquals(false, DateUtils.getInstance().isEventDueDateOverdue(calendar.getTime()));
+        Assertions.assertFalse(DateUtils.getInstance().isEventDueDateOverdue(calendar.getTime()));
         //true for past dates
         calendar.add(Calendar.DAY_OF_MONTH, -30);
-        assertEquals(true, DateUtils.getInstance().isEventDueDateOverdue(calendar.getTime()));
+        Assertions.assertTrue(DateUtils.getInstance().isEventDueDateOverdue(calendar.getTime()));
 
     }
 }
