f073ce771e3b5bca211524bdb38bdc1b8c90e9ee
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2024-11-29 12:08:54 +0100

    fix code smell

--- form/src/main/java/org/dhis2/form/model/OptionSetConfiguration.kt
+++ form/src/main/java/org/dhis2/form/model/OptionSetConfiguration.kt
@@ -3,13 +3,13 @@ package org.dhis2.form.model
 import androidx.paging.PagingData
 import androidx.paging.map
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import org.dhis2.ui.MetadataIconData
 import org.hisp.dhis.android.core.option.Option
 
 data class OptionSetConfiguration(
-    val searchEmitter: MutableStateFlow<String>? = null,
+    val searchEmitter: StateFlow<String>? = null,
     val onSearch: (String) -> Unit,
     val optionFlow: Flow<PagingData<OptionData>>,
 ) {
