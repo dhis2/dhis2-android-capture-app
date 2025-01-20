33b45a51ceebfb9f820c1b820314d01a84dcc56e
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-01-20 11:39:07 +0100

    add rules

--- form/build.gradle.kts
+++ form/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         getByName("debug") {
         }
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
