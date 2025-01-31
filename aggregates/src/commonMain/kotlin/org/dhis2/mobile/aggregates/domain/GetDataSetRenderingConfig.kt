b7fcf47929a6c3cd6f7e794b3a9bee08bd5c28fb
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-01-31 07:39:11 +0100

    set usecases internal

--- aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/domain/GetDataSetRenderingConfig.kt
+++ aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/domain/GetDataSetRenderingConfig.kt
@@ -2,7 +2,7 @@ package org.dhis2.mobile.aggregates.domain
 
 import org.dhis2.mobile.aggregates.data.DataSetInstanceRepository
 
-class GetDataSetRenderingConfig(
+internal class GetDataSetRenderingConfig(
     private val datasetUid: String,
     private val dataSetInstanceRepository: DataSetInstanceRepository,
 ) {
