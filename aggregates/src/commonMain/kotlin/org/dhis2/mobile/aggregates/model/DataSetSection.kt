ab49c869b443d463ebb9a2ec0c99e876d7907737
Xavier Molloy <xavi@dhis2.org>
2025-02-11 17:10:54 +0100

    build: [ANDROAPP-6820] revert modification in DataSetSection, remove unnecessary dependencies

--- aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/model/DataSetSection.kt
+++ aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/model/DataSetSection.kt
@@ -3,6 +3,4 @@ package org.dhis2.mobile.aggregates.model
 data class DataSetSection(
     val uid: String,
     val title: String,
-    val topContent: String? = null,
-    val bottomContent: String? = null,
 )
