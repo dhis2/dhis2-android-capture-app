b6f96a4b777e17741fa19b26240a61a79729fad5
andresmr <andres@dhis2.org>
2025-02-13 16:40:59 +0100

    test: [ANDROAPP-6802] move resourceManager to ui

--- aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/ui/provider/ResourceManager.kt
+++ aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/ui/provider/ResourceManager.kt
@@ -1,4 +1,4 @@
-package org.dhis2.mobile.aggregates.domain
+package org.dhis2.mobile.aggregates.ui.provider
 
 import org.dhis2.mobile.aggregates.resources.Res
 import org.dhis2.mobile.aggregates.resources.complete
