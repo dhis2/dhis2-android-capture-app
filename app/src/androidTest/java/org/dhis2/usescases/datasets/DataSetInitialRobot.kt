f6097a8d6fa1af192f4f325ad1c68f86b0247355
FerdyRod <ferdy@dhis2.org>
2025-02-18 13:52:22 +0100

    updates tests for new report period picker

--- app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetInitialRobot.kt
+++ app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetInitialRobot.kt
@@ -2,14 +2,9 @@ package org.dhis2.usescases.datasets
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import org.dhis2.R
 import org.dhis2.common.BaseRobot
-import org.dhis2.utils.customviews.DateViewHolder
-
 
 fun dataSetInitialRobot(dataSetInitialRobot: DataSetInitialRobot.() -> Unit) {
     DataSetInitialRobot().apply {
@@ -24,15 +19,11 @@ class DataSetInitialRobot : BaseRobot() {
     }
 
     fun clickOnInputPeriod() {
+        waitForView(withId(R.id.dataSetPeriodInputLayout), 5000)
         onView(withId(R.id.dataSetPeriodInputLayout)).perform(click())
     }
 
     fun clickOnActionButton() {
         onView(withId(R.id.actionButton)).perform(click())
-    }
-
-    fun selectPeriod(period: String) {
-        onView(withId(R.id.recycler_date))
-            .perform(RecyclerViewActions.actionOnItem<DateViewHolder>(hasDescendant(withText(period)), click()))
     }
 }
