0c4e2262ae7bd925a39d6b4b1e3ff479f6d7a024
CarlosMacaneta <carlosmacaneta95@gmail.com>
2025-02-20 09:44:28 +0200

    remove(stock-usecases): stockusecase navigation

--- app/src/main/java/org/dhis2/usescases/main/program/ProgramView.kt
+++ app/src/main/java/org/dhis2/usescases/main/program/ProgramView.kt
@@ -1,13 +1,10 @@
 package org.dhis2.usescases.main.program
 
 import org.dhis2.usescases.general.AbstractActivityContracts
-import org.hisp.dhis.android.core.usecase.stock.StockUseCase
 
 interface ProgramView : AbstractActivityContracts.View {
 
     fun navigateTo(program: ProgramUiModel)
-
-    fun navigateToStockManagement(stockUseCase: StockUseCase)
 
     fun showSyncDialog(program: ProgramUiModel)
 }
