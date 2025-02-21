7474aa73fb7fe54bbdcdec65387a5f0a2062e103
FerdyRod <ferdy@dhis2.org>
2025-02-21 16:30:38 +0100

    change to use Koin injection

---
+++ app/src/main/java/org/dhis2/usescases/datasets/datasetInitial/periods/DatasetPeriodPickerModule.kt
@@ -0,0 +1,23 @@
+package org.dhis2.usescases.datasets.datasetInitial.periods
+
+import org.dhis2.commons.date.DateUtils
+import org.dhis2.usescases.datasets.datasetInitial.periods.data.DatasetPeriodRepository
+import org.dhis2.usescases.datasets.datasetInitial.periods.domain.GetDatasetPeriodMaxDate
+import org.dhis2.usescases.datasets.datasetInitial.periods.domain.GetDatasetPeriods
+import org.dhis2.usescases.datasets.datasetInitial.periods.domain.HasDataInputPeriods
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val datasetPeriodPickerModule = module {
+    single<DateUtils> { DateUtils() }
+    single {
+        DatasetPeriodRepository(get(), get())
+    }
+    single { GetDatasetPeriods(get()) }
+    single { GetDatasetPeriodMaxDate(get()) }
+    single { HasDataInputPeriods(get()) }
+
+    viewModel {
+        DatasetPeriodViewModel(get(), get(), get())
+    }
+}
