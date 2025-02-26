dfba1f90a251dd94558ca65efe81bac1df6355e6
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-02-26 07:57:34 +0100

    fix designsystem breaking changes

--- aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/ui/viewModel/DataSetTableViewModel.kt
+++ aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/ui/viewModel/DataSetTableViewModel.kt
@@ -150,11 +150,14 @@ internal class DataSetTableViewModel(
                 val tableRows = tableGroup.cellElements
                     .map { cellElement ->
                         TableRowModel(
-                            rowHeader = RowHeader(
-                                id = cellElement.uid,
-                                title = cellElement.label,
-                                row = absoluteRowIndex,
-                                description = cellElement.description,
+                            rowHeaders = listOf(
+                                RowHeader(
+                                    id = cellElement.uid,
+                                    title = cellElement.label,
+                                    row = absoluteRowIndex,
+                                    column = 0,
+                                    description = cellElement.description,
+                                ),
                             ),
                             values = buildMap {
                                 repeat(tableHeader.tableMaxColumns()) { columnIndex ->
@@ -207,10 +210,13 @@ internal class DataSetTableViewModel(
                 val totalRow = if (sectionData.showColumnTotals()) {
                     listOf(
                         TableRowModel(
-                            rowHeader = RowHeader(
-                                id = "TotalRow",
-                                title = "Total",
-                                row = absoluteRowIndex,
+                            rowHeaders = listOf(
+                                RowHeader(
+                                    id = "TotalRow",
+                                    title = "Total",
+                                    row = absoluteRowIndex,
+                                    column = 0,
+                                ),
                             ),
                             values = buildMap {
                                 repeat(tableHeader.tableMaxColumns()) { columnIndex ->
@@ -275,10 +281,13 @@ internal class DataSetTableViewModel(
                 ),
                 tableRows = getDataSetSectionIndicators(sectionUid)?.entries?.map { (key, value) ->
                     TableRowModel(
-                        rowHeader = RowHeader(
-                            id = key,
-                            title = key,
-                            row = absoluteRowIndex,
+                        rowHeaders = listOf(
+                            RowHeader(
+                                id = key,
+                                title = key,
+                                row = absoluteRowIndex,
+                                column = 0,
+                            ),
                         ),
                         values = mapOf(
                             0 to TableCell(
