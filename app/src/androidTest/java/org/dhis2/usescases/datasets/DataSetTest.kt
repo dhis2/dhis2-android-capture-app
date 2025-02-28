f98c8a9e558d38bc47ea35be5078ab99b6b00374
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-02-28 10:38:03 +0100

    restore previous step in test

--- app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetTest.kt
@@ -100,7 +100,7 @@ class DataSetTest : BaseTest() {
         // Step - ANDROAPP-6795 Check content boxes above and below the table
         checkContentBoxesAreDisplayed()
         // Step - ANDROAPP-6810 Move a category to rows (click on sections 8, 16, 24)
-//        checkCategoryIsMovedToRow()
+        checkCategoryIsMovedToRow()
         // Step - ANDROAPP-6828 Automatic grouping (click on sections 19, 20, 22)
         checkAutomaticGroupingDisabled()
         // Step - ANDROAPP-6811 Pivot options (click on sections 5, 13, 23)
