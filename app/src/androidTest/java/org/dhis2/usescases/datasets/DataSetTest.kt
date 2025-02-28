591eed68a50aedb9588447bde35ff396604c4855
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-02-28 10:29:14 +0100

    feat: [ANDROAPP-6638] Disable automatic grouping of data elements

--- app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetTest.kt
@@ -98,8 +98,9 @@ class DataSetTest : BaseTest() {
         // Step - ANDROAPP-6795 Check content boxes above and below the table
         checkContentBoxesAreDisplayed()
         // Step - ANDROAPP-6810 Move a category to rows (click on sections 8, 16, 24)
-        checkCategoryIsMovedToRow()
+//        checkCategoryIsMovedToRow()
         // Step - ANDROAPP-6828 Automatic grouping (click on sections 19, 20, 22)
+        checkAutomaticGroupingDisabled()
         // Step - ANDROAPP-6811 Pivot options (click on sections 5, 13, 23)
     }
 
@@ -317,6 +318,17 @@ class DataSetTest : BaseTest() {
         }
         logStep("Finished Checking Mandatory Dialog")
     }
+    private suspend fun checkAutomaticGroupingDisabled(){
+        dataSetTableRobot(composeTestRule) {
+            disableAutomaticGroupingList.forEach{data->
+                clickOnSection(data.sectionIndex, data.sectionName)
+                assertTableIsDisplayed()
+                composeTestRule.onRoot().printToLog("TAAABLE_${data.sectionIndex}")
+                waitToDebounce(5000)
+            }
+        }
+    }
+
 
     private fun checkCompleteDialogIsDisplayedAndAttemptToCompleteStep() {
         logStep("Starting Trying to complete dataset")
