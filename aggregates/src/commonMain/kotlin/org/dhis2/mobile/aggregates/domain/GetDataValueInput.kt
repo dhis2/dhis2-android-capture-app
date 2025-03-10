9c58e40b4d06cb0e91017453063f2922e414f28c
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-03-10 13:50:00 +0100

    move ui classes out of domain ones

--- aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/domain/GetDataValueInput.kt
+++ aggregates/src/commonMain/kotlin/org/dhis2/mobile/aggregates/domain/GetDataValueInput.kt
@@ -1,15 +1,11 @@
 package org.dhis2.mobile.aggregates.domain
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.supervisorScope
 import org.dhis2.mobile.aggregates.data.DataSetInstanceRepository
 import org.dhis2.mobile.aggregates.data.OptionRepository
 import org.dhis2.mobile.aggregates.model.CellInfo
+import org.dhis2.mobile.aggregates.model.CellValueExtra
 import org.dhis2.mobile.aggregates.model.InputType
-import org.dhis2.mobile.aggregates.ui.inputs.TableId
-import org.dhis2.mobile.aggregates.ui.states.InputExtra
-import org.dhis2.mobile.commons.extensions.getFormattedFileSize
 import org.dhis2.mobile.commons.extensions.userFriendlyValue
 import org.hisp.dhis.mobile.ui.designsystem.component.CheckBoxData
 import org.hisp.dhis.mobile.ui.designsystem.component.Coordinates
@@ -27,12 +23,15 @@ internal class GetDataValueInput(
     private val optionRepository: OptionRepository,
 ) : ValueValidator(repository) {
     suspend operator fun invoke(
-        rowIds: List<TableId>,
-        columnIds: List<TableId>,
+        dataElementUid: String,
+        categoryOptionComboUidData: Pair<String?, List<String>>,
         fetchOptions: Boolean = false,
     ): CellInfo = supervisorScope {
-        val dataElementUid = checkOnlyOneDataElementIsProvided(rowIds, columnIds)
-        val categoryOptionComboUid = checkedCategoryOptionCombos(rowIds, columnIds)
+        val categoryOptionComboUid =
+            categoryOptionComboUidData.first
+                ?: repository.categoryOptionComboFromCategoryOptions(
+                    categoryOptionComboUidData.second,
+                )
 
         val dataElementInfo = repository.dataElementInfo(
             dataSetUid = dataSetUid,
@@ -98,32 +97,17 @@ internal class GetDataValueInput(
                     },
                 )
 
-                InputType.FileResource -> InputExtra.File(
-                    fileWeight = value?.let { getFormattedFileSize(value) },
-                )
-
-                InputType.MultiText -> InputExtra.MultiText(
-                    numberOfOptions = optionRepository.optionCount(dataElementUid),
+                InputType.MultiText -> CellValueExtra.Options(
+                    optionCount = optionRepository.optionCount(dataElementUid),
                     options = if (fetchOptions) {
-                        optionRepository.options(dataElementUid).map { optionData ->
-                            async {
-                                CheckBoxData(
-                                    uid = optionData.code ?: optionData.uid,
-                                    checked = optionData.code?.let {
-                                        value?.contains(it) == true
-                                    } ?: false,
-                                    enabled = true,
-                                    textInput = optionData.label,
-                                )
-                            }
-                        }.awaitAll()
+                        optionRepository.options(dataElementUid)
                     } else {
                         emptyList()
                     },
                     optionsFetched = fetchOptions,
                 )
 
-                else -> InputExtra.None
+                else -> null
             },
             supportingText = dataElementInfo.description?.let { listOf(it) } ?: emptyList(),
             errors = conflicts.first,
--- aggregates/src/commonTest/kotlin/org/dhis2/mobile/aggregates/domain/GetDataValueInputTest.kt
+++ aggregates/src/commonTest/kotlin/org/dhis2/mobile/aggregates/domain/GetDataValueInputTest.kt
@@ -3,19 +3,15 @@ package org.dhis2.mobile.aggregates.domain
 import kotlinx.coroutines.test.runTest
 import org.dhis2.mobile.aggregates.data.DataSetInstanceRepository
 import org.dhis2.mobile.aggregates.data.OptionRepository
+import org.dhis2.mobile.aggregates.model.CellValueExtra
 import org.dhis2.mobile.aggregates.model.DataElementInfo
 import org.dhis2.mobile.aggregates.model.InputType
 import org.dhis2.mobile.aggregates.onRunBlocking
-import org.dhis2.mobile.aggregates.ui.inputs.TableId
-import org.dhis2.mobile.aggregates.ui.inputs.TableIdType
-import org.dhis2.mobile.aggregates.ui.states.InputExtra
 import org.junit.Test
-import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
-import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 internal class GetDataValueInputTest {
@@ -26,7 +22,7 @@ internal class GetDataValueInputTest {
     }
     private val repository = mock<DataSetInstanceRepository> {
         onRunBlocking { dataElementInfo(any(), any(), any()) } doReturn mockedDataElementInfo
-        onRunBlocking { value(any(), any(), any(), any(), any()) } doReturn null
+        onRunBlocking { value(any(), any(), any(), any(), any()) } doReturn "Current value"
         onRunBlocking { conflicts(any(), any(), any(), any(), any(), any()) } doReturn Pair(
             emptyList(),
             emptyList(),
@@ -51,73 +47,21 @@ internal class GetDataValueInputTest {
             whenever(mockedDataElementInfo.inputType) doReturn inputType
             whenever(optionRepository.optionCount(any())) doReturn 1
             val result = getDataValueInputTest(
-                rowIds = listOf(TableId("dataElementId", TableIdType.DataElement)),
-                columnIds = listOf(TableId("catOptionComboUid", TableIdType.CategoryOptionCombo)),
+                dataElementUid = "dataElementId",
+                categoryOptionComboUidData = Pair("catOptionComboUid", emptyList()),
             )
+
+            print("INPUTTYPE: $inputType, extra: ${result.inputExtra}")
 
             assertTrue(
                 when (result.inputType) {
-                    InputType.Age -> result.inputExtra is InputExtra.Age
-                    InputType.Date, InputType.Time, InputType.DateTime -> result.inputExtra is InputExtra.Date
-                    InputType.Coordinates -> result.inputExtra is InputExtra.Coordinate
-                    InputType.FileResource -> result.inputExtra is InputExtra.File
-                    InputType.MultiText -> result.inputExtra is InputExtra.MultiText
-                    else -> result.inputExtra is InputExtra.None
+                    InputType.Coordinates -> result.inputExtra is CellValueExtra.Coordinates
+                    InputType.MultiText -> result.inputExtra is CellValueExtra.Options
+                    else -> result.inputExtra == null
                 },
             )
         }
     }
-
-    @Test
-    fun `should throw error if more than one data element is provided`() = runTest {
-        val exception = assertThrows<IllegalStateException> {
-            getDataValueInputTest(
-                rowIds = listOf(TableId("dataElementId", TableIdType.DataElement)),
-                columnIds = listOf(TableId("dataElementId", TableIdType.DataElement)),
-            )
-        }
-        assertEquals(
-            "Only one data element can be provided",
-            exception.message,
-        )
-    }
-
-    @Test
-    fun `should throw error if more than one category option combo is provided`() = runTest {
-        val exception = assertThrows<IllegalStateException> {
-            getDataValueInputTest(
-                rowIds = listOf(
-                    TableId("dataElementUid", TableIdType.DataElement),
-                    TableId("catOptionComboUid", TableIdType.CategoryOptionCombo),
-                ),
-                columnIds = listOf(TableId("catOptionComboUid", TableIdType.CategoryOptionCombo)),
-            )
-        }
-        assertEquals(
-            "Only one category option combo can be provided",
-            exception.message,
-        )
-    }
-
-    @Test
-    fun `should throw error if category options and category option combos are provided`() =
-        runTest {
-            val exception = assertThrows<IllegalStateException> {
-                getDataValueInputTest(
-                    rowIds = listOf(
-                        TableId("dataElementUid", TableIdType.DataElement),
-                        TableId("catOptionUid", TableIdType.CategoryOption),
-                    ),
-                    columnIds = listOf(
-                        TableId("catOptionComboUid", TableIdType.CategoryOptionCombo),
-                    ),
-                )
-            }
-            assertEquals(
-                "Category options and category option combos cannot be provided at the same time",
-                exception.message,
-            )
-        }
 
     private val inputTypeValues = listOf(
         InputType.Text,
