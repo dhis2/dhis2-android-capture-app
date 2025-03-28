eb314ddedd4ff4f2272b755767d166c86a2388a2
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2025-03-28 11:05:28 +0100

    add logs

--- app/src/androidTest/java/org/dhis2/usescases/BaseTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/BaseTest.kt
@@ -36,8 +36,11 @@ import org.junit.After
 import org.junit.Before
 import org.junit.ClassRule
 import org.junit.Rule
+import org.junit.rules.TestName
 import org.junit.rules.Timeout
+import timber.log.Timber
 import java.util.concurrent.TimeUnit
+
 
 open class BaseTest {
 
@@ -54,6 +57,9 @@ open class BaseTest {
 
     @get:Rule
     val timeout: Timeout = Timeout(120000, TimeUnit.MILLISECONDS)
+
+    @get: Rule
+    var testName: TestName = TestName()
 
     @get:Rule
     var permissionRule = if (Build.VERSION.SDK_INT > Build.VERSION_CODES.R) {
@@ -72,6 +78,8 @@ open class BaseTest {
     @Before
     @Throws(Exception::class)
     open fun setUp() {
+        val currentTest = testName.methodName
+        Timber.tag("RUNNER_LOG").d("Executing Before Actions for $currentTest")
         (context.applicationContext as AppTest).restoreDB()
         injectDependencies()
         registerCountingIdlingResource()
@@ -81,6 +89,8 @@ open class BaseTest {
     @After
     @Throws(Exception::class)
     open fun teardown() {
+        val currentTest = testName.methodName
+        Timber.tag("RUNNER_LOG").d("Executing After Actions for $currentTest")
         closeKeyboard()
         disableIntents()
         cleanPreferences()
@@ -179,7 +189,9 @@ open class BaseTest {
     }
 
     fun cleanLocalDatabase() {
-        (context.applicationContext as AppTest).deleteDatabase(DB_TO_IMPORT)
+       val deleted = (context.applicationContext as AppTest).deleteDatabase(DB_TO_IMPORT)
+        val currentTest = testName.methodName
+        Timber.tag("RUNNER_LOG").d("CleanDataBaseResult. Is deleted? answer: $deleted for $currentTest")
     }
 
     protected fun enableFeatureConfigValue(feature: Feature) {
