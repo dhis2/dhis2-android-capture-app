e7a0c437f5ba796eeca534de3ea12fb6920ca8cc
andresmr <andres@dhis2.org>
2025-04-17 09:53:48 +0200

    feat: [ANDROAPP-6925] enable development mode for training flavor

--- app/src/main/java/org/dhis2/usescases/main/MainActivity.kt
+++ app/src/main/java/org/dhis2/usescases/main/MainActivity.kt
@@ -252,7 +252,7 @@ class MainActivity :
     }
 
     private fun setUpDevelopmentMode() {
-        if (BuildConfig.DEBUG) {
+        if (BuildConfig.DEBUG || BuildConfig.FLAVOR == "training") {
             binding.menu.setOnLongClickListener {
                 getDevActivityContent.launch(Intent(this, DevelopmentActivity::class.java))
                 false
