183d873c1af0ef9dec80deb5a3809bbcbb99a010
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2025-05-08 10:02:20 +0200

    ktlint

--- aggregates/src/androidMain/kotlin/org/dhis2/mobile/aggregates/data/DataSetInstanceRepositoryImpl.kt
+++ aggregates/src/androidMain/kotlin/org/dhis2/mobile/aggregates/data/DataSetInstanceRepositoryImpl.kt
@@ -829,7 +829,7 @@ internal class DataSetInstanceRepositoryImpl(
             ?.compulsoryDataElementOperands()
             ?.find {
                 it.dataElement()?.uid() == dataElementUid &&
-                        it.categoryOptionCombo()?.uid() == categoryOptionComboUid
+                    it.categoryOptionCombo()?.uid() == categoryOptionComboUid
             } != null
         val dataElementValueType = dataElement?.valueType()?.toInputType()
         val inputType = requireNotNull(dataElementValueType).takeIf {
