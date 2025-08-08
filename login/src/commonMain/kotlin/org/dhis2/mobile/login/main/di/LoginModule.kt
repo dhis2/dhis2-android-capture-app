8be403462eaf81e31e598bef213b1b8a6ab62c0d
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2025-08-08 13:12:04 +0200

    feat: [ANDROAPP-7181] Added LoadingScreen - Parte 1

---
+++ login/src/commonMain/kotlin/org/dhis2/mobile/login/main/di/LoginModule.kt
@@ -0,0 +1,23 @@
+package org.dhis2.mobile.login.main.di
+
+import org.dhis2.mobile.login.authentication.di.twoFAModule
+import org.dhis2.mobile.login.main.domain.usecase.GetInitialScreen
+import org.dhis2.mobile.login.main.ui.navigation.DefaultNavigator
+import org.dhis2.mobile.login.main.ui.navigation.Navigator
+import org.dhis2.mobile.login.main.ui.viewmodel.LoginViewModel
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+internal val mainLoginModule = module {
+    single<Navigator> { DefaultNavigator() }
+    factoryOf(::GetInitialScreen)
+    viewModelOf(::LoginViewModel)
+}
+
+internal expect val accountModule: Module
+
+val loginModule = module {
+    includes(mainLoginModule, twoFAModule, accountModule)
+}
