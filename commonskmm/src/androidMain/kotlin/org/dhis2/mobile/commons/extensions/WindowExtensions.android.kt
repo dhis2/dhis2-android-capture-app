4e2da342eb6a0e393f98629e1183c8638d6f6eb0
andresmr <andres@dhis2.org>
2025-08-11 13:41:08 +0200

    feat: Implement window size class extensions for desktop and android

---
+++ commonskmm/src/androidMain/kotlin/org/dhis2/mobile/commons/extensions/WindowExtensions.android.kt
@@ -0,0 +1,16 @@
+package org.dhis2.mobile.commons.extensions
+
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@ExperimentalMaterial3WindowSizeClassApi
+@Composable
+internal actual fun getWindowSizeClass(): WindowSizeClass {
+    val context = LocalContext.current
+    val activity = context as ComponentActivity
+    return calculateWindowSizeClass(activity)
+}
---
+++ commonskmm/src/desktopMain/kotlin/org/dhis2/mobile/commons/extensions/WindowExtensions.desktop.kt
@@ -0,0 +1,12 @@
+package org.dhis2.mobile.commons.extensions
+
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+
+@ExperimentalMaterial3WindowSizeClassApi
+@Composable
+internal actual fun getWindowSizeClass(): WindowSizeClass {
+    return calculateWindowSizeClass()
+}
