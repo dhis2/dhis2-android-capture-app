1b142b8efb60999d6038d81ff8d20a5b5dc4b440
Pablo <pablo@dhis2.org>
2025-09-25 15:53:39 +0200

    Update KoinInitialization.kt

--- app/src/main/java/org/dhis2/di/KoinInitialization.kt
+++ app/src/main/java/org/dhis2/di/KoinInitialization.kt
@@ -31,7 +31,6 @@ object KoinInitialization {
                 stockModule,
                 loginModule,
                 settingsProgramModule,
-                biometricModule,
                 matomoModule,
             )
         }
