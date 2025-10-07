5b72a150bec2ed38db80442af83987f9d67c0766
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2025-10-07 17:01:08 +0200

    feat: [ANDROAPP-7243] Removed old openid classes and methods

--- app/src/main/java/org/dhis2/usescases/login/LoginContracts.kt
+++ app/src/main/java/org/dhis2/usescases/login/LoginContracts.kt
@@ -1,32 +1,11 @@
 package org.dhis2.usescases.login
 
 import androidx.annotation.UiThread
-import org.dhis2.data.server.UserManager
 import org.dhis2.usescases.general.AbstractActivityContracts
-import org.hisp.dhis.android.core.user.openid.IntentWithRequestCode
 
 class LoginContracts {
     interface View : AbstractActivityContracts.View {
         @UiThread
         fun onUnlockClick()
-
-        @UiThread
-        fun onLogoutClick(android: android.view.View)
-
-        fun handleLogout()
-
-        fun goToNextScreen()
-
-        // FingerPrintAuth
-
-        fun alreadyAuthenticated()
-
-        fun isNetworkAvailable(): Boolean
-
-        fun openOpenIDActivity(it: IntentWithRequestCode?)
-
-        fun showNoConnectionDialog()
-
-        fun initLogin(): UserManager?
     }
 }
--- app/src/main/java/org/dhis2/usescases/login/LoginViewModelFactory.kt
+++ app/src/main/java/org/dhis2/usescases/login/LoginViewModelFactory.kt
@@ -2,24 +2,18 @@ package org.dhis2.usescases.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import org.dhis2.commons.prefs.PreferenceProvider
 import org.dhis2.commons.resources.ResourceManager
-import org.dhis2.commons.schedulers.SchedulerProvider
 import org.dhis2.data.server.UserManager
 
 class LoginViewModelFactory(
     private val view: LoginContracts.View,
-    private val preferenceProvider: PreferenceProvider,
     private val resourceManager: ResourceManager,
-    private val schedulerProvider: SchedulerProvider,
     private val userManager: UserManager?,
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T =
         LoginViewModel(
             view,
-            preferenceProvider,
             resourceManager,
-            schedulerProvider,
             userManager,
         ) as T
 }
