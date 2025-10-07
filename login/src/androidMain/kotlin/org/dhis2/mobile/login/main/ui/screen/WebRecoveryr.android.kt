cb1485519251a7bb074864167383a898a2741f26
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2025-10-07 17:05:01 +0200

    feat: [ANDROAPP-7204] Remove CustomTabLauncher

--- login/src/androidMain/kotlin/org/dhis2/mobile/login/main/ui/screen/WebRecoveryr.android.kt
+++ login/src/androidMain/kotlin/org/dhis2/mobile/login/main/ui/screen/WebRecoveryr.android.kt
@@ -1,8 +1,13 @@
 package org.dhis2.mobile.login.main.ui.screen
 
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.core.net.toUri
-import org.dhis2.mobile.commons.navigation.CustomTabLauncher
 
 private const val RECOVERY_PATH = "/dhis-web-commons/security/recovery.action"
 
@@ -12,5 +17,30 @@ actual fun WebRecovery(
     onDismiss: () -> Unit,
 ) {
     val recoveryUrl = "$url$RECOVERY_PATH"
-    CustomTabLauncher(recoveryUrl.toUri(), onDismiss)
+    // Custom Tab launcher will handle the result of the Custom Tab to detect if the user closed it
+    val customTabLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.StartActivityForResult(),
+        ) { result ->
+            if (result.resultCode == Activity.RESULT_CANCELED) {
+                onDismiss()
+            }
+        }
+
+    LaunchedEffect(url) {
+        val customTabsIntent =
+            CustomTabsIntent
+                .Builder()
+                .setShowTitle(false)
+                .setUrlBarHidingEnabled(true)
+                .build()
+        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+
+        val intent =
+            customTabsIntent.intent.apply {
+                data = recoveryUrl.toUri()
+            }
+
+        customTabLauncher.launch(intent)
+    }
 }
