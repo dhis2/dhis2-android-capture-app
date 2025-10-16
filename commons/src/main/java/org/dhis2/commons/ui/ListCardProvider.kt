b5a616bf4573d3f3de77cac530e74ec4bf0e12ae
Pablo Pajuelo Cabezas <pablo@dhis2.org>
2025-10-16 12:09:29 +0200

    fix:[ANDROAPP-7263] Restore ListCard title style
    Signed-off-by: Pablo Pajuelo Cabezas <pablo@dhis2.org>

--- commons/src/main/java/org/dhis2/commons/ui/ListCardProvider.kt
+++ commons/src/main/java/org/dhis2/commons/ui/ListCardProvider.kt
@@ -1,12 +1,9 @@
 package org.dhis2.commons.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import org.dhis2.commons.ui.model.ListCardUiModel
 import org.hisp.dhis.mobile.ui.designsystem.component.AdditionalInfoItem
 import org.hisp.dhis.mobile.ui.designsystem.component.ListCard
@@ -14,29 +11,19 @@ import org.hisp.dhis.mobile.ui.designsystem.component.ListCardDescriptionModel
 import org.hisp.dhis.mobile.ui.designsystem.component.ListCardTitleModel
 import org.hisp.dhis.mobile.ui.designsystem.component.state.rememberAdditionalInfoColumnState
 import org.hisp.dhis.mobile.ui.designsystem.component.state.rememberListCardState
-import org.hisp.dhis.mobile.ui.designsystem.theme.TextColor
 
 @Composable
 fun ListCardProvider(
     modifier: Modifier = Modifier,
     card: ListCardUiModel,
+    title: ListCardTitleModel = ListCardTitleModel(text = card.title),
     @StringRes syncingResourceId: Int,
 ) {
     ListCard(
         modifier = modifier,
         listCardState =
             rememberListCardState(
-                title =
-                    ListCardTitleModel(
-                        text = card.title,
-                        style =
-                            LocalTextStyle.current.copy(
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight(500),
-                                lineHeight = 20.sp,
-                            ),
-                        color = TextColor.OnSurface,
-                    ),
+                title = title,
                 description = ListCardDescriptionModel(text = card.description),
                 lastUpdated = card.lastUpdated,
                 additionalInfoColumnState =
