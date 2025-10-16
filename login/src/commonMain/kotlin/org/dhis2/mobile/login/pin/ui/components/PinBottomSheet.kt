53330ffcedb6503faef6df7a50c170bc9e1813e7
andresmr <andres@dhis2.org>
2025-10-16 16:57:02 +0200

    feat: [ANDROAPP-7266] Request focus when PinDialog is displayed

--- login/src/commonMain/kotlin/org/dhis2/mobile/login/pin/ui/components/PinBottomSheet.kt
+++ login/src/commonMain/kotlin/org/dhis2/mobile/login/pin/ui/components/PinBottomSheet.kt
@@ -19,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.style.TextAlign
 import org.dhis2.mobile.login.pin.domain.model.PinState
 import org.dhis2.mobile.login.pin.ui.viewmodel.PinViewModel
@@ -85,6 +87,7 @@ fun PinBottomSheet(
     val viewModel: PinViewModel = koinViewModel()
     val uiState by viewModel.uiState.collectAsState()
     var currentPin by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
 
     // Handle state changes from ViewModel
     LaunchedEffect(uiState) {
@@ -111,6 +114,10 @@ fun PinBottomSheet(
     // Reset attempts when dismissed
     LaunchedEffect(Unit) {
         return@LaunchedEffect
+    }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
     }
 
     val (title, subtitle) =
@@ -178,7 +185,10 @@ fun PinBottomSheet(
 
                 // PIN Input using InputSegmentedShell
                 InputSegmentedShell(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier =
+                        Modifier
+                            .fillMaxWidth()
+                            .focusRequester(focusRequester),
                     segmentCount = pinLength,
                     initialValue = null,
                     supportingTextData =
