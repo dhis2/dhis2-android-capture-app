f2c7e9e107bd9ebba3ed635d4696042843ae5132
andresmr <andres@dhis2.org>
2025-10-17 12:27:10 +0200

    fix: Refactor session repository methods to use coroutine context for improved performance

--- login/src/androidMain/kotlin/org/dhis2/mobile/login/pin/data/SessionRepositoryImpl.kt
+++ login/src/androidMain/kotlin/org/dhis2/mobile/login/pin/data/SessionRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package org.dhis2.mobile.login.pin.data
 
 import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.withContext
+import org.dhis2.mobile.commons.coroutine.Dispatcher
 import org.dhis2.mobile.commons.network.NetworkStatusProvider
 import org.dhis2.mobile.commons.providers.PreferenceProvider
 import org.dhis2.mobile.commons.resources.D2ErrorMessageProvider
@@ -17,72 +19,78 @@ class SessionRepositoryImpl(
     private val preferenceProvider: PreferenceProvider,
     private val d2ErrorMessageProvider: D2ErrorMessageProvider,
     private val networkStatusProvider: NetworkStatusProvider,
+    private val dispatcher: Dispatcher,
 ) : SessionRepository {
     companion object {
         private const val PIN_KEY = "pin"
         private const val PREF_SESSION_LOCKED = "SessionLocked"
     }
 
-    override suspend fun savePin(pin: String) {
-        try {
-            d2
-                .dataStoreModule()
-                .localDataStore()
-                .value(PIN_KEY)
-                .blockingSet(pin)
-        } catch (d2Error: D2Error) {
-            Timber.e(d2Error, "Failed to save PIN")
-            val isNetworkAvailable = networkStatusProvider.connectionStatus.firstOrNull() ?: false
-            val errorMessage = d2ErrorMessageProvider.getErrorMessage(d2Error, isNetworkAvailable)
-            throw Exception(errorMessage, d2Error)
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to save PIN")
-            throw e
+    override suspend fun savePin(pin: String) =
+        withContext(dispatcher.io) {
+            try {
+                d2
+                    .dataStoreModule()
+                    .localDataStore()
+                    .value(PIN_KEY)
+                    .blockingSet(pin)
+            } catch (d2Error: D2Error) {
+                Timber.e(d2Error, "Failed to save PIN")
+                val isNetworkAvailable = networkStatusProvider.connectionStatus.firstOrNull() ?: false
+                val errorMessage = d2ErrorMessageProvider.getErrorMessage(d2Error, isNetworkAvailable)
+                throw Exception(errorMessage, d2Error)
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to save PIN")
+                throw e
+            }
         }
-    }
 
     override suspend fun getStoredPin(): String? =
-        try {
-            d2
-                .dataStoreModule()
-                .localDataStore()
-                .value(PIN_KEY)
-                .blockingGet()
-                ?.value()
-        } catch (d2Error: D2Error) {
-            Timber.e(d2Error, "Failed to retrieve PIN")
-            null
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to retrieve PIN")
-            null
+        withContext(dispatcher.io) {
+            try {
+                d2
+                    .dataStoreModule()
+                    .localDataStore()
+                    .value(PIN_KEY)
+                    .blockingGet()
+                    ?.value()
+            } catch (d2Error: D2Error) {
+                Timber.e(d2Error, "Failed to retrieve PIN")
+                null
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to retrieve PIN")
+                null
+            }
         }
 
-    override suspend fun deletePin() {
-        try {
-            d2
-                .dataStoreModule()
-                .localDataStore()
-                .value(PIN_KEY)
-                .blockingDeleteIfExist()
-        } catch (d2Error: D2Error) {
-            Timber.e(d2Error, "Failed to delete PIN")
-            val isNetworkAvailable = networkStatusProvider.connectionStatus.firstOrNull() ?: false
-            val errorMessage = d2ErrorMessageProvider.getErrorMessage(d2Error, isNetworkAvailable)
-            throw Exception(errorMessage, d2Error)
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to delete PIN")
-            throw e
+    override suspend fun deletePin() =
+        withContext(dispatcher.io) {
+            try {
+                d2
+                    .dataStoreModule()
+                    .localDataStore()
+                    .value(PIN_KEY)
+                    .blockingDeleteIfExist()
+            } catch (d2Error: D2Error) {
+                Timber.e(d2Error, "Failed to delete PIN")
+                val isNetworkAvailable = networkStatusProvider.connectionStatus.firstOrNull() ?: false
+                val errorMessage = d2ErrorMessageProvider.getErrorMessage(d2Error, isNetworkAvailable)
+                throw Exception(errorMessage, d2Error)
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to delete PIN")
+                throw e
+            }
         }
-    }
 
-    override suspend fun setSessionLocked(locked: Boolean) {
-        try {
-            preferenceProvider.setValue(PREF_SESSION_LOCKED, locked)
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to set session locked state")
-            throw e
+    override suspend fun setSessionLocked(locked: Boolean) =
+        withContext(dispatcher.io) {
+            try {
+                preferenceProvider.setValue(PREF_SESSION_LOCKED, locked)
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to set session locked state")
+                throw e
+            }
         }
-    }
 
     override suspend fun isSessionLocked(): Boolean =
         try {
@@ -92,17 +100,18 @@ class SessionRepositoryImpl(
             false
         }
 
-    override suspend fun logout() {
-        try {
-            d2.userModule().blockingLogOut()
-        } catch (d2Error: D2Error) {
-            Timber.e(d2Error, "Failed to logout")
-            val isNetworkAvailable = networkStatusProvider.connectionStatus.firstOrNull() ?: false
-            val errorMessage = d2ErrorMessageProvider.getErrorMessage(d2Error, isNetworkAvailable)
-            throw Exception(errorMessage, d2Error)
-        } catch (e: Exception) {
-            Timber.e(e, "Failed to logout")
-            throw e
+    override suspend fun logout() =
+        withContext(dispatcher.io) {
+            try {
+                d2.userModule().blockingLogOut()
+            } catch (d2Error: D2Error) {
+                Timber.e(d2Error, "Failed to logout")
+                val isNetworkAvailable = networkStatusProvider.connectionStatus.firstOrNull() ?: false
+                val errorMessage = d2ErrorMessageProvider.getErrorMessage(d2Error, isNetworkAvailable)
+                throw Exception(errorMessage, d2Error)
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to logout")
+                throw e
+            }
         }
-    }
 }
