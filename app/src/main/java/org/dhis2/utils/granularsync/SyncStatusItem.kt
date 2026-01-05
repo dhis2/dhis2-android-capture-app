cad81e57937d42a8fe76bc95a0af88ddba80cb5d
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2026-01-05 11:21:01 +0100

    build: remove ui components

--- app/src/main/java/org/dhis2/utils/granularsync/SyncStatusItem.kt
+++ app/src/main/java/org/dhis2/utils/granularsync/SyncStatusItem.kt
@@ -18,7 +18,7 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.dhis2.ui.R
+import org.dhis2.commons.R
 
 @Composable
 fun SyncStatusItem(
