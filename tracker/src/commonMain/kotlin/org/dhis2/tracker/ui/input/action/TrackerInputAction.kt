414373d870d89921c72cc5aa73118e4b126bf3a0
andresmr <andres@dhis2.org>
2026-01-27 10:45:39 +0100

    refactor: Rename SearchAction.kt to TrackerInputAction.kt

--- tracker/src/commonMain/kotlin/org/dhis2/tracker/ui/input/action/TrackerInputAction.kt
+++ tracker/src/commonMain/kotlin/org/dhis2/tracker/ui/input/action/TrackerInputAction.kt
@@ -1,11 +1,13 @@
-package org.dhis2.usescases.searchTrackEntity
+package org.dhis2.tracker.ui.input.action
 
 import org.dhis2.mobile.commons.model.CustomIntentModel
-import org.dhis2.usescases.searchTrackEntity.searchparameters.FieldUid
 
-sealed interface SearchAction {
+typealias CustomIntentUid = String
+typealias FieldUid = String
+
+sealed interface TrackerInputAction {
     data class LaunchCustomIntent(
         val fieldUid: FieldUid,
         val customIntentModel: CustomIntentModel,
-    ) : SearchAction
+    ) : TrackerInputAction
 }
