feb6b1dff13fbc5d4a6968319fcda4d0e82cee58
Xavier Molloy <xavi@dhis2.org>
2026-02-02 13:11:07 +0100

    feat: [ANDROAPP-7472] II data models and mappers  (#4623)
    * feat: [ANDROAPP-7472] data models and mappers
    
    * feat: [ANDROAPP-7472] ktlint and copilot review comments
    
    * feat: [ANDROAPP-7472] correct geometry mapping
    
    * feat: [ANDROAPP-7472] remove verbose imports
    
    * feat: [ANDROAPP-7472] copilot review
    
    * fix: [ANDROAPP-7472] copilot review
    
    * fix: [ANDROAPP-7472] sonar smell
    
    * fix: [ANDROAPP-7472] code review comments

---
+++ tracker/src/commonMain/kotlin/org/dhis2/tracker/search/model/SearchTrackedEntitiesInput.kt
@@ -0,0 +1,10 @@
+package org.dhis2.tracker.search.model
+
+data class SearchTrackedEntitiesInput(
+    val selectedProgram: String?,
+    val allowCache: Boolean,
+    val excludeValues: Set<String>?,
+    val hasStateFilters: Boolean,
+    val isOnline: Boolean,
+    val queryData: Map<String, List<String>?>?,
+)
