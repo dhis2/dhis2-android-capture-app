8660d588e4f3f7ff437cec9a7c3648b39da69431
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2026-02-06 14:28:38 +0100

    feat: [ANDROAPP-7459][2/6] Added required implementations for new workers

--- sync/src/androidMain/kotlin/org/dhis2/mobile/sync/di/SyncModule.android.kt
+++ sync/src/androidMain/kotlin/org/dhis2/mobile/sync/di/SyncModule.android.kt
@@ -5,6 +5,7 @@ import org.dhis2.mobile.sync.data.AndroidSyncBackgroundJobAction
 import org.dhis2.mobile.sync.data.AndroidSyncRepository
 import org.dhis2.mobile.sync.data.SyncBackgroundJobAction
 import org.dhis2.mobile.sync.data.SyncRepository
+import org.dhis2.mobile.sync.domain.SyncMetadata
 import org.dhis2.mobile.sync.domain.SyncSettings
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
@@ -23,6 +24,8 @@ actual val syncModule =
         factory<SyncRepository> {
             AndroidSyncRepository(get(), get(), get(), get(), get())
         }
+
+        factoryOf(::SyncMetadata)
 
         factoryOf(::SyncSettings)
     }
