7ace0f2ccd185e934fb701233642d091623ebfec
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2026-02-06 14:35:17 +0100

    feat: [ANDROAPP-7459][4/6] Implement new sync in the app

--- app/src/main/java/org/dhis2/usescases/sync/SyncActivity.kt
+++ app/src/main/java/org/dhis2/usescases/sync/SyncActivity.kt
@@ -18,6 +18,7 @@ import org.dhis2.mobile.sync.data.SyncBackgroundJobAction
 import org.dhis2.usescases.general.ActivityGlobalAbstract
 import org.dhis2.usescases.login.LoginActivity
 import org.dhis2.usescases.main.MainActivity
+import org.dhis2.usescases.main.navigateTo
 import org.dhis2.utils.OnDialogClickListener
 import org.dhis2.utils.extension.navigateTo
 import org.dhis2.utils.extension.share
