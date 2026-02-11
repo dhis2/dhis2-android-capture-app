74ee0e3bc97212ce7129017ea30620e43c9863f9
andresmr <andres@dhis2.org>
2026-02-11 07:50:48 +0100

    fix: avoid non null assertion in LoginUserWithOAuth.kt

--- login/src/commonMain/kotlin/org/dhis2/mobile/login/main/domain/usecase/LoginUserWithOAuth.kt
+++ login/src/commonMain/kotlin/org/dhis2/mobile/login/main/domain/usecase/LoginUserWithOAuth.kt
@@ -2,6 +2,9 @@ package org.dhis2.mobile.login.main.domain.usecase
 
 import org.dhis2.mobile.login.main.data.LoginRepository
 import org.dhis2.mobile.login.main.domain.model.LoginResult
+import org.dhis2.mobile.login.resources.Res
+import org.dhis2.mobile.login.resources.missing_username_from_oauth_login_response
+import org.jetbrains.compose.resources.getString
 
 class LoginUserWithOAuth(
     repository: LoginRepository,
@@ -14,7 +17,11 @@ class LoginUserWithOAuth(
         return when {
             result.isSuccess -> {
                 val username = result.getOrNull()
-                handleResult(Result.success(Unit), serverUrl, username!!)
+                if (username != null) {
+                    handleResult(Result.success(Unit), serverUrl, username)
+                } else {
+                    LoginResult.Error(getString(Res.string.missing_username_from_oauth_login_response))
+                }
             }
 
             else -> {
