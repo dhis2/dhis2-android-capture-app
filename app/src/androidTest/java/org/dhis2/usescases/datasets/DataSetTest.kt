7e02f5d91ec8f74889ddc43d6bba49618a4d5ed1
andresmr <andres@dhis2.org>
2026-02-20 07:54:47 +0100

    test: Replaced all awaitIdle() calls with waitForIdle()

--- app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetTest.kt
+++ app/src/androidTest/java/org/dhis2/usescases/datasets/DataSetTest.kt
@@ -367,15 +367,15 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun waitForTableToBeVisible() {
-        composeTestRule.awaitIdle()
+    private fun waitForTableToBeVisible() {
+        composeTestRule.waitForIdle()
         dataSetRobot {
             clickOnDataSetAtPosition(0)
         }
         tableIsVisible()
     }
 
-    private suspend fun checkTableIsNotEditable() {
+    private fun checkTableIsNotEditable() {
         tableIsVisible()
         composeTestRule.onNodeWithTag("TABLE_SCROLLABLE_COLUMN").printToLog("TABLE_LOG")
         dataSetTableRobot(composeTestRule) {
@@ -385,7 +385,7 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun checkContentBoxesAreDisplayed() {
+    private fun checkContentBoxesAreDisplayed() {
         tableIsVisible()
         // Check top and bottom content is displayed in initial section
         dataSetDetailRobot(composeTestRule) {
@@ -443,7 +443,7 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun checkCategoryIsMovedToRow() {
+    private fun checkCategoryIsMovedToRow() {
         val cellIdSection8 = "PGRlPlAzakpINVR1NVZDLCA8Y28+RmJMWlMzdWVXYlE6PGNvPg=="
         val cellId2Section8 = "PGRlPkZRMm84VUJsY3JTLCA8Y28+RmJMWlMzdWVXYlE6PGNvPg=="
         val cellIdSection16 = "PGRlPkFyUzdWeXVMOTVmLCA8Y28+RmJMWlMzdWVXYlE6PGNvPg=="
@@ -554,7 +554,7 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun checkAutomaticGroupingDisabled() {
+    private fun checkAutomaticGroupingDisabled() {
         val table19 = "t3aNCvHsoSn_0"
         val table219 = "aN8uN5b15YG_1"
         val table20 = "ck7mRNwGDjP_1"
@@ -631,7 +631,7 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun checkPivotOptions() {
+    private fun checkPivotOptions() {
         val table5 = "aN8uN5b15YG"
         val table23 = "aN8uN5b15YG_1"
         val cellIdSection5 = "PGNvYz5ET0M3ZW1MenlSaTo8ZGU+TFNKNW1LcHlFdjE="
@@ -700,29 +700,29 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun tableIsVisible() {
-        composeTestRule.awaitIdle()
+    private fun tableIsVisible() {
+        composeTestRule.waitForIdle()
         dataSetTableRobot(composeTestRule) {
             assertTableIsDisplayed()
         }
     }
 
-    private suspend fun checkImmunizationTableIsDisplayed() {
-        composeTestRule.awaitIdle()
+    private fun checkImmunizationTableIsDisplayed() {
+        composeTestRule.waitForIdle()
         dataSetTableRobot(composeTestRule) {
             assertImmunizationTableIsDisplayed()
         }
     }
 
-    private suspend fun syncButtonIsAvailableStep() {
-        composeTestRule.awaitIdle()
+    private fun syncButtonIsAvailableStep() {
+        composeTestRule.waitForIdle()
         dataSetTableRobot(composeTestRule) {
             syncIsAvailable()
         }
     }
 
-    private suspend fun checkTotals() {
-        composeTestRule.awaitIdle()
+    private fun checkTotals() {
+        composeTestRule.waitForIdle()
         dataSetTableRobot(composeTestRule) {
             totalsAreDisplayed(
                 tableId = "dzjKKQq0cSO",
@@ -787,14 +787,14 @@ class DataSetTest : BaseTest() {
         }
     }
 
-    private suspend fun checkIndicatorsStep() {
-        composeTestRule.awaitIdle()
+    private fun checkIndicatorsStep() {
+        composeTestRule.waitForIdle()
         dataSetTableRobot(composeTestRule) {
             indicatorTableIsDisplayed()
         }
     }
 
-    private suspend fun reenterDataSetToCheckValueSavedStep() {
+    private fun reenterDataSetToCheckValueSavedStep() {
         val cell00Id = "PGRlPnM0Nm01TVMwaHh1Ojxjb2M+UHJsdDBDMVJGMHM="
 
         dataSetTableRobot(composeTestRule) {
