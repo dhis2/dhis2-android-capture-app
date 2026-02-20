5ce9da3504226af60a20965de694d4d1268d4b5d
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2026-02-20 14:42:31 +0100

    fix crash when deleting account while metadata sync is in progress

--- app/src/main/java/org/dhis2/usescases/settings/domain/GetSettingsState.kt
+++ app/src/main/java/org/dhis2/usescases/settings/domain/GetSettingsState.kt
@@ -1,40 +1,42 @@
 package org.dhis2.usescases.settings.domain
 
+import org.dhis2.mobile.commons.domain.UseCase
 import org.dhis2.usescases.settings.GatewayValidator
-import org.dhis2.usescases.settings.SettingItem
 import org.dhis2.usescases.settings.SettingsRepository
 import org.dhis2.usescases.settings.models.SettingsState
+import org.dhis2.usescases.settings.models.SyncStateInput
 
 class GetSettingsState(
     private val settingsRepository: SettingsRepository,
     private val gatewayValidator: GatewayValidator,
-) {
-    suspend operator fun invoke(
-        openedItem: SettingItem?,
-        hasConnection: Boolean,
-        metadataSyncInProgress: Boolean,
-        dataSyncInProgress: Boolean,
-    ): SettingsState =
-        SettingsState(
-            openedItem = openedItem,
-            hasConnection = hasConnection,
-            metadataSettingsViewModel =
-                settingsRepository.metaSync().blockingGet().copy(
-                    syncInProgress = metadataSyncInProgress,
-                ),
-            dataSettingsViewModel =
-                settingsRepository.dataSync().blockingGet().copy(
-                    syncInProgress = dataSyncInProgress,
-                ),
-            syncParametersViewModel = settingsRepository.syncParameters().blockingGet(),
-            reservedValueSettingsViewModel = settingsRepository.reservedValues().blockingGet(),
-            smsSettingsViewModel =
-                with(settingsRepository.sms().blockingGet()) {
-                    copy(
-                        gatewayValidationResult = gatewayValidator(this.gatewayNumber),
-                    )
-                },
-            isTwoFAConfigured = settingsRepository.isTwoFAConfigured(),
-            versionName = settingsRepository.getVersionName(),
-        )
+) : UseCase<SyncStateInput, SettingsState> {
+    override suspend fun invoke(input: SyncStateInput) =
+        try {
+            val state =
+                SettingsState(
+                    openedItem = input.openedItem,
+                    hasConnection = input.hasConnection,
+                    metadataSettingsViewModel =
+                        settingsRepository.metaSync().blockingGet().copy(
+                            syncInProgress = input.metadataSyncInProgress,
+                        ),
+                    dataSettingsViewModel =
+                        settingsRepository.dataSync().blockingGet().copy(
+                            syncInProgress = input.dataSyncInProgress,
+                        ),
+                    syncParametersViewModel = settingsRepository.syncParameters().blockingGet(),
+                    reservedValueSettingsViewModel = settingsRepository.reservedValues().blockingGet(),
+                    smsSettingsViewModel =
+                        with(settingsRepository.sms().blockingGet()) {
+                            copy(
+                                gatewayValidationResult = gatewayValidator(this.gatewayNumber),
+                            )
+                        },
+                    isTwoFAConfigured = settingsRepository.isTwoFAConfigured(),
+                    versionName = settingsRepository.getVersionName(),
+                )
+            Result.success(state)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
 }
