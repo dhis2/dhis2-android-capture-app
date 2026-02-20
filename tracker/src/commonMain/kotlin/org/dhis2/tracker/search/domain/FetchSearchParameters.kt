97799db928c6f79ff554dde27ce1109249b3fd09
Xavier Molloy <xavi@dhis2.org>
2026-02-20 07:57:59 +0100

    feat: [ANDROAPP-7470] Remove todo, make methods internal in Fetch Search parameters usecase

--- tracker/src/commonMain/kotlin/org/dhis2/tracker/search/domain/FetchSearchParameters.kt
+++ tracker/src/commonMain/kotlin/org/dhis2/tracker/search/domain/FetchSearchParameters.kt
@@ -28,7 +28,7 @@ class FetchSearchParameters(
         }
 
     // Sort parameters to list first QR or BarCode uniques, then QR or BarCode and then remaining uniques
-    private fun sortSearchParameters(parameters: List<SearchParameterModel>): List<SearchParameterModel> =
+    internal fun sortSearchParameters(parameters: List<SearchParameterModel>): List<SearchParameterModel> =
         parameters.sortedWith(
             compareByDescending<SearchParameterModel> {
                 isQrCodeOrBarCode(it.inputType) && it.isUnique
@@ -37,6 +37,6 @@ class FetchSearchParameters(
             }.thenByDescending { it.isUnique },
         )
 
-    private fun isQrCodeOrBarCode(inputType: TrackerInputType?): Boolean =
+    internal fun isQrCodeOrBarCode(inputType: TrackerInputType?): Boolean =
         inputType == TrackerInputType.QR_CODE || inputType == TrackerInputType.BAR_CODE
 }
