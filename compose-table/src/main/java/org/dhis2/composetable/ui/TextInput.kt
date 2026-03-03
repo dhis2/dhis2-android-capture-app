1b295263d9705dafca75a3b3e45af97e28662d90
Andrés Miguel Rubio <andres@dhis2.org>
2026-03-03 12:36:51 +0100

    fix: Replace .height(columnHeightDp) from MainContent.kt by fillMaxHeight() (#4679)

--- compose-table/src/main/java/org/dhis2/composetable/ui/TextInput.kt
+++ compose-table/src/main/java/org/dhis2/composetable/ui/TextInput.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -76,8 +75,7 @@ fun TextInput(
                 .background(
                     color = Color.White,
                     shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
-                ).padding(start = 16.dp, end = 4.dp, top = 16.dp, bottom = 4.dp)
-                .imePadding(),
+                ).padding(start = 16.dp, end = 4.dp, top = 16.dp, bottom = 4.dp),
         verticalArrangement = spacedBy(8.dp),
     ) {
         InputTitle(textInputModel.mainLabel, textInputModel.secondaryLabels)
