55b101300226429103776d0bc5f49b95d4e79ecb
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2026-03-04 16:28:33 +0100

    feat: [ANDROAPP-7507] Support hybrid basemaps

--- dhis2_android_maps/src/test/java/org/dhis2/maps/usecases/MapStyleConfigurationTest.kt
+++ dhis2_android_maps/src/test/java/org/dhis2/maps/usecases/MapStyleConfigurationTest.kt
@@ -3,7 +3,10 @@ package org.dhis2.maps.usecases
 import org.dhis2.commons.data.ProgramConfigurationRepository
 import org.dhis2.maps.model.MapScope
 import org.hisp.dhis.android.core.D2
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.EnumFilterConnector
+import org.hisp.dhis.android.core.arch.repositories.filters.internal.StringFilterConnector
 import org.hisp.dhis.android.core.map.layer.MapLayer
+import org.hisp.dhis.android.core.map.layer.MapLayerCollectionRepository
 import org.hisp.dhis.android.core.map.layer.MapLayerImageryProvider
 import org.hisp.dhis.android.core.map.layer.MapLayerPosition
 import org.hisp.dhis.android.core.settings.DataSetConfigurationSetting
@@ -13,12 +16,13 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.Mockito
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 class MapStyleConfigurationTest {
-    private val d2: D2 = Mockito.mock(D2::class.java, Mockito.RETURNS_DEEP_STUBS)
+    private val d2: D2 = mock(defaultAnswer = Mockito.RETURNS_DEEP_STUBS)
     private val uid = "uid"
     private val programConfigurationSetting: ProgramConfigurationSetting = mock()
     private val dataSetConfigurationSetting: DataSetConfigurationSetting = mock()
@@ -35,17 +39,13 @@ class MapStyleConfigurationTest {
             programConfigurationRepository,
         )
 
+    private val stringFilterConnector: StringFilterConnector<MapLayerCollectionRepository> = mock()
+    private val enumFilterConnector: EnumFilterConnector<MapLayerCollectionRepository, MapLayerPosition> =
+        mock()
+
     @Test
     fun shouldFetchMapStyles() {
-        whenever(d2.mapsModule().mapLayers()) doReturn mock()
-        whenever(d2.mapsModule().mapLayers().withImageryProviders()) doReturn mock()
-        whenever(
-            d2
-                .mapsModule()
-                .mapLayers()
-                .withImageryProviders()
-                .blockingGet(),
-        ) doReturn
+        mockBasemaps(
             listOf(
                 mockMapLayer(
                     displayName = "basemap 1",
@@ -68,36 +68,45 @@ class MapStyleConfigurationTest {
                             mockImaginaryProvider("© Carto"),
                         ),
                 ),
-            )
+            ),
+        )
+
+        mockOverlays(emptyList())
 
         mapStyleConfiguration.fetchMapStyles().let { result ->
             assertTrue(result.size == 2)
             assertTrue(
                 result[0]
-                    .sources.rasterTiles.tiles.size == 2,
+                    .sources.entries
+                    .first()
+                    .value.tiles.size == 2,
             )
-            assertTrue(result[0].sources.rasterTiles.tiles[0] == "http://a.test.test/x/y/z")
-            assertTrue(result[0].sources.attribution == "© Maplibre")
-            assertTrue(result[0].sources.rasterTiles.tiles[1] == "http://b.test.test/x/y/z")
+            assertTrue(
+                result[0]
+                    .sources.entries
+                    .first()
+                    .value.tiles[0] == "http://a.test.test/x/y/z",
+            )
+            assertTrue(result[0].attribution == "© Maplibre")
+            assertTrue(
+                result[0]
+                    .sources.entries
+                    .first()
+                    .value.tiles[1] == "http://b.test.test/x/y/z",
+            )
             assertTrue(
                 result[1]
-                    .sources.rasterTiles.tiles.size == 1,
+                    .sources.entries
+                    .first()
+                    .value.tiles.size == 1,
             )
-            assertTrue(result[1].sources.attribution == "© Maplibre, © Carto")
+            assertTrue(result[1].attribution == "© Maplibre, © Carto")
         }
     }
 
     @Test
     fun shouldFetchCustomMaps() {
-        whenever(d2.mapsModule().mapLayers()) doReturn mock()
-        whenever(d2.mapsModule().mapLayers().withImageryProviders()) doReturn mock()
-        whenever(
-            d2
-                .mapsModule()
-                .mapLayers()
-                .withImageryProviders()
-                .blockingGet(),
-        ) doReturn
+        mockBasemaps(
             listOf(
                 mockMapLayer(
                     displayName = "basemap 1",
@@ -120,23 +129,122 @@ class MapStyleConfigurationTest {
                             mockImaginaryProvider("© Carto"),
                         ),
                 ),
-            )
+            ),
+        )
+
+        mockOverlays(emptyList())
 
         mapStyleConfiguration.fetchMapStyles().let { result ->
             assertTrue(result.size == 2)
             assertTrue(
                 result[0]
-                    .sources.rasterTiles.tiles.size == 4,
+                    .sources.entries
+                    .first()
+                    .value.tiles.size == 4,
             )
-            assertTrue(result[0].sources.rasterTiles.tiles[0] == "http://a.test.test/x/y/z")
-            assertTrue(result[0].sources.attribution == "© Maplibre")
-            assertTrue(result[0].sources.rasterTiles.tiles[1] == "http://b.test.test/x/y/z")
+            assertTrue(
+                result[0]
+                    .sources.entries
+                    .first()
+                    .value.tiles[0] == "http://a.test.test/x/y/z",
+            )
+            assertTrue(result[0].attribution == "© Maplibre")
+            assertTrue(
+                result[0]
+                    .sources.entries
+                    .first()
+                    .value.tiles[1] == "http://b.test.test/x/y/z",
+            )
             assertTrue(
                 result[1]
-                    .sources.rasterTiles.tiles.size == 4,
+                    .sources.entries
+                    .first()
+                    .value.tiles.size == 4,
             )
-            assertTrue(result[1].sources.attribution == "© Maplibre, © Carto")
+            assertTrue(result[1].attribution == "© Maplibre, © Carto")
         }
+    }
+
+    @Test
+    fun shouldFetchBasemapWithOverlay() {
+        mockBasemaps(
+            listOf(
+                mockMapLayer(
+                    displayName = "basemap 1",
+                    imageUrl = "http://{s}.test.test/x/y/z",
+                    subDomainPlaceHolder = null,
+                    subdomains = null,
+                    imaginaryProviders =
+                        listOf(
+                            mockImaginaryProvider("© Maplibre"),
+                        ),
+                ),
+            ),
+        )
+
+        mockOverlays(
+            listOf(
+                mockMapLayer(
+                    displayName = "overlay 1",
+                    imageUrl = "http://{s}.overlay.test/x/y/z",
+                    subdomains = null,
+                    subDomainPlaceHolder = null,
+                    imaginaryProviders =
+                        listOf(
+                            mockImaginaryProvider("© Overlay"),
+                        ),
+                ),
+            ),
+        )
+
+        mapStyleConfiguration.fetchMapStyles().let { result ->
+            assertTrue(result[0].sources.size == 2)
+            assertTrue(
+                result[0]
+                    .sources.keys
+                    .toList()[1] == "overlay 1",
+            )
+            assertTrue(result[0].sources["overlay 1"]?.tiles[0] == "http://a.overlay.test/x/y/z")
+            assertEquals("© Maplibre, © Overlay", result[0].attribution)
+        }
+    }
+
+    private fun mockBasemaps(mockedBasemap: List<MapLayer>) {
+        val mockedMapLayerCollectionRepository: MapLayerCollectionRepository = mock()
+
+        whenever(d2.mapsModule().mapLayers()) doReturn mockedMapLayerCollectionRepository
+        whenever(mockedMapLayerCollectionRepository.withImageryProviders()) doReturn mockedMapLayerCollectionRepository
+        whenever(
+            mockedMapLayerCollectionRepository.byMapLayerPosition(),
+        ) doReturn enumFilterConnector
+        whenever(
+            enumFilterConnector.eq(MapLayerPosition.BASEMAP),
+        ) doReturn mockedMapLayerCollectionRepository
+        whenever(
+            mockedMapLayerCollectionRepository.blockingGet(),
+        ) doReturn mockedBasemap
+    }
+
+    private fun mockOverlays(mockedOverlays: List<MapLayer>) {
+        val mockedMapLayerCollectionRepository: MapLayerCollectionRepository = mock()
+
+        whenever(d2.mapsModule().mapLayers()) doReturn mockedMapLayerCollectionRepository
+        whenever(mockedMapLayerCollectionRepository.withImageryProviders()) doReturn mockedMapLayerCollectionRepository
+        whenever(
+            mockedMapLayerCollectionRepository.byLinkedLayerUid(),
+        ) doReturn stringFilterConnector
+        whenever(
+            stringFilterConnector.eq(any()),
+        ) doReturn mockedMapLayerCollectionRepository
+        whenever(
+            mockedMapLayerCollectionRepository.byMapLayerPosition(),
+        ) doReturn enumFilterConnector
+        whenever(
+            enumFilterConnector.eq(MapLayerPosition.OVERLAY),
+        ) doReturn mockedMapLayerCollectionRepository
+        whenever(
+            mockedMapLayerCollectionRepository.blockingGet(),
+        ) doReturn mockedOverlays
     }
 
     @Test
