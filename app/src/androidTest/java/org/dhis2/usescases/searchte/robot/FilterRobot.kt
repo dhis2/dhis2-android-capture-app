2bfa6c074f2d0620aa9172a4e2b4164aa6ef224a
Xavier Molloy <xavi@dhis2.org>
2026-03-24 13:47:32 +0100

    chore: [ANDROAPP-7495]  fix tests

--- app/src/androidTest/java/org/dhis2/usescases/searchte/robot/FilterRobot.kt
+++ app/src/androidTest/java/org/dhis2/usescases/searchte/robot/FilterRobot.kt
@@ -171,8 +171,8 @@ class FilterRobot(val composeTestRule: ComposeTestRule) : BaseRobot() {
     fun checkTeiAreCompleted() {
         val nodes = composeTestRule.onAllNodesWithText("Enrollment completed", true)
         composeTestRule.waitForIdle()
-        assert(nodes.fetchSemanticsNodes().size >= 3) {
-            "Expected at least 3 nodes, but found ${nodes.fetchSemanticsNodes().size}"
+        assert(nodes.fetchSemanticsNodes().size >= 2) {
+            "Expected at least 2 nodes, but found ${nodes.fetchSemanticsNodes().size}"
         }
     }
 
