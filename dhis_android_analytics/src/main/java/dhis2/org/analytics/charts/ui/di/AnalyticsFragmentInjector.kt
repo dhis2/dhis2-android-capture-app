32e6736e81acbb64984f692c9c3ea8a0f15bf504
andresmr <andres@dhis2.org>
2026-03-26 17:20:21 +0100

    refactor: - Introduce `GetEnrollmentAnalyticsUseCase` to handle enrollment analytics fetching logic. - Migrated `GroupAnalyticsViewModel` to use `StateFlow` instead of `LiveData` for `chipItems` and `analytics`. - Updated `GroupAnalyticsFragment` to collect flows within the `repeatOnLifecycle` block.

--- dhis_android_analytics/src/main/java/dhis2/org/analytics/charts/ui/di/AnalyticsFragmentInjector.kt
+++ dhis_android_analytics/src/main/java/dhis2/org/analytics/charts/ui/di/AnalyticsFragmentInjector.kt
@@ -4,10 +4,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.Subcomponent
 import dhis2.org.analytics.charts.Charts
+import dhis2.org.analytics.charts.domain.GetEnrollmentAnalyticsUseCase
 import dhis2.org.analytics.charts.ui.AnalyticMode
 import dhis2.org.analytics.charts.ui.GroupAnalyticsFragment
 import dhis2.org.analytics.charts.ui.GroupAnalyticsViewModelFactory
 import org.dhis2.commons.matomo.MatomoAnalyticsController
+import org.dhis2.commons.viewmodel.DispatcherProvider
 
 @Subcomponent(modules = [AnalyticsFragmentModule::class])
 interface AnalyticsFragmentComponent {
@@ -23,5 +25,15 @@ class AnalyticsFragmentModule(
     fun provideViewModelFactory(
         charts: Charts,
         matomoAnalyticsController: MatomoAnalyticsController,
-    ): GroupAnalyticsViewModelFactory = GroupAnalyticsViewModelFactory(mode, uid, charts, matomoAnalyticsController)
+        getEnrollmentAnalyticsUseCase: GetEnrollmentAnalyticsUseCase,
+        dispatcherProvider: DispatcherProvider,
+    ): GroupAnalyticsViewModelFactory =
+        GroupAnalyticsViewModelFactory(
+            mode,
+            uid,
+            charts,
+            matomoAnalyticsController,
+            getEnrollmentAnalyticsUseCase,
+            dispatcherProvider,
+        )
 }
