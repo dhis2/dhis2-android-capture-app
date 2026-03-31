42142b658d5453cae6f6b90759a01b09b977bd00
Xavier Molloy <xavi@dhis2.org>
2026-03-31 13:05:33 +0200

    fix: [ANDROAPP-7495] fix tests

--- app/src/test/java/org/dhis2/usescases/main/MainPresenterTest.kt
+++ app/src/test/java/org/dhis2/usescases/main/MainPresenterTest.kt
@@ -33,12 +33,15 @@ import org.dhis2.mobile.sync.domain.SyncStatusController
 import org.dhis2.usescases.login.SyncIsPerformedInteractor
 import org.dhis2.usescases.main.domain.LogoutUser
 import org.dhis2.usescases.settings.DeleteUserData
+import org.hisp.dhis.android.core.D2
 import org.hisp.dhis.android.core.arch.helpers.DateUtils
 import org.hisp.dhis.android.core.category.CategoryCombo
 import org.hisp.dhis.android.core.category.CategoryOptionCombo
 import org.hisp.dhis.android.core.configuration.internal.DatabaseAccount
 import org.hisp.dhis.android.core.systeminfo.SystemInfo
+import org.hisp.dhis.android.core.user.AccountManager
 import org.hisp.dhis.android.core.user.User
+import org.hisp.dhis.android.core.user.UserModule
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
@@ -81,6 +84,9 @@ class MainPresenterTest {
     private val forceToNotSynced: Boolean = false
 
     private val logoutUser: LogoutUser = mock()
+    private val accountManager: AccountManager = mock()
+    private val userModule: UserModule = mock()
+    private val d2: D2 = mock()
 
     @Rule
     @JvmField
@@ -184,22 +190,18 @@ class MainPresenterTest {
     @Test
     fun `Should go to delete account`() = runTest {
         val randomFile = File("random")
+
         whenever(view.obtainFileView()) doReturn randomFile
-        whenever(userManager.d2) doReturn mock()
-        whenever(userManager.d2.userModule()) doReturn mock()
-        whenever(userManager.d2.userModule().accountManager()) doReturn mock()
-        whenever(userManager.d2.wipeModule()) doReturn mock()
-        whenever(userManager.d2.wipeModule().wipeEverything()) doReturn Unit
-        whenever(view.obtainFileView()) doReturn randomFile
+        whenever(userManager.d2) doReturn d2
+        whenever(d2.userModule()) doReturn userModule
+        whenever(userModule.accountManager()) doReturn accountManager
         whenever(repository.accountsCount()) doReturn 1
-        whenever(repository.checkDeleteBiometricsPermission()) doReturn Unit
 
         presenter.onDeleteAccount()
 
         verify(view).showProgressDeleteNotification()
         verify(deleteUserData).wipeCacheAndPreferences(randomFile, dispatcherProvider)
-        verify(userManager.d2?.wipeModule())?.wipeEverything()
-        verify(userManager.d2?.userModule()?.accountManager())?.deleteCurrentAccount()
+        verify(accountManager).deleteCurrentAccount()
         verify(view).cancelNotifications()
         verify(view).goToLogin(1, true)
     }
@@ -228,29 +230,19 @@ class MainPresenterTest {
         val randomFile = File("random")
 
         whenever(view.obtainFileView()) doReturn randomFile
-        whenever(userManager.d2) doReturn mock()
-        whenever(userManager.d2.userModule()) doReturn mock()
-        whenever(userManager.d2.userModule().accountManager()) doReturn mock()
-        whenever(userManager.d2.wipeModule()) doReturn mock()
-        whenever(userManager.d2.wipeModule().wipeEverything()) doReturn Unit
-        whenever(
-            userManager.d2
-                .userModule()
-                .accountManager()
-                .getAccounts(),
-        ) doReturn
-            listOf(
-                firstRandomUserAccount,
-                secondRandomUserAccount,
-            )
+        whenever(userManager.d2) doReturn d2
+        whenever(d2.userModule()) doReturn userModule
+        whenever(userModule.accountManager()) doReturn accountManager
+        whenever(accountManager.getAccounts()) doReturn listOf(
+            firstRandomUserAccount,
+            secondRandomUserAccount,
+        )
         whenever(repository.accountsCount()) doReturn 2
-        whenever(repository.checkDeleteBiometricsPermission()) doReturn Unit
 
         presenter.onDeleteAccount()
 
         verify(deleteUserData).wipeCacheAndPreferences(randomFile, dispatcherProvider)
-        verify(userManager.d2?.wipeModule())?.wipeEverything()
-        verify(userManager.d2?.userModule()?.accountManager())?.deleteCurrentAccount()
+        verify(accountManager).deleteCurrentAccount()
         verify(view).showProgressDeleteNotification()
         verify(view).cancelNotifications()
         verify(view).goToLogin(2, true)
