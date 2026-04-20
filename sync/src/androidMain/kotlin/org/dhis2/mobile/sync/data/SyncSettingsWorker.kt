55116c586ec640b99d059d89fbfe4f8709eb7f8d
Pablo Pajuelo Cabezas <ppajueloc@gmail.com>
2026-04-20 16:08:02 +0200

    remove settings foreground setter

--- sync/src/androidMain/kotlin/org/dhis2/mobile/sync/data/SyncSettingsWorker.kt
+++ sync/src/androidMain/kotlin/org/dhis2/mobile/sync/data/SyncSettingsWorker.kt
@@ -2,11 +2,9 @@ package org.dhis2.mobile.sync.data
 
 import android.content.Context
 import androidx.work.CoroutineWorker
-import androidx.work.ForegroundInfo
 import androidx.work.WorkerParameters
 import org.dhis2.mobile.commons.domain.invoke
 import org.dhis2.mobile.commons.notifications.NotificationManager
-import org.dhis2.mobile.commons.notifications.WorkerNotificationInfo
 import org.dhis2.mobile.sync.R
 import org.dhis2.mobile.sync.domain.SyncSettings
 import org.dhis2.mobile.sync.resources.Res
@@ -21,7 +19,6 @@ class SyncSettingsWorker(
     private val notificationManager: NotificationManager,
 ) : CoroutineWorker(context, workerParams) {
     override suspend fun doWork(): Result {
-        setForeground(getForegroundInfo())
         notificationManager.displaySyncSettingsNotification(
             smallIcon = R.drawable.ic_sync,
             contentTitle = getString(Res.string.app_name),
@@ -33,20 +30,5 @@ class SyncSettingsWorker(
             result.isSuccess -> Result.success()
             else -> Result.failure()
         }
-    }
-
-    override suspend fun getForegroundInfo(): ForegroundInfo {
-        val notificationModel =
-            notificationManager.getSyncSettingsNotificationModel(
-                smallIcon = R.drawable.ic_sync,
-                contentTitle = getString(Res.string.app_name),
-                contentText = getString(Res.string.syncing_settings),
-            )
-        val notificationInfo =
-            notificationModel as? WorkerNotificationInfo
-                ?: throw IllegalStateException(
-                    "Expected WorkerNotificationInfo but got ${notificationModel::class.qualifiedName}",
-                )
-        return notificationInfo.foregroundInfo
     }
 }
