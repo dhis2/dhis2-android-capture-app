011f77fa042dd00a9322a67c2cd0dcb158031a43
andresmr <andres@andresmr.com>
2026-05-13 07:32:56 +0200

    test: [ANDROAPP-7618] consolidate TEI dashboard note tests into a single workflow

--- app/src/androidTest/java/org/dhis2/usescases/teidashboard/TeiDashboardIntents.kt
+++ app/src/androidTest/java/org/dhis2/usescases/teidashboard/TeiDashboardIntents.kt
@@ -16,22 +16,12 @@ private const val TEI_UID_VALUE_COMPLETED = "vOxUH373fy5"
 private const val ENROLLMENT_VALUE_COMPLETED = "KpknKHptul0"
 private const val TEI_UID_VALUE_OPENED = "Pqv3LrNECkn"
 private const val ENROLLMENT_VALUE_OPENED = "clL63cPdk86"
-private const val TEI_UID_VALUE_OPENED_FULL = "r2FEXpX6ize"
-private const val ENROLLMENT_VALUE_OPENED_FULL = "Z6yN122z3Fw"
 private const val TEI_UID_VALUE_OPEN_REFERRAL = "G1afLIEKt8A"
 private const val ENROLLMENT_VALUE_OPEN_REFERRAL = "uzz6zvuRyjn"
 private const val TEI_UID_VALUE_OPEN_TO_COMPLETE = "qx4yw1EuxmW"
 private const val ENROLLMENT_VALUE_OPEN_TO_COMPLETE = "ld0PCa4FGfq"
-private const val TEI_UID_VALUE_WITH_NOTE = "UtDZmrX5lSd"
-private const val ENROLLMENT_VALUE_WITH_NOTE = "zDZ9lZCj2aa"
-private const val TEI_UID_VALUE_TO_DELETE = "SHnmavBQu72"
-private const val ENROLLMENT_VALUE_TO_DELETE = "xkOTz2PrM5Z"
 private const val TEI_UID_VALUE_TO_SCHEDULE = "uh47DXf1St9"
 private const val ENROLLMENT_VALUE_TO_SCHEDULE = "iSNBeFcHO0X"
-private const val TEI_UID_VALUE_TO_CREATE_EVENT = "xWGaOQL0INS"
-private const val ENROLLMENT_VALUE_TO_CREATE_EVENT = "NGFp6txgfn8"
-private const val TEI_UID_VALUE_TO_EDIT_EVENT = "PQfMcpmXeFE"
-private const val ENROLLMENT_VALUE_TO_EDIT_EVENT = "Yf47yST5FF2"
 private const val TEI_UID_VALUE_TO_ENROLL = "tIJu6iqQxNV"
 private const val ENROLLMENT_VALUE_TO_ENROLL = "CCBLMntFuzb"
 private const val TEI_UID_VALUE_ANALYTICS = "wsk89u7zquT"
@@ -81,39 +71,6 @@ fun prepareTeiOpenedForCompleteProgrammeAndLaunchActivity(
     )
 }
 
-fun prepareTeiWithExistingNoteAndLaunchActivity(
-    rule: LazyActivityScenarioRule<TeiDashboardMobileActivity>
-) {
-    startTeiDashboardActivity(
-        CHILD_PROGRAM_UID_VALUE,
-        TEI_UID_VALUE_WITH_NOTE,
-        ENROLLMENT_VALUE_WITH_NOTE,
-        rule
-    )
-}
-
-fun prepareTeiOpenedWithFullEventsAndLaunchActivity(
-    rule: LazyActivityScenarioRule<TeiDashboardMobileActivity>
-) {
-    startTeiDashboardActivity(
-        CHILD_PROGRAM_UID_VALUE,
-        TEI_UID_VALUE_OPENED_FULL,
-        ENROLLMENT_VALUE_OPENED_FULL,
-        rule
-    )
-}
-
-fun prepareTeiToDeleteAndLaunchActivity(
-    rule: LazyActivityScenarioRule<TeiDashboardMobileActivity>
-) {
-    startTeiDashboardActivity(
-        CHILD_PROGRAM_UID_VALUE,
-        TEI_UID_VALUE_TO_DELETE,
-        ENROLLMENT_VALUE_TO_DELETE,
-        rule
-    )
-}
-
 fun prepareTeiOpenedWithNoPreviousEventProgrammeAndLaunchActivity(
     rule: LazyActivityScenarioRule<TeiDashboardMobileActivity>
 ) {
@@ -121,28 +78,6 @@ fun prepareTeiOpenedWithNoPreviousEventProgrammeAndLaunchActivity(
         TB_PROGRAM_UID,
         TEI_UID_VALUE_TO_SCHEDULE,
         ENROLLMENT_VALUE_TO_SCHEDULE,
-        rule
-    )
-}
-
-fun prepareTeiToCreateANewEventAndLaunchActivity(
-    rule: LazyActivityScenarioRule<TeiDashboardMobileActivity>
-) {
-    startTeiDashboardActivity(
-        TB_PROGRAM_UID,
-        TEI_UID_VALUE_TO_CREATE_EVENT,
-        ENROLLMENT_VALUE_TO_CREATE_EVENT,
-        rule
-    )
-}
-
-fun prepareTeiOpenedToEditAndLaunchActivity(
-    rule: LazyActivityScenarioRule<TeiDashboardMobileActivity>
-) {
-    startTeiDashboardActivity(
-        TB_PROGRAM_UID,
-        TEI_UID_VALUE_TO_EDIT_EVENT,
-        ENROLLMENT_VALUE_TO_EDIT_EVENT,
         rule
     )
 }
