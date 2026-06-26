c9a9e88459f8e83f00426bd2d15ddcea5e56af4a
Manu Muñoz <manu.munoz@dhis2.org>
2026-06-26 14:46:20 +0200

    feat: [ANDROAPP-7501] custom tab oauth process (#4915)
    Signed-off-by: Manu Muñoz <manu.munoz@dhis2.org>

--- login/src/androidMain/kotlin/org/dhis2/mobile/login/main/ui/screen/WebAuthenticator.android.kt
+++ login/src/androidMain/kotlin/org/dhis2/mobile/login/main/ui/screen/WebAuthenticator.android.kt
@@ -4,7 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.auth.AuthTabIntent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.core.net.toUri
@@ -24,18 +24,17 @@ actual fun WebAuthenticator(
         }
 
     LaunchedEffect(url) {
-        val customTabsIntent =
-            CustomTabsIntent
+        val authTabIntent =
+            AuthTabIntent
                 .Builder()
-                .setShowTitle(false)
-                .setUrlBarHidingEnabled(true)
+                .setEphemeralBrowsingEnabled(true)
                 .build()
         // Custom Tab is not kept in the activity history stack and it's removed from memory.
         // It won't appear in the "Recent Apps" list
-        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        authTabIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
 
         val intent =
-            customTabsIntent.intent.apply {
+            authTabIntent.intent.apply {
                 data = url.toUri()
             }
 
