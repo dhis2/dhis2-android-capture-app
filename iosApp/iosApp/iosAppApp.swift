10059a3030c09c1faa551c6ab1d1f3610c0175aa
andresmr <andres@dhis2.org>
2026-07-21 13:20:44 +0200

    refactor: add ios target

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Andrés Miguel Rubio on 21/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
