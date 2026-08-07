35ab5e6de17de519b7f902cc4743ac16331df0bf
Pablo <pablo@dhis2.org>
2026-08-07 09:23:32 +0200

    feat: Display tag in org unit tree (#4981)

--- commons/src/main/java/org/dhis2/commons/orgunitselector/OUTreeViewModelFactory.kt
+++ commons/src/main/java/org/dhis2/commons/orgunitselector/OUTreeViewModelFactory.kt
@@ -3,6 +3,7 @@ package org.dhis2.commons.orgunitselector
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import org.dhis2.commons.viewmodel.DispatcherProvider
+import org.dhis2.mobile.commons.domain.FetchOrgUnits
 
 @Suppress("UNCHECKED_CAST")
 class OUTreeViewModelFactory(
@@ -19,5 +20,6 @@ class OUTreeViewModelFactory(
             selectedOrgUnits = selectedOrgUnits,
             singleSelection = singleSelection,
             model = model,
+            fetchOrgUnits = FetchOrgUnits(repository),
         ) as T
 }
